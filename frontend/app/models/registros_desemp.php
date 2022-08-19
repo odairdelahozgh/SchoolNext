5833779731004696bd0ce6560923c33958c36155
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2022-08-19 03:42:45 -0500

    Create registros_desemp.php

---
+++ frontend/app/models/registros_desemp.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+  * Modelo de Ejemplo  
+  * @category App
+  * @package Models 
+  * https://github.com/KumbiaPHP/Documentation/blob/master/es/active-record.md
+  */
+
+  /**
+   * annio, periodo_id, grado_id, salon_id, fortalezas, dificultades, compromisos, fecha, 
+   * acudiente, foto_acudiente, director, foto_director, 
+   * created_at, updated_at, created_by, updated_by
+   */
+class RegistrosDesemp extends LiteRecord
+{
+    protected static $table = 'sweb_estudiantes_reg_desacacom';
+
+
+    
+}

46d7c3f548aea98fd4600217e0ee9d98467f0e17
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2022-09-12 09:27:52 -0500

    Update estudiantes_controller.php
    Se mejoró API estudiantes

--- frontend/app/controllers/api/estudiantes_controller.php
+++ frontend/app/controllers/api/estudiantes_controller.php
@@ -11,18 +11,31 @@ class EstudiantesController extends RestController
 
    /**
     * Obtiene todos los registros de estudiantes
-    * @link http://schoolnext.local.com/api/estudiantes/todos
+    * @link http://username:password@schoolnext.local.com/api/estudiantes/all
     */
-   public function get_todos() {
-      $this->data = (new EstudianteApi())->all();
+   public function get_all() {
+      $this->data = (new ApiEstudiante())->all();
    }
 
    /**
     * Devuelve el estudiante buscado por UUID
-    * @link http://schoolnext.local.com/api/estudiantes/uno/1848
+    * @link http://username:password@schoolnext.local.com/api/estudiantes/singleuuid/3b22fefc7f6afa79c54f
     */
-   public function get_uno($uuid) {
-      $record = (new EstudianteApi())->get_uuid((int)$uuid);
+   public function get_singleuuid(string $uuid) {
+      $record = (new ApiEstudiante())->get_uuid($uuid);
+      if (isset($record)) {
+         $this->data = $record;
+      } else {
+         $this->error('El registro buscado no existe', 404);
+      }
+   }
+
+   /**
+    * Devuelve el estudiante buscado por ID
+    * @link http://username:password@schoolnext.local.com/api/estudiantes/singleid/775
+    */
+    public function get_singleid(int $id) {
+      $record = (new ApiEstudiante())->get_id($id);
       if (isset($record)) {
          $this->data = $record;
       } else {

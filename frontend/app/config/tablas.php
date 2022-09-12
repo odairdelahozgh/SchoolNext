6231d5e0d43b097eb1b9862f4510450dd738a18b
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2022-09-12 09:25:26 -0500

    Update tablas.php
    se añadieron mas alias de tablas de la base de datos

--- frontend/app/config/tablas.php
+++ frontend/app/config/tablas.php
@@ -4,12 +4,59 @@
  * Parámetros de configuracion de la aplicacion
  */
 return [
-    'user'  => 'dm_user',
-    'salon' => 'sweb_salones',
-    'grado' => 'sweb_grados',
-    'seccion' => 'sweb_secciones',
-    'estud' => 'sweb_estudiantes',
-    'asign' => 'sweb_asignaturas',
-    'sap'   => 'sweb_salon_asignat_profesor',
-    'reg_obs_gen' => 'sweb_estudiantes_reg_observ_gen',
+    
+    'acciones_seguim'       => 'sweb_acciones_segto',
+    
+    'areas'                 => 'sweb_areas',
+    
+    'asign'                 => 'sweb_asignaturas',
+    'asignaturas'               => 'sweb_asignaturas',
+    
+    'aspirantes'            => 'sweb_aspirantes',
+    'aspirantes_adjuntos'   => 'sweb_aspir_adjuntos',
+    'aspirantes_psico'      => 'sweb_aspirantepsico',
+    
+    'calendario'            => 'snxt_calendario',
+
+    'estudiantes'           => 'sweb_estudiantes',
+    'estud'                     => 'sweb_estudiantes',
+
+    'datosestud_no'         => 'sweb_datosestud',
+    'estud_adjuntos'        => 'sweb_estudiante_adjuntos',
+    'estud_adjuntos_dos'    => 'sweb_estudiante_adjuntos_dos',
+    'estud_ctas_x_cobrar'   => 'sweb_estud_cpc',
+    'estud_reg_academ'      => 'sweb_estudiantes_reg_desacacom',
+    
+    'estud_reg_obs_gen'     => 'sweb_estudiantes_reg_observ_gen',
+    'reg_obs_gen'           => 'sweb_estudiantes_reg_observ_gen',
+    
+    'exam_admision'         => 'sweb_exam_admision_gdocs',
+
+    'grados'                => 'sweb_grados',
+    'grado'                 => 'sweb_grados',
+
+    'grados_asignat'        => 'sweb_grado_asignat',
+    'indicadores'           => 'sweb_indicadores',
+    'menus'                 => 'snxt_menus',
+    'notas'                 => 'sweb_notas',
+    'periodos'              => 'sweb_periodos',
+    'rangos'                => 'sweb_rangos',
+    
+    'salones'               => 'sweb_salones',
+    'salon'                 => 'sweb_salones',
+    
+    'salon_asignat_profe'   => 'sweb_salon_asignat_profesor',
+    'sap'                   => 'sweb_salon_asignat_profesor',
+    
+    'salones'               => 'sweb_salones',
+    'salones_asignat'       => 'sweb_salon_asignat',
+    
+    'secciones'             => 'sweb_secciones',
+    'seccion'                   => 'sweb_secciones',
+
+    'user'                  => 'dm_user',
+    'user_estudiantes'      => 'sweb_dmuser_estudiantes',
+    'users_asignaturas'     => 'sweb_users_asignaturas',
+    'users_secciones'       => 'sweb_users_secciones',
+    
 ];

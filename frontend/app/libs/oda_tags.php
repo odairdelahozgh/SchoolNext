e316d1396bba696b9b42565332c68ec28bb314c7
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2022-09-16 05:06:23 -0500

    Create oda_tags.php

---
+++ frontend/app/libs/oda_tags.php
@@ -0,0 +1,95 @@
+<?php
+
+class OdaTags {
+
+   // HELPERS PARA TABLAS
+   public static function multiTags( $tag='span', $data=array(), $attrs='' ) {
+      if (is_array($attrs)) { $attrs = self::getAttrs($attrs); }
+      $tags='';
+      foreach ($data as $value) { $tags .="<$tag $attrs>".$value."</$tag>"; }
+      return $tags;
+   } // END-multiTags
+
+   // HELPERS PARA LINKS    
+   public static function getAttrs($params) {
+       if (!is_array($params)) {
+           return (string)$params;
+       }
+       $data = '';
+       foreach ($params as $k => $v) {
+           $data .= "$k=\"$v\" ";
+       }
+       return trim($data);
+   } // END-getAttrs
+
+   public static function link($action='', $text='Inicio', $attrs = '') {
+      if (is_array($attrs)) {
+         $attrs = self::getAttrs($attrs);
+      }
+      return '<a href="'.PUBLIC_PATH."$action\" $attrs>$text</a>";
+   } // END-link
+   
+   public static function linkBarItem($action, $text, $icon='home') { 
+      return '<span class="w3-bar-item w3-hover-blue">'
+                ._Icons::solid($icon, "w3-large").'&nbsp;'
+                .self::link($action, $text)
+            .'</span>';
+    } // END-linkBarItem
+
+    public static function linkExterno($url, $text, $attrs = '') {
+      if (is_array($attrs)) { $attrs = self::getAttrs($attrs); }
+      return "<a href=\"$url\" $attrs  target=\"_blank\">$text</a>";
+    } // END-linkExterno
+
+    //====================
+    public static function linkButton($action, $text, $icon='', $attrs = 'class="w3-button w3-green"') { 
+      if (is_array($attrs)) { $attrs = self::getAttrs($attrs); }
+      return self::link($action, _Icons::solid($icon).'&nbsp;'.$text, $attrs);
+    } // END-linkButton
+
+    // ===================
+    public static function linkIcon($action='', $icon='', $attr='') {
+      return self::link($action, _Icons::solid($icon), $attr).'&nbsp;';
+    } // END-linkIcon
+
+    public static function linkBC($action, $text) { 
+      return '&nbsp;'._Icons::solid('angles-right', "w3-small").'&nbsp;'
+            .self::link($action, $text, "title=\"Volver a $text\"");
+    } // END-linkBC
+    
+    //====================
+    public static function img($src, $alt = '', $attrs = '', $err_message='no image') {
+      if (!file_exists(ABS_PUBLIC_PATH."/img/$src")) {
+         return $err_message;
+      }
+        return '<img src="'.PUBLIC_PATH."img/$src\" alt=\"$alt\" ".Tag::getAttrs($attrs).'/>';
+    } // END-img
+    
+    //====================
+    public static function fileimg($src, $alt = '', $attrs = '', $err_message='no image') {
+      if (!file_exists(ABS_PUBLIC_PATH."/files/upload/$src")) {
+         return $err_message;
+      }
+        return '<img src="'.PUBLIC_PATH."files/upload/$src\" alt=\"$alt\" ".Tag::getAttrs($attrs).'/>';
+    } // END-fileimg
+
+
+   // HELPERS PARA LISTS
+   public static function list($array, $type = 'ul', $attrs = 'class="w3-ul w3-card-4 w3-hoverable"') {
+      if (is_array($attrs)) { $attrs = self::getAttrs($attrs); }
+      $list = "<$type $attrs>".PHP_EOL;
+      foreach ($array as $item) { $list .= "<li>$item</li>".PHP_EOL; }
+      return "$list</$type>".PHP_EOL;
+   }
+
+   public static function meter($nivel, $titulo='', $rango=array(33,66,80)){
+      return "<label>$titulo:
+         <meter
+               min=\"0\" max=\"100\"
+               low=\"$rango[0]\" high=\"$rango[1]\" optimum=\"$rango[2]\"
+               value=\"$nivel\">
+            $nivel/100
+         </meter>
+      </label>";
+   }
+} // END-_Tag

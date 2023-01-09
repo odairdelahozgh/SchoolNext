642863dd806722367b4200f5f2ef80e71d4adff0
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-09 08:00:24 -0500

    Create oda_pdf.php

---
+++ frontend/app/libs/oda_pdf.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * KumbiaPHP web & app Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.
+ *
+ * @category   Kumbia
+ * @package    Core
+ *
+ * @copyright  Copyright (c) 2005 - 2021 KumbiaPHP Team (http://www.kumbiaphp.com)
+ * @license    https://github.com/KumbiaPHP/KumbiaPHP/blob/master/LICENSE   New BSD License
+ */
+
+/**
+ * Utilidades para el manejo de ficheros y directorios
+ * @category   Kumbia
+ * @package    Core
+ */
+
+ 
+require(VENDOR_PATH.'/fpdf/fpdf.php');
+
+class OdaPdf extends fpdf
+{
+
+} // END-CLASS-OdaPdf

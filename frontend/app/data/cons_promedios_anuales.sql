91093a3d52b5f97351726c539e55765cffda2fd4
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-09 08:01:25 -0500

    Update cons_promedios_anuales.sql

--- frontend/app/data/cons_promedios_anuales.sql
+++ frontend/app/data/cons_promedios_anuales.sql
@@ -9,7 +9,7 @@ LEFT JOIN sweb_estudiantes AS E ON N.estudiante_id = E.id
 LEFT JOIN sweb_asignaturas AS A ON N.asignatura_id = A.id
 LEFT JOIN sweb_salones     AS S ON N.salon_id = S.id
 
-WHERE N.periodo_id =5 AND N.grado_id <= 11
+WHERE N.periodo_id=5 AND N.grado_id<=11
 
 GROUP BY N.annio, N.periodo_id, N.salon_id, N.estudiante_id
 ORDER BY N.salon_id, promedio DESC
@@ -17,7 +17,10 @@ ORDER BY N.salon_id, promedio DESC
 /*
 cons_prom_general_2021_5periodo
 
-$sql = "SELECT N.annio, N.periodo_id, S.nombre AS salon, CONCAT(TRIM(E.nombres), \' \', E.apellido1, \' \', E.apellido2) as estudiante, avg(N.nota_final) as promedio\n"
+$sql = "SELECT N.annio, 
+N.periodo_id, S.nombre AS salon, 
+CONCAT(TRIM(E.nombres), \' \', E.apellido1, \' \', E.apellido2) as estudiante, 
+avg(N.nota_final) as promedio\n"
     . "FROM sweb_notas_2021 AS N \n"
     . "LEFT JOIN sweb_estudiantes AS E ON N.estudiante_id = E.id\n"
     . "LEFT JOIN sweb_asignaturas AS A ON N.asignatura_id = A.id\n"

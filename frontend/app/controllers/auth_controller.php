e5fb2e07bbc9d9c1be29a1e654ed9881a9443d54
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-14 08:07:50 -0500

    Update auth_controller.php

--- frontend/app/controllers/auth_controller.php
+++ frontend/app/controllers/auth_controller.php
@@ -14,14 +14,14 @@ class AuthController extends AdminController
 
     public function login() {
         $this->page_title = 'Login';
-        $this->colegio_nombre   = Config::get('institucion.nombre');
-        $this->soporte_telefono = Config::get('construxzion.telefono');
-        $this->soporte_whatsapp = Config::get('construxzion.whatsapp');
-        $this->soporte_email    = Config::get('construxzion.email');
-        $this->sw_house_name    = Config::get('construxzion.name');
-        $this->sw_house_twitter = Config::get('construxzion.twitter');
-        $this->copy_right       = Config::get('construxzion.copy');
-        $this->copy_text        = Config::get('construxzion.copy_text');
+        $this->colegio_nombre   = Config::get('config.institution.nombre');
+        $this->soporte_telefono = Config::get('config.construxzion.telefono');
+        $this->soporte_whatsapp = Config::get('config.construxzion.whatsapp');
+        $this->soporte_email    = Config::get('config.construxzion.email');
+        $this->sw_house_name    = Config::get('config.construxzion.name');
+        $this->sw_house_twitter = Config::get('config.construxzion.twitter');
+        $this->copy_right       = Config::get('config.construxzion.copy');
+        $this->copy_text        = Config::get('config.construxzion.copy_text');
         View::template('login');
         if(Load::model('user')->login()) { Redirect::to('index'); }
     } 

2c7f3727e247a74d187c0a53bb113f2c51e524eb
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-14 11:30:19 -0500

    Update periodo.php

--- frontend/app/models/periodo.php
+++ frontend/app/models/periodo.php
@@ -20,7 +20,7 @@ class Periodo extends LiteRecord
   
   //=============
   public function getPeriodoActual($periodo=null) {
-    $pk = (!is_null($periodo)) ? $periodo : Config::get('academico.periodo_actual');
+    $pk = (!is_null($periodo)) ? $periodo : Config::get('config.academic.periodo_actual');
     return $this->get($pk);
   } //END-getPeriodoActual
   

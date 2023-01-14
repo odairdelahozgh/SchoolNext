798605824160ed1373de85149fd6c7e1cc2d525c
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-14 13:36:31 -0500

    Update ejemplo.php

--- frontend/app/models/ejemplo.php
+++ frontend/app/models/ejemplo.php
@@ -15,30 +15,30 @@
   // ::class,  ::getTable(), ::getDriver()
   
 
-  // crear registro:               ->create(array $data = []): bool {}
-  // actualizar registro:          ->update(array $data = []): bool {}
-  // Guardar registro:             ->save(array $data = []): bool {}
-  // Eliminar registro por pk:     ::delete($pk): bool
+  // crear registro:         ->create(array $data = []): bool {}
+  // actualizar registro:      ->update(array $data = []): bool {}
+  // Guardar registro:       ->save(array $data = []): bool {}
+  // Eliminar registro por pk:   ::delete($pk): bool
   //
-  // Averigua si existe registro          ::exists($pk): bool
-  // Buscar por clave pk:                 ::get($pk, $fields = '*')
-  // Todos los registros:                 ::all(string $sql = '', array $values = []): array {}
+  // Averigua si existe registro      ::exists($pk): bool
+  // Buscar por clave pk:         ::get($pk, $fields = '*')
+  // Todos los registros:         ::all(string $sql = '', array $values = []): array {}
   // Primer registro de la consulta sql:  ::first(string $sql, array $values = [])//: static in php 8
-  // Filtra las consultas                 ::filter(string $sql, array $values = []): array
+  // Filtra las consultas         ::filter(string $sql, array $values = []): array
 
 
 class Ejemplo extends LiteRecord
 {
-    protected static $table = 'sweb_ejemplo';
-    
-    const TIPO = [
-        1 => 'Apartamento',
-        2 => "Estudio"
-    ];
+  protected static $table = 'sweb_ejemplo';
+  
+  const TIPO = [
+    1 => 'Apartamento',
+    2 => "Estudio"
+  ];
 
-    public function __toString() { return $this->nombre; }
+  public function __toString() { return $this->nombre; }
 
-    
+  
 	public function paginateCustomers(int $page = 1, int $perPage = 50): Paginator
 	{
 		$sql = 'SELECT * FROM ' . self::getSource() . ' WHERE status = ?';

52e690849300e5f6d681c04c67475beea0b33d51
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-21 01:25:20 -0500

    Update index.php

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -3,20 +3,22 @@ setlocale(LC_ALL, 'es_ES');
 ini_set('date.timezone', 'America/Bogota');
 const APP_CHARSET = 'UTF-8';
 
-
 /*
 // =======================
 // SERVIDOR - Bluehost Odair
-const PRODUCTION = false;
-error_reporting(E_ALL ^ E_STRICT);ini_set('display_errors', 'On');
+const PRODUCTION = true;
+//error_reporting(E_ALL ^ E_STRICT);
+//ini_set('display_errors', 'On');
 const APP_ROOT_PRIVATE = '/home2/dwsjrnmy/_windsor_schoolnext_private/';
 const APP_PATH         = APP_ROOT_PRIVATE.'frontend/app/';
 const CORE_PATH        = APP_ROOT_PRIVATE.'core/';
 const PUBLIC_PATH      = 'https://windsor.schoolnext.space/';
+
+$url = $_SERVER['PATH_INFO'] ?? '/';
+
 const VENDOR_PATH      = APP_ROOT_PRIVATE.'vendor/';
 const ABS_PUBLIC_PATH  = '/home2/dwsjrnmy/public_html/windsor_schoolnext_public';
-// END - SERVIDOR
-// =======================
+const SCHOOLWEB_PUBLIC_PATH = 'https://windsortemp.schoolnext.space/';
 */
 
 /*
@@ -27,11 +29,12 @@ const PRODUCTION = true;
 const APP_ROOT_PRIVATE = '/home/windsor1/schoolnext_private/';
 const APP_PATH         = APP_ROOT_PRIVATE.'frontend/app/';
 const CORE_PATH        = APP_ROOT_PRIVATE.'core/';
-const VENDOR_PATH      = APP_ROOT_PRIVATE.'vendor/';
 const PUBLIC_PATH     = 'https://schoolnext.windsorschool.edu.co/';
+$url = $_SERVER['ORIG_PATH_INFO'] ?? '/';
+
+const VENDOR_PATH      = APP_ROOT_PRIVATE.'vendor/';
 const ABS_PUBLIC_PATH = '/home/windsor1/schoolnext.windsorschool.edu.co/';
-// END - SERVIDOR
-// =======================
+const SCHOOLWEB_PUBLIC_PATH = 'https://schoolweb.windsorschool.edu.co/';
 */
 
 
@@ -42,25 +45,17 @@ const PRODUCTION = true;
 define('APP_PATH', dirname(__DIR__).'/app/');
 define('CORE_PATH', dirname(dirname(APP_PATH)).'/core/');
 define('PUBLIC_PATH', substr($_SERVER['SCRIPT_NAME'], 0, -9)); // - index.php string[9]
+$url = $_SERVER['PATH_INFO'] ?? '/';
+
+define('VENDOR_PATH', APP_PATH.'../vendor/');
 const ABS_PUBLIC_PATH = 'D:\schoolnext\frontend\public';
-// END - DESARROLLO LOCAL
-// =======================
+const SCHOOLWEB_PUBLIC_PATH = 'D:\windsortemp\web';
 
 
-$url = $_SERVER['PATH_INFO'] ?? '/';  // OJO ::::: ORIG_PATH_INFO en hosting de Rafa
-
-
-// ODAIR
 const APP_NAME= "SchoolNEXT>>";
 const FILE_UPLOAD_PATH   = PUBLIC_PATH.'files/upload/';
 const FILE_DOWNLOAD_PATH = PUBLIC_PATH.'files/download/';
 const IMG_UPLOAD_PATH    = PUBLIC_PATH.'img/upload/';
 const IMG_DOWNLOAD_PATH  = PUBLIC_PATH.'img/download/';
-
-const SCHOOLWEB_URL_PROD  = 'https://windsortemp.schoolnext.space/';
-const SCHOOLNEXT_URL_PROD = 'https://windsor.schoolnext.space/';
-
-const SCHOOLWEB_URL_TEST  = 'https://schoolweb.windsorschool.edu.co/';
-const SCHOOLNEXT_URL_TEST = 'https://schoolnext.windsorschool.edu.co/';
 
 require CORE_PATH.'kumbia/bootstrap.php'; //bootstrap del core

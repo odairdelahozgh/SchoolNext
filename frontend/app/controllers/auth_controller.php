e5e3cad4fd63de7bfc0392547495c57ec8e6b872
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-21 00:21:01 -0500

    Update auth_controller.php

--- frontend/app/controllers/auth_controller.php
+++ frontend/app/controllers/auth_controller.php
@@ -14,21 +14,13 @@ class AuthController extends AdminController
 
     public function login() {
         $this->page_title = 'Login';
-        $this->colegio_nombre   = Config::get('config.institution.nombre');
-        $this->soporte_telefono = Config::get('config.construxzion.telefono');
-        $this->soporte_whatsapp = Config::get('config.construxzion.whatsapp');
-        $this->soporte_email    = Config::get('config.construxzion.email');
-        $this->sw_house_name    = Config::get('config.construxzion.name');
-        $this->sw_house_twitter = Config::get('config.construxzion.twitter');
-        $this->copy_right       = Config::get('config.construxzion.copy');
-        $this->copy_text        = Config::get('config.construxzion.copy_text');
         View::template('login');
-        if(Load::model('user')->login()) { Redirect::to('index'); }
+        if((new User)->login()) { Redirect::to('index'); }
     } 
 
     public function logout() {
-        Load::model('user')->logout();
-        Redirect::toAction('login');
+      (new User)->logout();
+      Redirect::toAction('login');
     }
 
 }

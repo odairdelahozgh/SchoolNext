994791067e99d997750ea5a7c7d9084e12d3d8f3
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-24 05:20:58 -0500

    Update secretaria_controller.php

--- frontend/app/controllers/secretaria_controller.php
+++ frontend/app/controllers/secretaria_controller.php
@@ -7,81 +7,90 @@
   */
 class SecretariaController extends AppController
 {    
-    protected function before_filter() {
-      $this->theme = Session::get('theme');
-    }
 
     public function index() {
       $this->page_action = 'M&oacute;dulo Secretar&iacute;a';
     }
     
     public function listadoEstudActivos() {
-      $this->page_action = 'Estudiantes Activos';
+      $this->page_action = 'Listado de Estudiantes Activos';
       $this->data = (new Estudiante)->getListActivos();
-      //$this->theme = Session::get('theme');
       View::select('estudiantes/estud_list_activos');
     } // END-listadoEstudActivos
     
     public function listadoEstudInactivos() {
-      $this->page_action = 'Estudiantes Inactivos';
+      $this->page_action = 'Listado de Estudiantes Inactivos';
       $this->data = (new Estudiante)->getListInactivos();
-      $this->num_regs = Count($this->data);
-      //$this->theme = Session::get('theme');
       View::select('estudiantes/estud_list_inactivos');
     } // END-listadoEstudInactivos
     
-    public function actualizarPago($estudiante_id) {
-      (new Estudiante)->setActualizarPago((int)$estudiante_id);
+    
+    /**
+     * Actualizar Mes Pagado de un Estudiante
+     */
+    public function actualizarPago(int $estudiante_id) {
+      $this->page_action = 'Actualizar Mes Pagado Estudiante';
+      $Estud = (new Estudiante)->get($estudiante_id);
+      if ($Estud->setActualizarPago($estudiante_id)) {
+        OdaFlash::valid(msg: "$this->page_action: $Estud");
+      } else {
+        OdaFlash::error(msg: "$this->page_action: $Estud", audit: true);
+      }
       Redirect::toAction('listadoEstudActivos');
     } // END-actualizarPago
     
-    
-    public function activarEstudiante($estudiante_id) {
-      (new Estudiante)->setActivar((int)$estudiante_id);
+    /**
+     * Activar un Estudiante
+     */
+    public function activarEstudiante(int $estudiante_id) {
+      $this->page_action = 'Activar Estudiante';
+      $Estud = (new Estudiante)->get($estudiante_id);
+      if ($Estud->setActivar()) {
+        OdaFlash::valid(msg: "$this->page_action: $Estud");
+      } else {
+        OdaFlash::error(msg: "$this->page_action: $Estud", audit: true);
+      }
       Redirect::toAction('listadoEstudInactivos');
     } // END-activarEstudiante
     
 
     /**
-     * Editar un Registro de Estudiante
-     * @param int $id (requerido)
+     * Editar un Registro de Estudiante: estudiantes/estud_edit_activos
      */
     public function editarEstudActivos(int $id) {
-      $this->page_action = 'Editar un Registro de Estudiante';
+      $this->page_action = 'Editar Registro de Estudiante';
       $obj_estudiante = new Estudiante;
       if (Input::hasPost('estudiante')) { // se verifica si se ha enviado el formulario (submit)
           if ($obj_estudiante->update(Input::post('estudiante'))) {
-            OdaFlash::valid('Operaci&oacute;n exitosa [editar registro]');
+            OdaFlash::valid(msg: "$this->page_action: $obj_estudiante");
               return Redirect::to(); // enrutando por defecto al index del controller
           }
-          OdaFlash::error("Fall&oacute; Operaci&oacute;n [editar registro estudiante id=$id]", true);
+          OdaFlash::error(msg: "$this->page_action: $obj_estudiante", audit: true);
           return;
       }
       // Aplicando la autocarga de objeto, para comenzar la edici&oacute;n
-      $this->estudiante = $obj_estudiante->get((int)$id);
+      $this->estudiante = $obj_estudiante->get($id);
       View::select('estudiantes/estud_edit_activos');
     } // END-editarEstudActivos
 
 
   /**
-   * Cambiar de salon a un estudiante
-   * @param int $estudiante_id (requerido)
-   * @param int $salon_id (requerido)
-   * @param int $audit (requerido)
+   * Cambiar de salon a un estudiante: /secre-estud-list-activos
    */
   public function cambiar_salon_estudiante(int $estudiante_id, int $salon_id, bool $cambiar_en_notas = true) {
-    $Estud = (new Estudiante)::first("SELECT * FROM sweb_estudiantes WHERE id=?", [(int)$estudiante_id]);
-    if ( $Estud->setCambiarSalon((int)$salon_id, $cambiar_en_notas) ) {
-      OdaFlash::valid("Operación exitosa [Cambiar salón] $Estud", true);
+    $this->page_action = 'Cambiar Sal&oacute;n Estudiante';
+    $Estud = (new Estudiante)::first("SELECT * FROM sweb_estudiantes WHERE id=?", [$estudiante_id]);
+    if ( $Estud->setCambiarSalon($salon_id, $cambiar_en_notas) ) {
+      OdaFlash::valid(msg: "$this->page_action: $Estud", audit: true);
     } else {
-      OdaFlash::error("Falló Operación [Cambiar salón]", true);
+      OdaFlash::error(msg: "$this->page_action: $Estud", audit: true);
     }
     return Redirect::to('/secre-estud-list-activos');
   }
 
 
   /**
-   * Index a Historico notas
+   * Index a Historico notas: historico_notas/index
    */
   public function historico_notas() {
     $this->page_action = 'Hist&oacute;rico de Notas';

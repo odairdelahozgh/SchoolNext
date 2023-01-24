f295ff8bace3aecb95f1f77125cd686e06285b94
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-24 05:19:07 -0500

    Create salones_controller.php

---
+++ frontend/app/controllers/admin/salones_controller.php
@@ -0,0 +1,84 @@
+<?php
+/**
+  * Controlador Salones  
+  * @category App
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  */
+  
+class SalonesController extends AppController
+{
+
+  public function index() {
+    Redirect::toAction('list');
+  }
+
+  public function list() {
+    $this->page_action = 'Lista de Salones';
+    //$this->breadcrumb->addCrumb(title: 'Salones');
+    $this->data = (new Salon)->getList();
+  }
+
+  /**
+   * Crea un Registro
+   */
+  public function create() {
+    $this->page_action = 'CREAR Registro';
+    $Grado = new Grado();
+    $this->grados  = (new Grado)->getListActivos('id, nombre');
+
+    if (Input::hasPost('grados')) {
+/*       $validador = new Validate(Input::post('grados.nombre'), $this->reglas() );
+      if (!$validador->exec()) {
+        $validador->flash();
+        //OdaFlash::error('Falló Operación VALIDAR :: Crear Registro');
+        return false;
+      } */
+      if ( $Grado->create(Input::post('grados'))) {
+        OdaFlash::valid('Operación exitosa :: Crear Registro');
+        Input::delete();
+        return Redirect::to(); // al index del controller
+      }
+      OdaFlash::error('Falló Operación :: Crear Registro');
+    }
+  }
+   
+  /**
+   * Edita un Registro
+   *
+   * @param int $id (requerido)
+   */
+  public function edit($id=0) {
+    $this->page_action = 'Editar Registro';
+    $Salon = new Salon();
+    //$this->secciones = (new Seccion)->getListActivos('id, nombre');
+    //$this->salones   = (new Salon)->getListActivos('id, nombre');
+    //$this->grados  = (new Grado)->getListActivos('id, nombre');
+    
+    if (Input::hasPost('salones')) {
+      if ($Salon->update(Input::post('salones'))) {
+        OdaFlash::valid("$this->page_action $Salon");
+        return Redirect::to(); // al index del controller
+      }
+      OdaFlash::error('Editar Registro');
+      return; // Redirect::to('?');
+    }
+    $this->Modelo = $Salon->get((int) $id);
+  }
+
+  /**
+   * Eliminar un registro
+   *
+   * @param int $uuid (requerido)
+   */
+  public function del(string $uuid) {
+    $this->page_action = 'Eliminar Registro';
+    $Grado = (new Grado)->get_uuid($uuid);
+    if ($Grado::deleteID($Grado->id)) {
+      OdaFlash::valid("$this->page_action: $Grado");
+    } else {
+      OdaFlash::error("$this->page_action: $Grado");
+    }
+    return Redirect::to();
+  }
+
+} // END CLASS

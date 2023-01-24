94ff912dac7a8874d175e72dfde3fbcbe7980dd3
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-24 05:19:03 -0500

    Update grados_controller.php

--- frontend/app/controllers/admin/grados_controller.php
+++ frontend/app/controllers/admin/grados_controller.php
@@ -8,40 +8,39 @@
 class GradosController extends AppController
 {
 
-    public function index() {
-        Redirect::toAction('list');
-    }
+  public function index() {
+    Redirect::toAction('list');
+  }
 
-    public function list() {
-        $this->page_action = 'Lista de Grados';
-        $this->breadcrumb->addCrumb(title:'Grados');
-        $this->data = (new Grado)->getListSeccion();
-        $this->tot_regs = count($this->data);
-    }
-    
-    
+  public function list() {
+    $this->page_action = 'Lista de Grados';
+    //$this->breadcrumb->addCrumb(title:'Grados');
+    $this->data = (new Grado)->getListSeccion();
+  }
+  
+  
 	private function reglas() {
 	
 		return array(
-            'grados.abrev' => [
-                'required' => ['error' => 'El nombre es obligatorio.'],
-            ],
-            /*
+      'grados.abrev' => [
+        'required' => ['error' => 'El nombre es obligatorio.'],
+      ],
+      /*
 			'NombreCompleto' => [
 				'required' => ['error' => 'Indique su nombre.'],
-				'alpha'    => ['error' => 'Nombre incompleto o incorrecto.']
+				'alpha'  => ['error' => 'Nombre incompleto o incorrecto.']
 			],
 			'Email' => [
 				'required' => ['error' => 'Indique su email.'],
-				'email'    => ['error' => 'Email incorrecto.']
+				'email'  => ['error' => 'Email incorrecto.']
 			],
 			'Movil' => [
 				'required' => ['error' => 'Indique su teléfono / móvil.'],
 				'length'   => ['min' => '9',
-                                'max' => '17',
-                                'error' => 'Teléfono / móvil incorrecto'],
+                'max' => '17',
+                'error' => 'Teléfono / móvil incorrecto'],
 				'pattern'  => ['regexp' => '/^\+(?:[0-9] ?){6,14}[0-9]$/', 
-                                'error'  => 'Teléfono incorrecto. Formato ejemplo. +34 862929929']
+                'error'  => 'Teléfono incorrecto. Formato ejemplo. +34 862929929']
 			],
 			'Asunto' => [
 				'required' => ['error' => 'Indique un asunto.'],
@@ -49,89 +48,91 @@ class GradosController extends AppController
 			/'Mensaje' => [
 				'required' => ['error' => 'Indique un mensaje.'],
 				'length'   => ['min' => 100,
-                                'error' => 'Si es posible, concrete más en su mensaje.'],
+                'error' => 'Si es posible, concrete más en su mensaje.'],
 			]
-            */
+      */
 		);
 
 	}
 
 
-    public function enviar($datos) {
-        $validador = new Validate($datos, $this->reglas() );
-        if (!$validador->exec()) {
-                $validador->flash();
-                return false;
-        }
-        // proseguir con el proceso
-    }
-    /*$validador = new Validate(Input::post('grados.nombre'), $this->reglas() );
+  public function enviar($datos) {
+    $validador = new Validate($datos, $this->reglas() );
     if (!$validador->exec()) {
         $validador->flash();
         return false;
-    }*/
-
-    /**
-     * Crea un Registro
-     */
-    public function create() {
-        $this->page_action = 'CREAR Registro';
-        $Grado = new Grado(true);
-        $this->secciones = (new Seccion)->getListActivos('id, nombre');
-        $this->salones   = (new Salon)->getListActivos('id, nombre');
-        $this->grados    = (new Grado)->getListActivos('id, nombre');
-
-        if (Input::hasPost('grados')) {
-/*             $validador = new Validate(Input::post('grados.nombre'), $this->reglas() );
-            if (!$validador->exec()) {
-                $validador->flash();
-                //OdaFlash::error('Falló Operación VALIDAR :: Crear Registro');
-                return false;
-            } */
-            if ( $Grado->create(Input::post('grados'))) {
-                OdaFlash::valid('Operación exitosa :: Crear Registro');
-                Input::delete();
-                return Redirect::to(); // al index del controller
-            }
-            OdaFlash::error('Falló Operación :: Crear Registro');
-        }
     }
+    // proseguir con el proceso
+  }
+  /*$validador = new Validate(Input::post('grados.nombre'), $this->reglas() );
+  if (!$validador->exec()) {
+    $validador->flash();
+    return false;
+  }*/
+
+  /**
+   * Crea un Registro
+   */
+  public function create() {
+    $this->page_action = 'CREAR Registro';
+    $Grado = new Grado(true);
+    $this->secciones = (new Seccion)->getListActivos('id, nombre');
+    $this->salones   = (new Salon)->getListActivos('id, nombre');
+    $this->grados  = (new Grado)->getListActivos('id, nombre');
+
+    if (Input::hasPost('grados')) {
+/*       $validador = new Validate(Input::post('grados.nombre'), $this->reglas() );
+      if (!$validador->exec()) {
+        $validador->flash();
+        //OdaFlash::error('Falló Operación VALIDAR :: Crear Registro');
+        return false;
+      } */
+      if ( $Grado->create(Input::post('grados'))) {
+        OdaFlash::valid('Operación exitosa :: Crear Registro');
+        Input::delete();
+        return Redirect::to(); // al index del controller
+      }
+      OdaFlash::error('Falló Operación :: Crear Registro');
+    }
+  }
    
   /**
-     * Edita un Registro
-     *
-     * @param int $id (requerido)
-     */
-    public function edit($id=0) {
-        $this->page_action = 'EDITAR un Registro';
-        $Grado = new Grado();
-        $this->secciones = (new Seccion)->getListActivos('id, nombre');
-        $this->salones   = (new Salon)->getListActivos('id, nombre');
-        $this->grados    = (new Grado)->getListActivos('id, nombre');
-        
-        if (Input::hasPost('grados')) {
-            if ($Grado->update(Input::post('grados'))) {
-                OdaFlash::valid('Operación exitosa: Se guardó el registro');
-                return Redirect::to(); // al index del controller
-            }
-            OdaFlash::error('Falló Operación: Editar Registro');
-            return; // Redirect::to('?');
-        }
-        $this->Modelo = $Grado->get((int) $id);
+   * Edita un Registro
+   *
+   * @param int $id (requerido)
+   */
+  public function edit($id=0) {
+    $this->page_action = 'Editar Registro';
+    $Grado = new Grado();
+    $this->secciones = (new Seccion)->getListActivos('id, nombre');
+    $this->salones   = (new Salon)->getListActivos('id, nombre');
+    $this->grados  = (new Grado)->getListActivos('id, nombre');
+    
+    if (Input::hasPost('grados')) {
+      if ($Grado->update(Input::post('grados'))) {
+        OdaFlash::valid("$this->page_action $Grado");
+        return Redirect::to(); // al index del controller
+      }
+      OdaFlash::error('Editar Registro');
+      return; // Redirect::to('?');
     }
+    $this->Modelo = $Grado->get((int) $id);
+  }
 
-    /**
-     * Eliminar un registro
-     *
-     * @param int $id (requerido)
-     */
-    public function del($uuid) {
-        if ((new Grado)->deleteUUID($uuid)) {
-            OdaFlash::valid('Operación exitosa: Se eliminó el registro');
-        } else {
-            OdaFlash::error('Falló Operación: eliminar registro');
-        }
-        return Redirect::to();
+  /**
+   * Eliminar un registro
+   *
+   * @param int $uuid (requerido)
+   */
+  public function del(string $uuid) {
+    $this->page_action = 'Eliminar Registro';
+    $Grado = (new Grado)->get_uuid($uuid);
+    if ($Grado::deleteID($Grado->id)) {
+      OdaFlash::valid("$this->page_action: $Grado");
+    } else {
+      OdaFlash::error("$this->page_action: $Grado");
     }
+    return Redirect::to();
+  }
 
 } // END CLASS

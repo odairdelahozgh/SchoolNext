b242f3f58784736d64d5eaf0d839df6d38c7dd61
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-24 05:35:30 -0500

    Create trait_uuid.php

---
+++ frontend/app/models/trait_uuid.php
@@ -0,0 +1,55 @@
+<?php
+// UUID: Universally Unique IDentifier
+trait TraitUuid {
+
+  /**
+   * UUID Generator Optimized
+   */
+  public function UUIDReal(int $lenght=20):string {
+    if ($lenght <= parent::$lim_tam_campo_uuid) {
+      if (function_exists("random_bytes")) {
+        $bytes = random_bytes(ceil($lenght / 2));
+      } elseif (function_exists("openssl_random_pseudo_bytes")) {
+        $bytes = openssl_random_pseudo_bytes(ceil($lenght / 2));
+      } else {
+        throw new Exception("no cryptographically secure random function available");
+      }
+    } else {
+      throw new Exception("lenght must be <= 36");
+    }
+    return substr(bin2hex($bytes), 0, $lenght);
+  }//END-UUIDReal
+  
+  
+  /**
+   * Devuelve un Registro por UUID.
+   */
+  public static function get_uuid(string $uuid, string $fields = '*') {
+    $sql = "SELECT $fields FROM ".static::getSource().' WHERE uuid = ?';
+    return static::query($sql, [$uuid])->fetch();
+  }
+  
+
+  /**
+   * Elimina un registro por UUID.
+   */
+  public static function deleteUUID(string $uuid): bool {
+    $source  = static::getSource();
+    return static::query("DELETE FROM $source WHERE uuid = ?", [$uuid])->rowCount() > 0;
+  } //END-deleteUUID
+  
+
+  
+  /**
+   * Rellena el campo UUID de todos los registros del modelo
+   */
+  public function setUUID_All_ojo($long=20) {
+    $Todos = $this::all();
+    foreach ($Todos as $reg) {
+      $reg->uuid = $this->UUIDReal($long);
+      $reg->update();
+    }
+  } //END-setUUID_All_ojo
+  
+
+} //END-TRAIT

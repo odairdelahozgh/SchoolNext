60f5a52df4d474f361bf16cc74438d73acf52a94
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-24 05:58:24 -0500

    Update nota_t_props.php

--- frontend/app/models/nota_t_props.php
+++ frontend/app/models/nota_t_props.php
@@ -1,7 +1,5 @@
 <?php
 
 trait NotaTProps {
-    public function __toString() { return $this->id; }
-    public function getIsActiveF() { return (($this->is_active) ? _Icons::solid('face-smile', 'w3-small') : _Icons::solid('face-frown', 'w3-small') ); }
     public function getfoto() { return IMG_UPLOAD_PATH.'/estudiantes/'.$this->id.'.png'; }
 }

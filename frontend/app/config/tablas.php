dfddc550662f68053b2a1aeb5f76c31d46b338ff
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-29 23:31:29 -0500

    Update config app

--- frontend/app/config/tablas.php
+++ frontend/app/config/tablas.php
@@ -3,60 +3,64 @@
  * KumbiaPHP Web Framework
  * Parámetros de configuracion de la aplicacion
  */
+
 return [
-    
-    'acciones_seguim'       => 'sweb_acciones_segto',
-    
-    'areas'                 => 'sweb_areas',
-    
-    'asign'                 => 'sweb_asignaturas',
-    'asignaturas'               => 'sweb_asignaturas',
-    
-    'aspirantes'            => 'sweb_aspirantes',
-    'aspirantes_adjuntos'   => 'sweb_aspir_adjuntos',
-    'aspirantes_psico'      => 'sweb_aspirantepsico',
-    
-    'calendario'            => 'snxt_calendario',
+  'acciones_seguim'     => 'sweb_acciones_segto',
+  'area'                => 'sweb_areas',
+  'areas'               => 'sweb_areas',
+  'asignatura'          => 'sweb_asignaturas',
+  'asignaturas'         => 'sweb_asignaturas',
+  'aspirante'           => 'sweb_aspirantes',
+  'aspirantes'          => 'sweb_aspirantes',
+  'aspirantes_adjuntos' => 'sweb_aspir_adjuntos',
+  'aspirantes_psico'    => 'sweb_aspirantepsico',
+  
+  'calendario'          => 'snxt_calendario',
+  
+  'datosestud_no'       => 'sweb_datosestud',
 
-    'estudiantes'           => 'sweb_estudiantes',
-    'estud'                     => 'sweb_estudiantes',
+  'estudiante'          => 'sweb_estudiantes',
+  'estudiantes'         => 'sweb_estudiantes',
+  'estud_adjuntos'      => 'sweb_estudiante_adjuntos',
+  'estud_adjuntos_dos'  => 'sweb_estudiante_adjuntos_dos',
+  'estud_ctas_x_cobrar' => 'sweb_estud_cpc',
+  'estud_reg_academ'    => 'sweb_estudiantes_reg_desacacom',
+  'estud_reg_obs_gen'   => 'sweb_estudiantes_reg_observ_gen',
+  'exam_admision'       => 'sweb_exam_admision_gdocs',
+  
+  'grado'               => 'sweb_grados',
+  'grados'              => 'sweb_grados',
+  'grados_asignat'      => 'sweb_grado_asignat',
+  
+  'indicador'           => 'sweb_indicadores',
+  'indicadores'         => 'sweb_indicadores',
+  
+  'menu'                => 'snxt_menus',
+  'menus'               => 'snxt_menus',
 
-    'datosestud_no'         => 'sweb_datosestud',
-    'estud_adjuntos'        => 'sweb_estudiante_adjuntos',
-    'estud_adjuntos_dos'    => 'sweb_estudiante_adjuntos_dos',
-    'estud_ctas_x_cobrar'   => 'sweb_estud_cpc',
-    'estud_reg_academ'      => 'sweb_estudiantes_reg_desacacom',
-    
-    'estud_reg_obs_gen'     => 'sweb_estudiantes_reg_observ_gen',
-    'reg_obs_gen'           => 'sweb_estudiantes_reg_observ_gen',
-    
-    'exam_admision'         => 'sweb_exam_admision_gdocs',
+  'nota'                => 'sweb_notas',
+  'notas'               => 'sweb_notas',
 
-    'grados'                => 'sweb_grados',
-    'grado'                 => 'sweb_grados',
+  'periodo'               => 'sweb_periodos',
+  'periodos'              => 'sweb_periodos',
 
-    'grados_asignat'        => 'sweb_grado_asignat',
-    'indicadores'           => 'sweb_indicadores',
-    'menus'                 => 'snxt_menus',
-    'notas'                 => 'sweb_notas',
-    'periodos'              => 'sweb_periodos',
-    'rangos'                => 'sweb_rangos',
-    
-    'salones'               => 'sweb_salones',
-    'salon'                 => 'sweb_salones',
-    
-    'salon_asignat_profe'   => 'sweb_salon_asignat_profesor',
-    'sap'                   => 'sweb_salon_asignat_profesor',
-    
-    'salones'               => 'sweb_salones',
-    'salones_asignat'       => 'sweb_salon_asignat',
-    
-    'secciones'             => 'sweb_secciones',
-    'seccion'                   => 'sweb_secciones',
+  'rango'                 => 'sweb_rangos',
+  'rangos'                => 'sweb_rangos',
+  'reg_obs_gen'           => 'sweb_estudiantes_reg_observ_gen',
+  
+  'salon'                 => 'sweb_salones',
+  'salones'               => 'sweb_salones',
+  'sal_asig_prof'         => 'sweb_salon_asignat_profesor',
+  'salon_asignat_profe'   => 'sweb_salon_asignat_profesor',
+  'salones_asignat'       => 'sweb_salon_asignat',
+  
+  'seccion'               => 'sweb_secciones',
+  'secciones'             => 'sweb_secciones',
 
-    'user'                  => 'dm_user',
-    'user_estudiantes'      => 'sweb_dmuser_estudiantes',
-    'users_asignaturas'     => 'sweb_users_asignaturas',
-    'users_secciones'       => 'sweb_users_secciones',
-    
+  'usuario'               => 'dm_user',
+  'usuarios'              => 'dm_user',
+  'usuarios_estudiantes'  => 'sweb_dmuser_estudiantes',
+  'usuarios_asignaturas'  => 'sweb_users_asignaturas',
+  'usuarios_secciones'    => 'sweb_users_secciones',
+  
 ];

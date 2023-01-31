f723e584cc99cd8cc508f3b5354373beb4b9d0cb
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-01-31 08:46:02 -0500

    Create salones_controller.php

---
+++ frontend/app/controllers/api/salones_controller.php
@@ -0,0 +1,21 @@
+<?php
+/**
+  * Controlador API SALONES  
+  * @category API
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  * @author odairdelahoz@gmail.com
+  * @example http://username:password@URL/api/salones/all
+  */
+class SalonesController extends RestController
+{
+
+  /**
+   * Obtiene todos los registros de salones
+   * @link ../api/salones/all
+   */
+  public function get_all() {
+    $this->data = (new Salon)->getListActivos();
+  }//END-get_all
+
+
+} //END-CLASS

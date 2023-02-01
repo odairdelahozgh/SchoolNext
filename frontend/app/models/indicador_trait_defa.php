8e0a894d4cb64e8d910b433e14fdd2c12774d59a
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-01 04:10:10 -0500

    Update indicador_trait_defa.php

--- frontend/app/models/indicador_trait_defa.php
+++ frontend/app/models/indicador_trait_defa.php
@@ -3,22 +3,46 @@ trait IndicadorTraitDefa {
 
   public function getTLabels() {
     return array(
-      'is_active'    => 'Está Activo? ',
-      'created_at'   => 'Creado el: ',
-      'created_by'   => 'Creado por: ',
-      'updated_at'   => 'Actualizado el: ',
-      'updated_by'   => 'Actualizado por: ',
+      'annio'           => 'Año',
+      'periodo_id'      => 'Periodo',
+      'grado_id'        => 'Grado',
+      'asignatura_id'   => 'Asignatura',
+      'codigo'          => '<i class="fa-solid fa-bolt w3-small"></i> Código',
+      'concepto'        => '<i class="fa-solid fa-bolt w3-small"></i> Concepto',
+      'valorativo'      => 'Valorativo',
+      'is_visible'      => 'Visible en Calificar?',
+      'is_active'       => 'Está Activo? ',
+      'created_at'      => 'Creado el: ',
+      'created_by'      => 'Creado por: ',
+      'updated_at'      => 'Actualizado el: ',
+      'updated_by'      => 'Actualizado por: ',
     );
   }
 
   public function getTDefaults() {
     return array(
+      /*'annio'         => '',
+      'periodo_id'      => '',
+      'grado_id'        => '',
+      'asignatura_id'   => '',
+      'codigo'          => '',
+      'concepto'        => '',
+      'valorativo'      => '',
+      'is_visible'      => '',*/
       'is_active'    => 1,
     );
   }
 
   public function getTHelps() {
     return array(
+      'codigo'          => 'Min: 100  -  Max: 399 [Excepto Inglés]',
+      /*'annio'         => '',
+      'periodo_id'      => '',
+      'grado_id'        => '',
+      'asignatura_id'   => '',
+      'concepto'        => '',
+      'valorativo'      => '',
+      'is_visible'      => '',*/
       'is_active'    => 'Indica si está activo el registro.',
     );
   }
@@ -26,13 +50,33 @@ trait IndicadorTraitDefa {
   public function getTAttribs() {
     return array(
       'new' => [
-         'is_active'    => '',
-         'created_by'   => '',
-         'updated_by'   => '',
-         'created_at'   => '',
-         'updated_at'   => '',
+        'codigo'        => 'required inputmode="numeric" pattern="\d*',
+        'concepto'      => 'required',
+        /*
+        'periodo_id'      => '',
+        'annio'           => '',
+        'grado_id'        => '',
+        'asignatura_id'   => '',
+        'valorativo'      => '',
+        'is_visible'      => '',
+        */
+        'is_active'    => '',
+        'created_by'   => '',
+        'updated_by'   => '',
+        'created_at'   => '',
+        'updated_at'   => '',
       ],
       'edit' => [
+        'codigo'        => 'required inputmode="numeric" pattern="\d*',
+        'concepto'      => 'required',
+        /*
+        'periodo_id'      => '',
+        'annio'           => '',
+        'grado_id'        => '',
+        'asignatura_id'   => '',
+        'valorativo'      => '',
+        'is_visible'      => '',
+        */
         'is_active'    => '',
         'created_by'   => '',
         'updated_by'   => '',
@@ -44,6 +88,14 @@ trait IndicadorTraitDefa {
 
   public function getTPlaceholders() {
     return  array(
+      'codigo'     => 'Fort:100-199 - Debil:200-299 - Recom:300-399',
+      /*'annio'         => '',
+      'periodo_id'      => '',
+      'grado_id'        => '',
+      'asignatura_id'   => '',
+      'concepto'        => '',
+      'valorativo'      => '',
+      'is_visible'      => '',*/
     );
   }
 

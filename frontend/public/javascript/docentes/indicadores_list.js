c401aa5debe398dca4bfeec8a269a660bc70be67
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-01 02:32:29 -0500

    Update indicadores_list.js

--- frontend/public/javascript/docentes/indicadores_list.js
+++ frontend/public/javascript/docentes/indicadores_list.js
@@ -6,8 +6,10 @@
   });
 
   function traer_data(periodo_id, grado_id, asignatura_id) {
-    let ruta = document.getElementById('public_path');
+    let ruta = document.getElementById('public_path').innerHTML.trim()+'api/indicadores/list/'+periodo_id+'/'+grado_id+'/'+asignatura_id;
 
+    let ruta_delete = document.getElementById('public_path').innerHTML.trim()+'admin/indicadores/delID/';
+    let link_delete = '';
     let todas_las_filas = '';
     let input_search = '';
     let btn_add = '<div class="w3-padding">'
@@ -16,18 +18,26 @@
     let output = '';
     let cont = 1;
     let cod_id = '';
+    let row_data = '';
 
-    fetch(ruta.innerHTML.trim()+'api/indicadores/list/'+periodo_id+'/'+grado_id+'/'+asignatura_id)
+    fetch(ruta)
     .then((res) => res.json())
 
     .then(datos => {
-      for (let reg in datos) {
-        cod_id = '<span>' + datos[reg].periodo_id + '-' +datos[reg].codigo + ' ['+ datos[reg].valorativo +']' +'</span>';
-        let row_data = '<b>P'+cod_id+'</b><br>'+datos[reg].concepto+'.';
-        row_string = '<tr class="item w3-theme-'+(((cont % 2) == 0) ? "d1" : "d4")+'"><td> info </td></tr>';
-        todas_las_filas = todas_las_filas + row_string.replace('info', row_data);
+      for (let indicador in datos) {
+        indicador_codigo = '<span>' + datos[indicador].periodo_id + '-' +datos[indicador].codigo + ' ['+ datos[indicador].valorativo +']' +'</span>';
+        href_delete = ruta_delete +datos[indicador].id +'/docentes.listIndicadores.' +grado_id +'.' +asignatura_id;
+        
+        link_delete = '<a href="'+href_delete+'"><i class="fa-solid fa-trash-can w3-large"></i></a>';
+
+        row_data = '<b>P' +indicador_codigo +'</b><br>'
+                    +link_delete +'  ' +datos[indicador].concepto +'.';
+        
+        row_string = '<tr class="item w3-theme-'+(((cont % 2) === 0) ? "d1" : "d4")+'"><td> datos </td></tr>';
+        todas_las_filas = todas_las_filas + row_string.replace('datos', row_data);
+
         cont = cont + 1;
-      }
+      } // fin del recorrido de datos
 
       btn_new = btn_add.replace('btn_new_id', 'btn_new_'+periodo_id+','+grado_id+','+asignatura_id);
       btn_new = btn_new.replace('btn_new_caption', 'Nuevo Indicador P'+periodo_id);

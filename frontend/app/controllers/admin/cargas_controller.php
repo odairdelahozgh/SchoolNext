7607abc731f7bbf58b73368f5c05671fc7e5f766
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:26:29 -0500

    Update cargas_controller.php

--- frontend/app/controllers/admin/cargas_controller.php
+++ frontend/app/controllers/admin/cargas_controller.php
@@ -5,42 +5,7 @@
   * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
   */
   
-class CargasController extends AppController
+class CargasController extends ScaffoldController
 {
-
-  public function index() {
-    Redirect::toAction('list');
-  }
-  
-  public function list() {
-    $this->page_action = 'Lista de Cargas';
-    $this->data = (new SalAsigProf)->getList();
-  }
-
-  /**
-   * Eliminar un registro
-   */
-  public function delUUID(string $uuid, string $redirect='') {
-    $this->page_action = 'Eliminar Registro';
-    $SalAsigProf = (new SalAsigProf)->getByUUID($uuid);
-    if ($SalAsigProf::deleteById($SalAsigProf->id)) {
-      OdaFlash::valid("$this->page_action: $SalAsigProf");
-    } else {
-      OdaFlash::error("$this->page_action: $SalAsigProf");
-    }
-    return Redirect::to("$redirect");
-  }
-
-  public function delID(string $id, string $redirect='') {
-    $this->page_action = 'Eliminar Registro';
-    $SalAsigProf = (new SalAsigProf)->getById($id);
-    if ($SalAsigProf::deleteById($id)) {
-      OdaFlash::valid("$this->page_action: $SalAsigProf");
-    } else {
-      OdaFlash::error("$this->page_action: $SalAsigProf");
-    }
-    return Redirect::to("$redirect");
-  }
-
 
 } // END CLASS

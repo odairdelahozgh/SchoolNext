a6c7abe0d21e0d80a95fed4f116159147dd11a63
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:26:57 -0500

    Create eventos_controller.php

---
+++ frontend/app/controllers/admin/eventos_controller.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * Controlador Eventos
+  * @category App
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  */
+  
+class EventosController extends ScaffoldController
+{
+
+} // END CLASS

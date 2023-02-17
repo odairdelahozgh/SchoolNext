24ebb330be43e9570eedb5de26fb49950ee98e17
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:24:04 -0500

    Update tablas.php

--- frontend/app/config/tablas.php
+++ frontend/app/config/tablas.php
@@ -18,16 +18,20 @@ return [
   'calendario'          => 'snxt_calendario',
   
   'datosestud'          => 'sweb_datosestud',
-
+  
   'estudiante'          => 'sweb_estudiantes',
   'estudiantes'         => 'sweb_estudiantes',
   'estud_adjuntos'      => 'sweb_estudiante_adjuntos',
   'estud_adjuntos_dos'  => 'sweb_estudiante_adjuntos_dos',
   'estud_ctas_x_cobrar' => 'sweb_estud_cpc',
-  'estud_reg_academ'    => 'sweb_estudiantes_reg_desacacom',
+  'reg_obs_gen'           => 'sweb_estudiantes_reg_observ_gen',
   'estud_reg_obs_gen'   => 'sweb_estudiantes_reg_observ_gen',
+  'estud_reg_academ'    => 'sweb_estudiantes_reg_desacacom',
   'exam_admision'       => 'sweb_exam_admision_gdocs',
+  'evento'              => 'snxt_eventos',
+  'eventos'             => 'snxt_eventos',
   
+  'sweb_grados'         => 'Grado',
   'grado'               => 'sweb_grados',
   'grados'              => 'sweb_grados',
   'grados_asignat'      => 'sweb_grado_asignat',
@@ -40,13 +44,13 @@ return [
 
   'nota'                => 'sweb_notas',
   'notas'               => 'sweb_notas',
+  'notas_hist'          => 'sweb_notas_historia',
 
   'periodo'               => 'sweb_periodos',
   'periodos'              => 'sweb_periodos',
 
   'rango'                 => 'sweb_rangos',
   'rangos'                => 'sweb_rangos',
-  'reg_obs_gen'           => 'sweb_estudiantes_reg_observ_gen',
   
   'salon'                 => 'sweb_salones',
   'salones'               => 'sweb_salones',
@@ -56,6 +60,9 @@ return [
   
   'seccion'               => 'sweb_secciones',
   'secciones'             => 'sweb_secciones',
+
+  'tareas'                => 'snxt_tareas_sw',
+  'tarea'                 => 'snxt_tareas_sw',
 
   'usuario'               => 'dm_user',
   'usuarios'              => 'dm_user',

1111fa397eb9f9bb8a9528b15caf940aae520bff
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:26:34 -0500

    Create correcciones_controller.php

---
+++ frontend/app/controllers/admin/correcciones_controller.php
@@ -0,0 +1,11 @@
+<?php
+/**
+  * Controlador Correcciones  
+  * @category App
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  */
+
+class CorreccionesController extends ScaffoldController
+{
+
+} // END CLASS

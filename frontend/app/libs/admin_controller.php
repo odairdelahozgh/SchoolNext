54097ed70705f91dd01cd77e4e0e147a670045ca
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:40:56 -0500

    Update admin_controller.php

--- frontend/app/libs/admin_controller.php
+++ frontend/app/libs/admin_controller.php
@@ -25,12 +25,34 @@ abstract class AdminController extends Controller
     public $themei      = 'd';
     public $user_id     = 0;
 
+    public $page_module = '';
+    public $id_instit   = '';
+    
+    public array  $arrData = [];
+    public object $Modelo;
+    public array  $fieldsToShow = [];
+    public array  $fieldsToShowLabels = [];
+    public array  $fieldsToHidden = [];
+    public string $nombre_post   = '';
+    public string $nombre_modelo = '';
+    
+
     final protected function initialize()
     {
+      $this->user_id = Session::get('id');
+      $this->id_instit = Config::get('config.institution.id_name');
+      $this->theme = (Session::get('theme')) ? Session::get('theme') : 'dark' ;
+      $this->themei = substr($this->theme,0,1);
+
+      $this->nombre_modelo = OdaUtils::singularize($this->controller_name);
+      $this->nombre_post  = strtolower(OdaUtils::pluralize($this->nombre_modelo));
+      //OdaLog::alert("$this->nombre_post : $this->nombre_modelo");
     }
 
     final protected function finalize() {
-        $this->page_title = trim($this->page_title).' | '.APP_NAME;
+        //$this->page_title = trim($this->page_title).' | '.APP_NAME;
+        $this->page_action = (!$this->page_action) ? $this->action_name : $this->page_action;
+        $this->page_title  = strtoupper($this->controller_name) .' - ' . $this->page_action .' | ' .APP_NAME;
         
     }
 

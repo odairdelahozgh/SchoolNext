481832156bf585d42aeaa1a1d91194c1c6410946
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:57:06 -0500

    Update asignatura.php

--- frontend/app/models/asignatura.php
+++ frontend/app/models/asignatura.php
@@ -1,45 +1,23 @@
 <?php
 /**
- * Modelo Asignatura * 
+ * Modelo Asignatura  
  * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
  * @category App
  * @package  Models https://github.com/KumbiaPHP/ActiveRecord
+ * 
+ * id, nombre, abrev, orden, created_by, updated_by, created_at, updated_at, area_id, is_active, calc_prom
  */
-
- /* 
-  // ->create(array $data = []): bool {}
-  // ->update(array $data = []): bool {}
-  // ->save(array $data = []): bool {}
-  // ::delete($pk): bool
-  //
-  // ::get($pk, $fields = '*')
-  // ::all(string $sql = '', array $values = []): array
-  // ::first(string $sql, array $values = []): static
-  // ::filter(string $sql, array $values = []): array
-
-  // setActivar, setDesactivar
-  // getById, deleteById, getList, getListActivos, getListInactivos
-  // getByUUID, deleteByUUID, setUUID_All_ojo
-*/
-
-class Asignatura extends LiteRecord
-{
-  use TraitUuid, AsignaturaTraitCallBacks, AsignaturaTraitDefa, AsignaturaTraitProps,  AsignaturaTraitLinksOlds;
   
-  // Para debuguear: debug, warning, error, alert, critical, notice, info, emergence
-  // OdaLog::debug(msg: "Mensaje", name_log:'nombre_log'); 
-  
+class Asignatura extends LiteRecord {
+
+  use AsignaturaTraitSetUp;
+
   public function __construct() {
     parent::__construct();
     self::$table = Config::get('tablas.asignatura');
-    self::$_defaults     = $this->getTDefaults();
-    self::$_labels       = $this->getTLabels();
-    self::$_placeholders = $this->getTPlaceholders();
-    self::$_helps        = $this->getTHelps();
-    self::$_attribs      = $this->getTAttribs();
-    self::$class_name = __CLASS__;
-    self::$order_by_default = 'nombre ASC';
+    $this->setUp();
   } //END-__construct
 
+  public function __toString() { return $this->nombre; }
 
 } //END-CLASS

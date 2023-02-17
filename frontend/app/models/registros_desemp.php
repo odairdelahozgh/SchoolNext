1b4cd4ee5a489b1d8f54283d91c200e091100af5
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:59:47 -0500

    Update registros_desemp.php

--- frontend/app/models/registros_desemp.php
+++ frontend/app/models/registros_desemp.php
@@ -1,21 +1,42 @@
 <?php
-
 /**
-  * Modelo de Ejemplo  
-  * @category App
-  * @package Models 
-  * https://github.com/KumbiaPHP/Documentation/blob/master/es/active-record.md
-  */
+ * Modelo RegistrosDesemp * 
+ * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
+ * @category App
+ * @package  Models https://github.com/KumbiaPHP/ActiveRecord
+ */
 
-  /**
-   * annio, periodo_id, grado_id, salon_id, fortalezas, dificultades, compromisos, fecha, 
-   * acudiente, foto_acudiente, director, foto_director, 
-   * created_at, updated_at, created_by, updated_by
-   */
-class RegistrosDesemp extends LiteRecord
-{
-    protected static $table = 'sweb_estudiantes_reg_desacacom';
+ /* 
+  // ->create(array $data = []): bool {}
+  // ->update(array $data = []): bool {}
+  // ->save(array $data = []): bool {}
+  // ::delete($pk): bool
+  //
+  // ::get($pk, $fields = '*')
+  // ::all(string $sql = '', array $values = []): array
+  // ::first(string $sql, array $values = []): static
+  // ::filter(string $sql, array $values = []): array
+*/
+  
+class RegistrosDesemp extends LiteRecord {
+
+  use RegistrosDesempTraitSetUp;
+
+  public function __construct() {
+    parent::__construct();
+    self::$table = Config::get('tablas.estud_reg_academ');
+    $this->setUp();
+  } //END-__construct
 
 
-    
-}
+  public function cambiarSalonEstudiante(int $salon_id, int $estudiante_id) {
+    try {
+      $RegSalon = (new Salon)->first("SELECT id, grado_id FROM ".Config::get('tablas.salones')." WHERE id=?", [$salon_id]);
+      $this::query("UPDATE ".Config::get('tablas.estud_reg_academ')." SET salon_id=?, grado_id=? WHERE estudiante_id = ?", [$salon_id, $RegSalon->grado_id, $estudiante_id])->rowCount() > 0;
+    } catch (\Throwable $th) {
+      OdaLog::error($th);
+    }
+  } //END-cambiarSalonEstudiante
+  
+
+} //END-CLASS

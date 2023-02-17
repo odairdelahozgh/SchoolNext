d6059a9251da33084f628c42d245b4c26892ae86
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:59:30 -0500

    Update nota_trait_props.php

--- frontend/app/models/nota_trait_props.php
+++ frontend/app/models/nota_trait_props.php
@@ -1,12 +1,21 @@
 <?php
 trait NotaTraitProps {
   /*
-  public function __toString() { return $this->id.' '.$this->nombre; }
   */
   public function getFoto() { 
     return $this->estudiante_id.'<br>'.OdaTags::img("upload/estudiantes/$this->estudiante_id.png", $this->estudiante_id,
             "class=\"w3-round\" style=\"width:100%;max-width:80px\"", "[sin foto]");
   }
   
+  //====================
+  public function verNota() {
+    $color = (new Rango)::getColorRango($this->nota_final);
+    $rango = (new Rango)::getRango($this->nota_final);
+    $plan_apoyo = ($this->definitiva<60) ? 'Definitiva: '.$this->definitiva.' => Plan de Apoyo: '.$this->plan_apoyo : '' ;
+    return "<span class=\"w3-tag w3-round $color\">
+              $this->nota_final $rango
+            </span> $plan_apoyo ";
+  }
 
+  
 } //END-TraitProps

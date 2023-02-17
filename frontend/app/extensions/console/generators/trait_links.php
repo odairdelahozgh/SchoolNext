d7ca8f57e8c24ae27cbe30aebb05cf5b24cd9d0f
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:34:27 -0500

    Create trait_links.php

---
+++ frontend/app/extensions/console/generators/trait_links.php
@@ -0,0 +1,23 @@
+trait <?=$class?>TraitLinks {
+
+  
+  public function linkList() {
+  }
+
+  public function linkCreate() {
+  }
+
+  public function linkEdit() {
+  }
+
+  public function linkEditUuid() {
+  }
+
+  public function linkDelete() {
+  }
+
+  public function linkDeleteUuid() {
+  }
+
+
+} //END-TraitLinks

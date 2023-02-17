dcdc6d9365019878ee7b3f84563aeb32318b0128
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 18:27:04 -0500

    Update indicadores_controller.php

--- frontend/app/controllers/admin/indicadores_controller.php
+++ frontend/app/controllers/admin/indicadores_controller.php
@@ -5,35 +5,9 @@
   * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
   */
   
-class IndicadoresController extends AppController
+class IndicadoresController extends ScaffoldController
 {
 
-  public function index() {
-    Redirect::toAction('list');
-  }
-  
-  public function list() {
-    $this->page_action = 'Lista de Indicadores';
-    $this->data = (new Indicador)->getList();
-  }
-
-  /**
-   * Crea un Registro
-   */
-  public function create() {
-    $this->page_action = 'CREAR Registro';
-    $Indicador = new Indicador();
-    if (Input::hasPost('indicadors')) {
-      if ( $Indicador->create(Input::post('indicadors'))) {
-        OdaFlash::valid('Operación exitosa :: Crear Registro');
-        Input::delete();
-        return Redirect::to();
-      }
-      OdaFlash::error('Falló Operación :: Crear Registro');
-    }
-  }
-   
-  
   /**
    * Crea un Registro con AJAX
    */
@@ -41,67 +15,31 @@ class IndicadoresController extends AppController
     try {
       View::select(null, null);
       $this->page_action = 'CREAR Registro';
-      $Indicador = new Indicador();
-      if (Input::hasPost('indicadors')) {
-        if ( $Indicador->create(Input::post('indicadors'))) {
-          OdaFlash::valid($this->page_action);
-          $grado = Input::post('indicadors.grado_id');
-          $asignatura = Input::post('indicadors.asignatura_id');
-          Input::delete();
-          return Redirect::to("docentes/listIndicadores/$grado/$asignatura");
+      
+      $Indicador = new $this->nombre_modelo();
+      
+      if (Input::hasPost($this->nombre_post)) {
+        if ((new $this->nombre_modelo())->validar(Input::post($this->nombre_post))) {
+          if ( $Indicador->create(Input::post($this->nombre_post))) {
+            OdaFlash::valid("$this->page_action.");
+            OdaLog::info("$this->page_action");
+            //$grado = Input::post("$this->nombre_post.grado_id");
+            //$asignatura = Input::post("$this->nombre_post.asignatura_id");
+            //return Redirect::to("docentes/listIndicadores/$grado/$asignatura");
+          } else {
+            OdaFlash::error("$this->page_action. Falló operación guardar.");
+            OdaLog::error("$this->page_action. Falló operación guardar.");
+          }
+        } else {
+          OdaFlash::error("$this->page_action. ".Session::get('error_validacion'));
+          OdaLog::error("$this->page_action. ".Session::get('error_validacion'));
         }
-        OdaFlash::error($this->page_action);
       }
+
     } catch (\Throwable $th) {
       OdaLog::error($th);
     }
   } //END-create_ajax()
 
-
-  /**
-   * Edita un Registro
-   */
-  public function edit(int $id) {
-    $this->page_action = 'Editar Registro';
-    $Indicador = new Indicador();    
-    if (Input::hasPost('indicadores')) {
-      if ($Indicador->update(Input::post('indicadores'))) {
-        OdaFlash::valid("$this->page_action $Indicador");
-        return Redirect::to(); // al index del controller
-      }
-      OdaFlash::error('Editar Registro');
-      return; // Redirect::to('?');
-    }
-    $this->Modelo = $Indicador->get($id);
-  }
-
   
-  /**
-   * Eliminar un registro
-   */
-  public function delUUID(string $uuid, string $redirect='') {
-    $this->page_action = 'Eliminar Registro';
-    $Indicador = (new Indicador)->getByUUID($uuid);
-    if ($Indicador::deleteById($Indicador->id)) {
-      OdaFlash::valid("$this->page_action: $Indicador");
-    } else {
-      OdaFlash::error("$this->page_action: $Indicador");
-    }
-    $redirect = str_replace('.','/', $redirect);
-    return Redirect::to("$redirect");
-  }
-
-  public function delID(string $id, string $redirect='') {
-    $this->page_action = 'Eliminar Registro';
-    $Indicador = (new Indicador)->getById($id);
-    if ($Indicador::deleteById($id)) {
-      OdaFlash::valid("$this->page_action: $Indicador");
-    } else {
-      OdaFlash::error("$this->page_action: $Indicador");
-    }
-    $redirect = str_replace('.','/', $redirect);
-    return Redirect::to("$redirect");
-  }
-
-
 } // END CLASS

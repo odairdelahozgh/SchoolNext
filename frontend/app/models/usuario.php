aecc11ba681c9b4be809371df950b594939c2ab2
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 21:42:39 -0500

    Update usuario.php

--- frontend/app/models/usuario.php
+++ frontend/app/models/usuario.php
@@ -4,46 +4,25 @@
  * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
  * @category App
  * @package  Models https://github.com/KumbiaPHP/ActiveRecord
+ * 
+ * 
+ * id, uuid, username, roll, nombres, apellido1, apellido2, photo, profesion, direccion, documento, email, 
+ * telefono1, telefono2, cargo, sexo, fecha_nac, fecha_ing, fecha_ret, observacion, is_carga_acad_ok, 
+ * is_partner, usuario_instit, clave_instit, theme, 
+ * algorithm, salt, password, is_super_admin, last_login, forgot_password_code, 
+ * is_active, created_at, updated_at,  
  */
 
- /*  
-  // crear registro:               ->create(array $data = []): bool {}
-  // actualizar registro:          ->update(array $data = []): bool {}
-  // Guardar registro:             ->save(array $data = []): bool {}
-  // Eliminar registro por pk:     ::delete($pk): bool
-  //
-  // Buscar por clave pk:                 ::get($pk, $fields = '*') $fields: campos separados por coma
-  // Todos los registros:                 ::all(string $sql = '', array $values = []): array {}
-  // Primer registro de la consulta sql:  ::first(string $sql, array $values = [])//: static in php 8
-  // Filtra las consultas                 ::filter(string $sql, array $values = []): array
-    
-  setActivar, setDesactivar
-  getById, deleteById, getList, getListActivos, getListInactivos
-  getByUUID, deleteByUUID, setUUID_All_ojo
-
-  Para debuguear: debug, warning, error, alert, critical, notice, info, emergence
-  OdaLog::debug(msg: "Mensaje", name_log:'nombre_log');
   
-  
-  id, uuid, username, roll, is_active, is_carga_acad_ok, documento, email, algorithm, salt, password, is_super_admin, 
-  last_login, forgot_password_code, created_at, updated_at, nombres, apellido1, apellido2, photo, profesion, direccion, 
-  telefono1, telefono2, cargo, sexo, fecha_nac, fecha_ing, fecha_ret, observacion, is_partner, 
-  usuario_instit, clave_instit, theme
- */
+class Usuario extends LiteRecord {
 
-class Usuario extends LiteRecord
-{
-  use TraitUuid, UsuarioTraitCallBacks, UsuarioTraitDefa, UsuarioTraitProps;  
+  use UsuarioTraitSetUp;
+
   public function __construct() {
     parent::__construct();
-    self::$table = Config::get('tablas.usuarios');
-    self::$_defaults     = $this->getTDefaults();
-    self::$_labels       = $this->getTLabels();
-    self::$_placeholders = $this->getTPlaceholders();
-    self::$_helps        = $this->getTHelps();
-    self::$_attribs      = $this->getTAttribs();
-    self::$class_name = __CLASS__;
-    self::$order_by_default = 't.nombres, t.apellido1, t.apellido2';
+    self::$table = Config::get('tablas.usuario');
+    $this->setUp();
   } //END-__construct
 
+  
 } //END-CLASS

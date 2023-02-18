5229c430f7657bdaf3f7f18e580122aa8ccd5ef6
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-17 21:42:20 -0500

    Create tarea.php

---
+++ frontend/app/models/tarea.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Modelo Tarea
+ * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
+ * @category App
+ * @package  Models https://github.com/KumbiaPHP/ActiveRecord
+ * 
+ * 'id', 'uuid', 'modulo', 'seccion', 'nombre', 'descripcion', 'prioridad', 'fecha_ini', 'fecha_fin', 'estado', 'avance', 'is_active', 'created_at', 'created_by', 'updated_at', 'updated_by'
+ */
+  
+class Tarea extends LiteRecord {
+
+  use TareaTraitSetUp;
+  
+  public function __construct() {
+    parent::__construct();
+    self::$table = Config::get('tablas.tareas');
+    self::$order_by_default = 't.estado, t.prioridad, t.avance, t.nombre';
+    $this->setUp();
+  } //END-__construct
+
+
+} //END-CLASS

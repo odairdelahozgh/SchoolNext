99cdf4ac82449709e3e536500364c3a0ca29265c
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-23 06:20:04 -0500

    Update registros_desemp_acad_controller.php

--- frontend/app/controllers/admin/registros_desemp_acad_controller.php
+++ frontend/app/controllers/admin/registros_desemp_acad_controller.php
@@ -38,9 +38,42 @@ class RegistrosDesempAcadController extends ScaffoldController
       OdaFlash::error($this->page_action, true);
       OdaLog::debug($th, __CLASS__.'-'.__FUNCTION__);
     }
-
   } //END-create_ajax()
 
+  
+  /**
+   * Crea un Registro con AJAX
+   */
+  public function create_ajax2() {
+    try {
+      View::select(null, null);
+      $this->page_action = 'CREAR Registro de Desempeño Académico';
+      $RegistroDesempAcad = new RegistroDesempAcad();
+      if (Input::hasPost('registrodesempacads')) {
+        if ($RegistroDesempAcad->validar(Input::post('registrodesempacads'))) {
+          if ($RegistroDesempAcad->saveWithPhoto(Input::post('registrodesempacads'))) {
+            OdaFlash::valid($this->page_action, true);
+            Input::delete();
+            return Redirect::to("docentes/registros_desemp_acad");
+          }
+          $this->data = Input::post('registrodesempacads');
+          OdaFlash::error("$this->page_action. Falló operación guardar.", true);
+          return Redirect::to("docentes/registros_desemp_acad");
+        } else {
+          OdaFlash::error("$this->page_action. ".Session::get('error_validacion'), true);
+          return Redirect::to("docentes/registros_desemp_acad");
+        }
+      }
+      OdaFlash::error("$this->page_action. No coincide post.", true);      
+      return Redirect::to("docentes/registros_desemp_acad");
 
+    } catch (\Throwable $th) {
+      OdaFlash::error($this->page_action, true);
+      OdaLog::debug($th, __CLASS__.'-'.__FUNCTION__);
+    }
+
+  } //END-create_ajax2()
+
+  
 
 } // END CLASS

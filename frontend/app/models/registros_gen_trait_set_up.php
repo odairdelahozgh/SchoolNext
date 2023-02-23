a9a1c595bf8f101cb3ad268ff82e99aa6e092413
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-23 06:25:39 -0500

    Update registros_gen_trait_set_up.php

--- frontend/app/models/registros_gen_trait_set_up.php
+++ frontend/app/models/registros_gen_trait_set_up.php
@@ -11,7 +11,7 @@ trait RegistrosGenTraitSetUp {
   public function validar($input_post) {
     Session::set('error_validacion', '');
     try{
-      validar::number()->length(1)->min(0)->max(1)->assert($input_post['is_active']);
+      //validar::number()->length(1)->min(0)->max(1)->assert($input_post['is_active']);
       return true;
     } catch(NestedValidationException $exception) {
       Session::set('error_validacion', $exception->getFullMessage());
@@ -19,7 +19,6 @@ trait RegistrosGenTraitSetUp {
     }
   } //END-validar
 
-  
   // ===========
   public function getFecha($date_format="d-M-Y") {
     return date($date_format, strtotime($this->fecha));
@@ -46,39 +45,49 @@ trait RegistrosGenTraitSetUp {
   private function setUp() {
 
     self::$_fields_show = [
-      'all'     => ['id', 'uuid', 'estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 
-                    'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director', 
+      'all'       => ['id', 'uuid', 'tipo_reg', 'estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director', 
                     'created_at', 'updated_at', 'created_by', 'updated_by'],
-      'index'   => ['id', 'uuid', 'estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'fecha', 'asunto'],
-      'create'  => ['estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director'],
-      'edit'    => ['estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director']
+      'index'     => ['tipo_reg', 'estudiante_id', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'fecha', 'asunto'],
+      'create'    => ['tipo_reg', 'periodo_id', 'fecha', 'asunto', 'acudiente', 'director'],
+      'edit'      => ['tipo_reg', 'periodo_id', 'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director'],
+      'editUuid'  => ['tipo_reg', 'periodo_id', 'fecha', 'asunto', 'acudiente', 'foto_acudiente', 'director', 'foto_director'],
     ];
   
     self::$_attribs = [
+      'tipo_reg'        => 'required',
+      'estudiante_id'   => 'required',
+      'annio'           => 'required',
+      'periodo_id'      => 'required',
+      'grado_id'        => 'required',
+      'salon_id'        => 'required',
+      'asunto'          => 'required',
+      'fecha'           => 'required',
+      'acudiente'       => 'required',
+      'director'        => 'required',
+
       'id'                => 'required',
       'uuid'              => 'required',
-      'estudiante_id'     => 'required',
     ];
   
     self::$_defaults = [
     ];
   
     self::$_helps = [
-      'fecha'=>'Fecha de Registro', 
       'asunto'=>'L&iacute;mite: 1024 caracteres', 
       'acudiente'=>'Nombre del Acudiente que Asisti&oacute; a la reuni&oacute;n',
-      'foto_acudiente'=>'Evidencia Acudiente [foto]',
+      'foto_acudiente'=>'Evidencia Acudiente. Archivo tipo imagen [jpg, png, gif, jpeg]',
       'director'=>'Nombre del Director de grupo o Profesor que organiz&oacute;',
-      'foto_director'=>'Evidencia Director [foto]',
+      'foto_director'=>'Evidencia Docente. Archivo tipo imagen [jpg, png, gif, jpeg]',
     ];
   
     self::$_labels = [
+      'tipo_reg'=>'Tipo Registro',
       'estudiante_id'=>'Estudiante',
       'annio'=>'A&nacute;o', 
       'periodo_id'=>'Periodo',
       'grado_id'=>'Grado', 
       'salon_id'=>'Sal&oacute;n',
-      'fecha'=>'Fecha', 
+      'fecha'=>'Fecha de Registro', 
       'asunto'=>'Asunto', 
       'acudiente'=>'Acudiente', 
       'foto_acudiente'=>'Evidencia', 
@@ -94,24 +103,7 @@ trait RegistrosGenTraitSetUp {
     self::$_placeholders = [
     ];
   
-    // numeric, int, maxlength, length, range, select, email, url, ip, required, alphanum, alpha, date, pattern, decimal, equal
-    self::$_rules_validators = [
-        /*
-        'NombreCompleto' => [
-				  'required' => [ 'error' => 'Indique su nombre.' ],
-				  'alpha'    => [ 'error' => 'Nombre incompleto o incorrecto.' ]
-			  ],
-			  'Email' => [
-				  'required' => [ 'error' => 'Indique su email.' ],
-				  'email'    => [ 'error' => 'Email incorrecto.' ]
-			  ],
-			  'Movil' => [
-				  'required' => [ 'error' => 'Indique su teléfono / móvil.' ],
-				  'length'   => [ 'min' => '9', 'max' => '17', 'error' => 'Teléfono / móvil incorrecto' ],
-          'pattern'  => [ 'regexp' => '/^\+(?:[0-9] ?){6,14}[0-9]$/', 'error'  => 'Teléfono incorrecto. ejemplo. +34 862929929']
-        ], 
-        */
-    ];
+  }//END-setUp
 
-  }
+
 } //END-SetUp

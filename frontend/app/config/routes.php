f1b1f1d6794304c99f63de0f80d9121e10bf22f9
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-02-28 15:35:39 -0500

    Update routes.php

--- frontend/app/config/routes.php
+++ frontend/app/config/routes.php
@@ -37,6 +37,7 @@ return [
         '/notas'                    => 'docentes/listNotas',
         '/docen-indicadores'        => 'docentes/listIndicadores',
         '/indicadores'              => 'docentes/listIndicadores',
+        '/docen-perfil'             => 'docentes/perfilUsuario',
         
         //'/coord-consoli-notas'   => 'coordinador/consolidado_notas',
         //'/coord-consoli-notas'   => 'coordinador/consolidado',

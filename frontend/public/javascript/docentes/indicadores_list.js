2cb2b8c5e841bb90f2d23cd42de9a106e0aae4c5
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-03-23 07:09:25 -0500

    Update indicadores_list.js

--- frontend/public/javascript/docentes/indicadores_list.js
+++ frontend/public/javascript/docentes/indicadores_list.js
@@ -1,98 +1,83 @@
 
-  document.addEventListener('DOMContentLoaded', function () {
-    console.clear();
-    document.getElementById('btn1').click();
-    document.getElementById('formulario').style.display="none";
+document.addEventListener('DOMContentLoaded', function () {
+  console.clear();
+  document.getElementById('btn1').click();
+});
+
+function show_edit_form() {
+  w3.show('#form_edit');
+
+  w3.hide('#form_new');
+  w3.hide('#list_index');
+}//END-show_edit_form
+
+function show_new_form(periodo_id, grado_id, asignatura_id) {
+  w3.hide('#form_edit');
+  w3.hide('#list_index');
+  w3.show('#form_new');
+  
+  var dt = new Date();
+  const frm = document.getElementById("frm_new");
+// 'annio', 'periodo_id', 'grado_id', 'asignatura_id', 
+// 'codigo', 'concepto', 'valorativo', 'is_visible', 'is_active', 'created_at', 'updated_at', 'created_by', 'updated_by'
+  document.getElementById("titulo_indicador_new").innerHTML = 'Nuevo Indicador';
+  
+  frm.indicadors_annio.value = dt.getFullYear();
+  frm.indicadors_periodo_id.value    = periodo_id;
+  frm.indicadors_grado_id.value      = grado_id;
+  frm.indicadors_asignatura_id.value = asignatura_id;
+  frm.indicadors_codigo.value        = '';
+  frm.indicadors_concepto.value      = '';
+  frm.indicadors_valorativo.value    = 'Recomendación';
+
+  frm.indicadors_is_visible.value = 1;
+  frm.indicadors_is_active.value  = 1;
+  frm.indicadors_created_by.value = user_id;
+  frm.indicadors_updated_by.value = user_id;
+
+}//END-show_new_form
+
+function cancelar() {
+  w3.show('#list_index');
+  w3.hide('#form_new');
+  w3.hide('#form_edit');
+}//END-cancelar
+
+function traer_data(periodo_id, grado_id, asignatura_id) {
+  const capa_datos = document.getElementById("capa_datos");
+  getData(periodo_id, grado_id, asignatura_id).then(res => {
+    const elements = res.reduce((acc, data) => acc + template(data, periodo_id, grado_id, asignatura_id), "");
+    const tabla = `
+      <div class="w3-panel"><button class="w3-button w3-circle w3-green" onclick="show_new_form(`+periodo_id+`, `+grado_id+`, `+asignatura_id+`)">+</button></div>`
+      +`<table id="myTable" class="w3-table w3-responsive w3-striped w3-border w3-bordered">
+        <tbody id="searchBody"></tbody>
+          ${elements}
+      </table>
+    `;
+    capa_datos.innerHTML = tabla;
   });
+} //END-traer_data
 
-  function traer_data(periodo_id, grado_id, asignatura_id) {
-    document.getElementById("capa_datos").innerHTML = '';
+function template(data, periodo_id, grado_id, asignatura_id) {
+  let ruta_delete = document.getElementById('public_path').innerHTML.trim()+'admin/indicadores/del/';
+  href_delete = ruta_delete +data.id +'/docentes.listIndicadores.' +grado_id +'.' +asignatura_id;
+  indicador_codigo = 'P'+data.periodo_id + '-' +data.codigo + ' ['+ data.valorativo +']';
+  return `
+    <tr class="item w3-theme-d4">
+      <td id="${data.id}"> 
+        <a href="${href_delete}" onclick="return confirm('Atención: ¿Quiere borrar Indicador ${data.codigo}?')">
+        <i class="fa-solid fa-trash-can w3-large"></i></a>
+        <b>${indicador_codigo}</b>
+        <br>
+        <p>${data.concepto}</p>
+      </td>
+    </tr>
+  `;
+}//END-template
 
-    // AÑADIR BOTÓN
-    var btn_template = document.querySelector('#btn_new_template');
-    btn_nuevo = btn_template.content.querySelector("#id_btn_new");
-    btn_nuevo.textContent = "Nuevo Indicador P"+periodo_id;
-
-    var capa_datos = document.querySelector("#capa_datos");
-    var clone = document.importNode(btn_template.content, true);
-    capa_datos.appendChild(clone);
-
-    // AÑADIR TABLA
-    var template_table = document.querySelector('#table_template');
-    //table_body = template_table.content.querySelector("#searchBody");
-    //table_body.textContent = "Nuevo Indicador P"+periodo_id;
-
-    var capa_datos = document.querySelector("#capa_datos");
-    var clone = document.importNode(template_table.content, true);
-    capa_datos.appendChild(clone);
-    
-
-    let ruta = document.getElementById('public_path').innerHTML.trim()+'api/indicadores/list/'+periodo_id+'/'+grado_id+'/'+asignatura_id;
-    let ruta_delete = document.getElementById('public_path').innerHTML.trim()+'admin/indicadores/del/';
-    let link_delete = '';
-    let todas_las_filas = '';
-    let input_search = '';
-    let output = '';
-    let cont = 1;
-    let cod_id = '';
-    let row_data = '';
-
-    fetch(ruta)
-    .then((res) => res.json())
-
-    .then(datos => {
-      for (let indicador in datos) {
-        //indicador_codigo = '<span>' + datos[indicador].periodo_id + '-' +datos[indicador].codigo + ' ['+ datos[indicador].valorativo +']' +'</span>';
-        href_delete = ruta_delete +datos[indicador].id +'/docentes.listIndicadores.' +grado_id +'.' +asignatura_id;
-        link_delete = '<a href="'+href_delete+'"><i class="fa-solid fa-trash-can w3-large"></i></a>';
-        
-        var template_row = document.querySelector('#new_row_template');
-
-        td_row_title = template_row.content.querySelector("b");
-        td_row_title.textContent = datos[indicador].periodo_id + '-' +datos[indicador].codigo + ' ['+ datos[indicador].valorativo +']';
-
-        td_row_lnk_delete = template_row.content.querySelector("span");
-        //td_row_lnk_delete.textContent = link_delete;
-        
-        td_row_p = template_row.content.querySelector("p");
-        td_row_p.textContent = datos[indicador].concepto;
-        
-        var capa_datos = document.querySelector("#capa_datos");
-        var clone = document.importNode(template_table.content, true);
-        capa_datos.appendChild(clone);
-        
-        
-        var table_body = document.querySelector("#searchBody");
-        var clone = document.importNode(template_row.content, true);
-        table_body.appendChild(clone);
-
-        cont = cont + 1;
-      } // fin del recorrido de datos
-      
-      
-      document.getElementById("indicadors_annio").value = 2023;
-      document.getElementById("indicadors_periodo_id").value = periodo_id;
-      document.getElementById("indicadors_grado_id").value = grado_id;
-      document.getElementById("indicadors_asignatura_id").value = asignatura_id;
-      document.getElementById("indicadors_is_visible").value = 1;
-      document.getElementById("form_new").action='/admin/indicadores/create_ajax';
-
-    })
-    .catch(
-      error => {
-        console.log(error);
-        document.getElementById("capa_datos").innerHTML = error;
-      }
-    );    
-  }
-
-
-  function show_new_form() {
-    w3.hide('#lista_indicadores');
-    w3.show('#formulario');
-  }
-
-  function cancelar() {
-    w3.show('#lista_indicadores');
-    w3.hide('#formulario');
-  }
+function getData(periodo_id, grado_id, asignatura_id) {
+  let ruta = document.getElementById('public_path').innerHTML.trim()+'api/indicadores/list/'+periodo_id+'/'+grado_id+'/'+asignatura_id;
+  return fetch(ruta).then(res =>
+    res.json()
+  );
+}//END-getData

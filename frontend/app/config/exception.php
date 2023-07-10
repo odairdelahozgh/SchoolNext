544ed90d9e37ed9c247e6e1d17135ea1e3909e0d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-10 08:17:15 -0500

    Update exception.php

--- frontend/app/config/exception.php
+++ frontend/app/config/exception.php
@@ -2,6 +2,6 @@
 
 return [
     // array de IPs para mostrar la exception de desarrollador 
-    // ej: ['12.12.12.12','23.23.23.23']
+    // ej: ['192.168.0.1', '186.169.55.178']
     'trustedIp' => ['192.168.0.1', '186.169.55.178']
 ];

2963fe2e826a9972d72f2a0d82d2035de1e90ef7
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-11 05:17:34 -0500

    Update side_bar.php

--- frontend/app/views/padres/estudiantes/side_bar.php
+++ frontend/app/views/padres/estudiantes/side_bar.php
@@ -8,6 +8,18 @@
     }
     return $salida.'</div>'; */
 
+    $ver_matriculas = Config::get('padres.matriculas');
+    $ver_seguimientos = Config::get('padres.seguimientos');
+    $ver_boletines = Config::get('padres.boletines');
+    $ver_planes_apoyo = Config::get('padres.planes_apoyo');
+    $ver_reconocimientos = Config::get('padres.reconocimientos');
+
+    echo OdaForm::hidden(field: 'ver_matriculas', value:$ver_matriculas);
+    echo OdaForm::hidden(field: 'ver_seguimientos', value:$ver_seguimientos);
+    echo OdaForm::hidden(field: 'ver_boletines', value:$ver_boletines);
+    echo OdaForm::hidden(field: 'ver_planes_apoyo', value:$ver_planes_apoyo);
+    echo OdaForm::hidden(field: 'ver_reconocimientos', value:$ver_reconocimientos);
+
     $periodo = $arrData['periodo'];
     $buttons = [];
     if (count($data)>0) {
@@ -16,7 +28,7 @@
       }
       return OdaTags::buttonBars(arrButtons: $buttons);
     }
-  
+    
   } catch (\Throwable $th) {
     OdaFlash::error($th);
   }

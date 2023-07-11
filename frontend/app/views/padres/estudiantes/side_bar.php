dbb3231781486a7e3e1f1d06b32b5f8e8aa8f6be
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-10 22:24:17 -0500

    update views folder

--- frontend/app/views/padres/estudiantes/side_bar.php
+++ frontend/app/views/padres/estudiantes/side_bar.php
@@ -1,15 +1,24 @@
 <?php
-    $salida = '<div class="w3-bar">';
+  try {
+/*     $salida = '<div class="w3-bar">';
     foreach ($data as $key => $estud) {
-      $salida .= '<div class="w3-bar-item"><span id="btn'.($key+1).'" class="w3-btn w3-large">'.$estud->getFotoCircle().' '.$estud->nombres.'</span></div>';
+      $salida .= '<div class="w3-bar-item">
+            <span id="btn'.($key+1).'" class="w3-btn w3-large">'.$estud->getFotoCircle().' '.$estud->nombres.'</span>
+            </div>';
     }
-    //return $salida.'</div>';
-    $periodo = $arrData['periodo'];
+    return $salida.'</div>'; */
 
+    $periodo = $arrData['periodo'];
     $buttons = [];
+    if (count($data)>0) {
       foreach ($data as $key => $estud) {
         $buttons[$key] = ['caption'=>$estud->getFotoCircle().'<br>'.$estud->nombres, 'action'=>"traer_data($estud->id, '$estud->salon_nombre', $periodo)"];
+      }
+      return OdaTags::buttonBars(arrButtons: $buttons);
     }
-    return OdaTags::buttonBars(arrButtons: $buttons);
+  
+  } catch (\Throwable $th) {
+    OdaFlash::error($th);
+  }
 
 ?>

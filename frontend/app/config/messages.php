fbc8a8ef178b8f2e3913b91dd2d511466d6d8aa7
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-10 20:46:43 -0500

    Create messages.php

---
+++ frontend/app/config/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+$msg_comprimir_imagenes = '<span class="w3-small">Con el fin de optimizar el espacio en disco duro de nuestro servidor,
+le sugerimos <u>comprimir las imágenes antes de cargarlas</u>.<br> Este servicio lo puede obtener gratuitamente en '
+.OdaTags::linkExterno('https://www.iloveimg.com/es/comprimir-imagen', 'www.iloveimg.com', '').'</span>';
+
+return [
+    'comprimir_imagenes' => $msg_comprimir_imagenes,
+];
+

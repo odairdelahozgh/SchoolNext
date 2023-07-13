0518342180ca0b6d55ad3d2ce2abdde1f1981124
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-13 03:35:24 -0500

    delete model _ejemplo

--- frontend/app/models/_ejemplo_base_trait_links.php
+++
@@ -1,6 +0,0 @@
-<?php
-trait EjemploBaseTraitLinks {
-
-    public function linkOld_Demo() {
-    }
-}
--- frontend/app/models/_ejemplo_base_trait_props.php
+++
@@ -1,7 +0,0 @@
-<?php
-trait EjemploBaseTraitProps {
-  /*
-  public function __toString() { return $this->id.' '.$this->nombre; }
-  */ 
-  
-} //END-Trait

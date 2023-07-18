7d5161cac19d0232c97a58588949826cac35eebc
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-07-17 23:17:09 -0500

    Create planes_apoyo_controller.php

---
+++ frontend/app/controllers/admin/planes_apoyo_controller.php
@@ -0,0 +1,178 @@
+<?php
+/**
+  * Controlador PlanesApoyo
+  * @category App
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  */
+  
+class PlanesApoyoController extends ScaffoldController
+{
+
+
+  function guardarPlanesApoyo(int $periodo, int $salon_id, int $asignatura_id) {
+    //$this->page_action = "Notas Guardadas";
+    $redirect = "docentes/listNotas/$asignatura_id/$salon_id";
+
+    //  var_dump(array_filter($_POST, function($k) {
+    //   return $k == 'notas';
+    //   }, ARRAY_FILTER_USE_KEY));
+    //echo include(APP_PATH.'views/_shared/partials/snippets/show_input_post.phtml');
+
+    try {
+      //$cnt_success = 0; 
+      $cnt_fails = 0;
+      if (Input::hasPost($this->nombre_post)) {
+        $Post = Input::post($this->nombre_post);
+        $notas = [];
+        foreach ($Post as $field_name => $value) {
+          $codigo_id = (int)substr(string: $field_name, offset: strpos(haystack: $field_name, needle: "_") + 1);
+          if ($codigo_id>0) { // evitar que se cuele una variable que no sea del registro de calificacion (formato "_$id")
+            $notas[$codigo_id][$field_name] = $value;
+          }
+        }//end-foreach
+
+        
+        foreach ($notas as $id => $registro) { // PREPARA EL REGISTRO INDIVIDUAL DE NOTAS
+          $data_temp = [];
+          foreach ($registro as $field_name_id => $value) {
+            $long = strlen($field_name_id) - (strlen($id)+1);
+            $field_name = substr($field_name_id,0, $long);
+            $data_temp[$field_name] = "$value";
+          }
+          $dt_debug = '';
+          //if (4138==$id) {
+          //  $dt_debug .= 'Data Temp: '.implode(', ', $data_temp).PHP_EOL;
+          //  $dt_debug .= 'Data Temp keys: '.implode(', ', array_keys($data_temp)).PHP_EOL.PHP_EOL;
+          //}
+          
+          $cnt_num_ind = 0;
+          $data_indicadores = [];
+          $data = [];
+          foreach ($data_temp as $key => $value) { 
+            if (str_starts_with($key, 'i')) { // campos de indicadores
+              if (strlen($value)>0) { // indicadores validos
+                $cnt_num_ind +=1;
+                $index = ($cnt_num_ind<10) ? 'i0'.$cnt_num_ind : 'i'.$cnt_num_ind;
+                $data_indicadores[$index] = $value;
+              }
+            } else {  // campos que no son indicadores
+              $data[$key] = $value;
+            }
+          }
+          // if (4138==$id) {
+          //   $dt_debug .= 'Data indicadores: '.implode(', ', $data_indicadores).PHP_EOL;
+          //   $dt_debug .= 'Data indicadores keys: '.implode(', ', array_keys($data_indicadores)).PHP_EOL.PHP_EOL;
+          //   $dt_debug .= 'Data: '.implode(', ', $data).PHP_EOL.PHP_EOL;
+          // }
+
+          $data_indicadores = array_unique($data_indicadores); // elimina indicadores duplicados
+          // if (4138==$id) {
+          //   $dt_debug .= 'Data indicadores sin duplicados : '.implode(', ', $data_indicadores).PHP_EOL;
+          //   $dt_debug .= 'Data indicadores sin duplicados keys: '.implode(', ', array_keys($data_indicadores)).PHP_EOL.PHP_EOL;
+          // }
+
+          sort($data_indicadores, SORT_NUMERIC); // los ordena
+          // if (4138==$id) {
+          //   $dt_debug .= 'Data indicadores sin duplicados y ordenados: '.implode(', ', $data_indicadores).PHP_EOL;
+          //   $dt_debug .= 'Data indicadores sin duplicados y ordenados keys: '.implode(', ', array_keys($data_indicadores)).PHP_EOL.PHP_EOL;
+          // }
+          
+          $cnt_num_ind = 0;
+          foreach ($data_indicadores as $value) { 
+            $cnt_num_ind +=1;
+            $index = ($cnt_num_ind<10) ? 'i0'.$cnt_num_ind : 'i'.$cnt_num_ind;
+            $data[$index] = $value;
+          }
+          // if (4138==$id) {
+          //   $dt_debug .= 'Data : '.implode(', ', $data).PHP_EOL;
+          //   $dt_debug .= 'Data keys: '.implode(', ', array_keys($data)).PHP_EOL.PHP_EOL;
+          // }
+
+          $cnt_num_ind +=1;
+          for ($i=$cnt_num_ind; $i<=20; $i++) { // COMPLETA LOS INDICADORES EN BLANCO
+            $index = ($i<10) ? 'i0'.$i : 'i'.$i;
+            $data[$index] = ' ';
+          }
+          // if (4138==$id) {
+          //   $dt_debug .= 'Data FINAL: '.implode(', ', $data).PHP_EOL;
+          //   $dt_debug .= 'Data FINAL keys: '.implode(', ', array_keys($data)).PHP_EOL.PHP_EOL;
+          // }
+
+          //if (4138==$id) {
+          //  OdaLog::debug(str_repeat('-', 15).PHP_EOL.$dt_debug);
+          //}
+          $adicionales =[];
+          $adicionales['updated_at']= date('Y-m-d H:i:s', time());
+          $adicionales['updated_by']= $this->user_id;
+          
+          
+          $data['nota_final'] = (0==$data['nota_final']) ? $data['definitiva'] : $data['nota_final'] ;
+
+          $Modelo = (new $this->nombre_modelo());
+          $DQL = new OdaDql($Modelo::class);
+          $DQL->update($data)
+          ->addUpdate($adicionales)
+          ->where("t.id=?")
+          ->setParams([$id])
+          ->execute();
+
+          // if (4138==$id) {
+          //   OdaLog::debug(str_repeat('=', 15).PHP_EOL.$DQL->render());
+          // }
+          //$success = (new Nota())::query($DQL->render(), [$id])->rowCount() > 0;
+          //if (!$success) { $cnt_fails += 1;}
+          
+          //OdaLog::debug("[$id]: ".implode(', ',$data).PHP_EOL.$DQL->render().PHP_EOL.$DQL->getParams());
+        } //end-foreach-notas
+        //if ($cnt_fails) { OdaFlash::warning("$this->page_action"); }
+
+      }
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+    return Redirect::to(route: $redirect);
+
+  } //END-guardarNotas()
+
+  
+  public function exportPlanesApoyoEstudiantePdf(string $plan_apoyo_uuid): void {
+    try {    
+      $PA = (new PlanesApoyo)::getByUUID($plan_apoyo_uuid);
+      
+      $Estud = (new Estudiante())::get($PA->estudiante_id);
+      $this->arrData['Estud'] = $Estud;
+      
+      //$this->arrData['Profesor'] = (new SalAsigProf())::getProfesor($PA->salon_id, $PA->asignatura_id);
+      //OdaLog::debug($this->arrData['Profesor']);
+      $Asign = (new Asignatura)::get($PA->asignatura_id);
+      $Salon = (new Salon)::get($PA->salon_id);
+      $this->arrData['Periodo'] = $PA->periodo_id;
+      $this->arrData['Asign'] = $Asign;
+      $this->arrData['Salon'] = $Salon;
+      $this->arrData['Grado'] = (new Grado)::get($PA->grado_id);
+      
+      $this->arrData['Docentes'] = [];
+      foreach ((new Empleado)->getList() as $empleado) {
+        $this->arrData['Docentes'][$empleado->id] = $empleado;
+      }
+      
+      $Indicadores = (new Indicador())->getByPeriodoGrado($PA->periodo_id, (int)$PA->grado_id);
+      foreach ($Indicadores as $key => $indic) {
+        $this->arrData [ 'Indicadores' ] [ $indic->asignatura_id ] [ $indic->codigo ] ['concepto'] = trim($indic->concepto);
+      }
+      $this->file_tipo = 'Plan de Apoyo';
+      $this->file_name = OdaUtils::getSlug("plan-apoyo-{$Estud}-periodo-{$PA->periodo_id}");
+      $this->file_title = "$this->file_tipo de $Estud";
+      
+      $this->data = $PA;
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+    
+    View::select(view: "planes_apoyo.pdf", template: null);
+  } //END-exportPlanesApoyoEstudiantePdf
+
+
+} // END CLASS

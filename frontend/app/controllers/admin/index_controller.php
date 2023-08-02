e079c56c1baca464a6ba8ceb0dc10acfec10e499
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-02 01:05:34 -0500

    Update index_controller.php

--- frontend/app/controllers/admin/index_controller.php
+++ frontend/app/controllers/admin/index_controller.php
@@ -6,9 +6,18 @@
  */
 class IndexController extends AppController
 {
-    public function index() {
-        $this->page_action = 'M&oacute;dulo Admin';
-        $this->data = (new Evento)->getEventosDashboard();
+  public function index() {
+    try {
+      $this->page_action = 'M&oacute;dulo Admin';
+      $this->data = (new Evento)->getEventosDashboard();
+
+      if ('looper' == Config::get('config.theme.admin')) {        
+        View::select('layout-pagenavs', 'looper/layout-pagenavs');
       }
 
-}
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  } //END-index
+
+} //END-CLASS

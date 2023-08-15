c84c74e4d98789ab781f22c62f0b60f9ab96a9de
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-15 18:55:47 -0500

    Create docentes.php

---
+++ frontend/app/config/docentes.php
@@ -0,0 +1,21 @@
+<?php
+return [
+  'calificaciones' => [
+    'periodos'    => [3],
+    'salones'     => [],
+    'asignaturas' => [],
+    'usuarios'    => [],
+  ],
+  'seguimientos' => [
+    'periodos'    => [3],
+    'salones'     => [],
+    'asignaturas' => [],
+    'usuarios'    => [],
+  ],
+  'planes_apoyo' => [
+    'periodos'    => [3],
+    'salones'     => [],
+    'asignaturas' => [],
+    'usuarios'    => [],
+  ],
+];

c67d398f9282ecc018257be30b5c32c70057e8b0
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-15 05:00:36 -0500

    Create admisiones.php

---
+++ frontend/app/config/admisiones.php
@@ -0,0 +1,5 @@
+<?php
+return [
+  'lnk_ley_1581_2012' => 'http://www.secretariasenado.gov.co/senado/basedoc/ley_1581_2012.html',
+  'lnk_decreto_1377_2013' => 'http://www.suin-juriscol.gov.co/viewDocument.asp?ruta=Decretos/1276081',
+];

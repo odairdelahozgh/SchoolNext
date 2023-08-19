6e3cddbbc5e4cea2cecce5d62140001ff0fec247
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-19 10:39:09 -0500

    Update padres.php

--- frontend/app/config/padres.php
+++ frontend/app/config/padres.php
@@ -1,12 +1,13 @@
 <?php
 return [
   'mostrar_matriculas' => 0,
-  'mostrar_seguimientos' => 0,
+  'mostrar_seguimientos' => 1,
   'mostrar_boletines' => 1,
   'mostrar_planes_apoyo' => 1,
   'mostrar_reconocimientos' => 0,
 
   'periodo_boletines' => 2,
   'periodo_planes_apoyo' => 2,
+  'periodo_seguimientos' => 3,
 ];
 

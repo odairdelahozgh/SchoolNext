9acbd674ba4e8a5ba3d64233492d2b54e9f90fe2
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-19 03:34:40 -0500

    Update docentes_controller.php

--- frontend/app/controllers/docentes_controller.php
+++ frontend/app/controllers/docentes_controller.php
@@ -12,8 +12,13 @@ class DocentesController extends AppController
 {
 
   public function index() {
-    $this->page_action = 'Inicio';
-    $this->data = (new Evento)->getEventosDashboard();
+    try {
+      $this->page_action = 'Inicio';
+      $this->data = (new Evento)->getEventosDashboard();
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
   }//END-index
 
 
@@ -53,15 +58,13 @@ class DocentesController extends AppController
     try {
       $this->page_action = 'Direcci&oacute;n de Grupo';
       $periodo = Session::get('periodo');
-
-      $this->data = (new usuario)->misGrupos(); //($this->user_id
+      $this->data = (new usuario)->misGrupos();
 
       for ($i=1; $i <=$periodo ; $i++) { 
         $a_regs = (new Nota)::getNotasPromAnnioPeriodoSalon($i, $this->data[0]->id);
         foreach ($a_regs as $key => $value) {
           $this->arrData[$value->asignatura_nombre][$i]['avg'] = $value->avg;
         }
-        //$this->a_prom_p[$i]  = array_prom_key($a_regs, 'avg' );
       }
 
       $this->buttons = [];
@@ -82,7 +85,6 @@ class DocentesController extends AppController
       $this->page_action = 'Registros de Observaciones Generales';
       $estudiantes = (new Estudiante)->getListEstudiantes(estado: 1);
       $this->arrData = ['estudiantes' => $estudiantes];
-
       $this->data = (new RegistrosGen)->getRegistrosProfesor(user_id: Session::get(index: 'id'));
 
     } catch (\Throwable $th) {
@@ -97,8 +99,7 @@ class DocentesController extends AppController
       $this->page_action = 'Registros de Desempeño Académico';
       $estudiantes = (new Estudiante)->getListEstudiantes(estado: 1);
       $this->arrData = ['estudiantes' => $estudiantes];
-      
-      $this->data = (new RegistroDesempAcad)->getRegistrosProfesor(user_id: Session::get(index: 'id'));
+      $this->data = (new RegistroDesempAcad)->getRegistrosProfesor(user_id: Session::get('id'));
     
     } catch (\Throwable $th) {
       OdaFlash::error($th);
@@ -111,18 +112,15 @@ class DocentesController extends AppController
   public function listIndicadores(int $grado_id, int $asignatura_id): void {
     try {
       $this->page_action = 'Indicadores de Logro';
-      $RegGrado = (new Grado)->get($grado_id);
-      $RegAsignatura = (new Asignatura)->get($asignatura_id);
-      $this->arrData = ['grado' => $RegGrado, 'asignatura' => $RegAsignatura];
+      //$RegGrado = (new Grado)->get($grado_id);
+      //$RegAsignatura = (new Asignatura)->get($asignatura_id);
+      $this->arrData = [
+        'grado' => (new Grado)->get($grado_id), 
+        'asignatura' => (new Asignatura)->get($asignatura_id) ];
     }
     catch (\Throwable $th) {
       OdaFlash::error($th);
     }
-    // $arrIndic = array( 1=>array(), 2=>array(), 3=>array(), 4=>array(), 5=>array() );
-    // foreach ($indicadores as $indic) {
-    //   array_push($arrIndic[$indic->periodo_id], $indic);
-    // }
-    //$this->data = $arrIndic;
 
     View::select(view: 'indicadores/index');
   }//END-indicadores
@@ -134,17 +132,14 @@ class DocentesController extends AppController
   public function listNotas(int $asignatura_id, int $salon_id): void {
     try {  
       $this->page_action = 'Notas del Sal&oacute;n';
-      
       $this->Asignatura = (new Asignatura)->get($asignatura_id);
       $this->Salon= (new Salon)->get($salon_id);
-      
       $arr_periodos = range(start: 1, end: $this->_periodo_actual);
 
       $Notas = (new Nota)->getBySalonAsignaturaPeriodos($salon_id, $asignatura_id, $arr_periodos);
       if (0==count($Notas)) { 
         OdaFlash::info('No hay registros para mostrar.');
       }
-
       $this->data = array( 1=>array(), 2=>array(), 3=>array(), 4=>array(), 5=>array() );
       foreach ($Notas as $key => $nota) {
         array_push($this->data[$nota->periodo_id], $nota);
@@ -164,56 +159,27 @@ class DocentesController extends AppController
       $RegSalon = (new Salon)->get($salon_id);
       $RegPeriodo =(new Periodo)->get($periodo_id);
       $RegAsignatura = (new Asignatura)->get($asignatura_id);
-
-      $Nota = new Nota();
-      $this->data = $Nota->getBySalonAsignaturaPeriodos(salon_id: $salon_id, asignatura_id: $asignatura_id, periodos: [$periodo_id]);
+      $this->data = (new Nota)->getBySalonAsignaturaPeriodos(salon_id: $salon_id, asignatura_id: $asignatura_id, periodos: [$periodo_id]);
       $RegsIndicad = (new Indicador)->getIndicadoresCalificar(periodo_id: $periodo_id, grado_id: $RegSalon->grado_id, asignatura_id: $asignatura_id);
-      
-      $min_fortaleza = 0;
-      if ($RegsIndicad) {
-        $regs_min = min($RegsIndicad) ?? 0;
-        $regs_max = max($RegsIndicad) ?? 0;
-        
-        $min_fortaleza = min(array_filter(array: $RegsIndicad, callback: function ($element): bool {
-          return $element->valorativo == 'Fortaleza';
-        }));
-        $max_fortaleza = max(array_filter(array: $RegsIndicad, callback: function ($element) {
-          return $element->valorativo == 'Fortaleza';
-        }));
-        
-        $min_debilidad = min(array_filter(array: $RegsIndicad, callback: function ($element) {
-          return $element->valorativo == 'Debilidad';
-        }));
-        $max_debilidad = max(array_filter(array: $RegsIndicad, callback: function ($element) {
-          return $element->valorativo == 'Debilidad';
-        }));
-        
-        $min_recomendacion = min(array_filter(array: $RegsIndicad, callback: function ($element) {
-          return $element->valorativo == 'Recomendación';
-        }));
-        $max_recomendacion = max(array_filter(array: $RegsIndicad, callback: function ($element) {
-          return $element->valorativo == 'Recomendación';
-        }));
-      }
+      $MinMaxIndicad = (new Indicador)->getMinMaxByPeriodoGradoAsignatura($periodo_id, $RegSalon->grado_id, $asignatura_id);
 
-      
-      $this->fieldsToShow = $Nota::getFieldsShow(show: 'calificar');
       $this->arrData = [
         'Periodo'           => $RegPeriodo,
         'Asignatura'        => $RegAsignatura,
         'Salon'             => $RegSalon,
         'Indicadores'       => $RegsIndicad,
-        'annio_actual'      => (int)Config::get(var: 'config.academic.annio_actual'),
-        'periodo_actual'    => (int)Config::get(var: 'config.academic.periodo_actual'),
-        'min_fortaleza'     =>$min_fortaleza,
-        'max_fortaleza'     =>$max_fortaleza,
-        'min_debilidad'     =>$min_debilidad,
-        'max_debilidad'     =>$max_debilidad,
-        'min_recomendacion' =>$min_recomendacion,
-        'max_recomendacion' =>$max_recomendacion,
-        'min_indic' => $regs_min,
-        'max_indic' => $regs_max,
-        'cnt_indicador'   =>count(value: $RegsIndicad),
+        'annio_actual'      => $this->_annio_actual,
+        'periodo_actual'    => $this->_periodo_actual,
+        'min_fortaleza'     => $MinMaxIndicad['min_fortaleza'],
+        'max_fortaleza'     => $MinMaxIndicad['max_fortaleza'],
+        'min_debilidad'     => $MinMaxIndicad['min_debilidad'],
+        'max_debilidad'     => $MinMaxIndicad['max_debilidad'],
+        'min_recomendacion' => $MinMaxIndicad['min_recomendacion'],
+        'max_recomendacion' => $MinMaxIndicad['max_recomendacion'],
+        'min_indic'         => $MinMaxIndicad['regs_min'],
+        'max_indic'         => $MinMaxIndicad['regs_max'],
+        'ancho_lim'         => $MinMaxIndicad['ancho_lim'],
+        'cnt_indicador'     => count($RegsIndicad),
       ];
       
     } catch (\Throwable $th) {
@@ -229,62 +195,29 @@ class DocentesController extends AppController
       $RegSalon = (new Salon)->get($salon_id);
       $RegPeriodo =(new Periodo)->get($periodo_id);
       $RegAsignatura = (new Asignatura)->get($asignatura_id);
-      
-      $SegInt = new Seguimientos();
-      $this->data = $SegInt->getBySalonAsignaturaPeriodos($salon_id, $asignatura_id, [$periodo_id]);
-
+      $this->data = (new Seguimientos)->getBySalonAsignaturaPeriodos($salon_id, $asignatura_id, [$periodo_id]);
       $RegsIndicad = (new Indicador)->getIndicadoresCalificar(periodo_id: $periodo_id, grado_id: $RegSalon->grado_id, asignatura_id: $asignatura_id);
       $MinMaxIndicad = (new Indicador)->getMinMaxByPeriodoGradoAsignatura($periodo_id, $RegSalon->grado_id, $asignatura_id);
       
-      $regs_min = 0; $regs_max = 0;
-      $min_fortaleza = 0; $max_fortaleza = 0;
-      $min_debilidad = 0; $max_debilidad = 0;
-      $min_recomendacion = 0; $max_recomendacion = 0;
-      $ancho_lim = 0;
-      
-      if ($RegsIndicad) { // verifico que hay registros de indicadores
-        $min_max_todos = [];
-        foreach ($MinMaxIndicad as $key => $Indic) {
-          $min_max_todos[] = $Indic->min;
-          $min_max_todos[] = $Indic->max;
-          if ('Fortaleza'==$Indic->valorativo) {
-            $min_fortaleza = $Indic->min;
-            $max_fortaleza = $Indic->max;
-          }
-          if ('Debilidad'==$Indic->valorativo) {
-            $min_debilidad = $Indic->min;
-            $max_debilidad = $Indic->max;
-          }
-          if ('Recomendación'==$Indic->valorativo) {
-            $min_recomendacion = $Indic->min;
-            $max_recomendacion = $Indic->max;
-          }
-        }
-        $regs_min = min($min_max_todos) ?? 0;
-        $regs_max = max($min_max_todos) ?? 0;
-        $ancho_lim = strlen((string)$regs_max);
-      }
-      
-      
-      $this->fieldsToShow = $SegInt::getFieldsShow(show: 'calificar');
       $this->arrData = [
         'Periodo'           => $RegPeriodo,
         'Asignatura'        => $RegAsignatura,
         'Salon'             => $RegSalon,
         'Indicadores'       => $RegsIndicad,
-        'annio_actual'      => (int)Config::get(var: 'config.academic.annio_actual'),
-        'periodo_actual'    => (int)Config::get(var: 'config.academic.periodo_actual'),
-        'min_fortaleza'     =>$min_fortaleza,
-        'max_fortaleza'     =>$max_fortaleza,
-        'min_debilidad'     =>$min_debilidad,
-        'max_debilidad'     =>$max_debilidad,
-        'min_recomendacion' =>$min_recomendacion,
-        'max_recomendacion' =>$max_recomendacion,
-        'min_indic' => $regs_min,
-        'max_indic' => $regs_max,
-        'ancho_lim' => $ancho_lim,
-        'cnt_indicador'   =>count(value: $RegsIndicad),
+        'annio_actual'      => $this->_annio_actual,
+        'periodo_actual'    => $this->_periodo_actual,
+        'min_fortaleza'     => $MinMaxIndicad['min_fortaleza'],
+        'max_fortaleza'     => $MinMaxIndicad['max_fortaleza'],
+        'min_debilidad'     => $MinMaxIndicad['min_debilidad'],
+        'max_debilidad'     => $MinMaxIndicad['max_debilidad'],
+        'min_recomendacion' => $MinMaxIndicad['min_recomendacion'],
+        'max_recomendacion' => $MinMaxIndicad['max_recomendacion'],
+        'min_indic'         => $MinMaxIndicad['regs_min'],
+        'max_indic'         => $MinMaxIndicad['regs_max'],
+        'ancho_lim'         => $MinMaxIndicad['ancho_lim'],
+        'cnt_indicador'     => count($RegsIndicad),
       ];
+
       View::select(view: 'notas/seguimientos/index');
       
     } catch (\Throwable $th) {
@@ -300,60 +233,27 @@ class DocentesController extends AppController
       $RegSalon = (new Salon)->get($salon_id);
       $RegPeriodo =(new Periodo)->get($periodo_id);
       $RegAsignatura = (new Asignatura)->get($asignatura_id);
-
-      $PA = new PlanesApoyo();
-      $this->data = $PA->getBySalonAsignaturaPeriodos($salon_id, $asignatura_id, [$periodo_id]);
+      $this->data = (new PlanesApoyo)->getBySalonAsignaturaPeriodos($salon_id, $asignatura_id, [$periodo_id]);
       $RegsIndicad = (new Indicador)->getIndicadoresCalificar($periodo_id, $RegSalon->grado_id, $asignatura_id);
       $MinMaxIndicad = (new Indicador)->getMinMaxByPeriodoGradoAsignatura($periodo_id, $RegSalon->grado_id, $asignatura_id);
       
-      $regs_min = 0;
-      $regs_max = 0;
-
-      $min_fortaleza = 0;
-      $max_fortaleza = 0;
-      $min_debilidad = 0;
-      $max_debilidad = 0;
-      $min_recomendacion = 0;
-      $max_recomendacion = 0;
-
-      if ($RegsIndicad) {
-        //$regs_min = min($RegsIndicad) ?? 0;
-        //$regs_max = max($RegsIndicad) ?? 0;
-        foreach ($MinMaxIndicad as $key => $Indic) {
-          if ('Fortaleza'==$Indic->valorativo) {
-            $min_fortaleza = $Indic->min;
-            $max_fortaleza = $Indic->max;
-            $regs_min = $Indic->min; // revisar un poco mas
-          }
-          if ('Debilidad'==$Indic->valorativo) {
-            $min_debilidad = $Indic->min;
-            $max_debilidad = $Indic->max;
-          }
-          if ('Recomendación'==$Indic->valorativo) {
-            $min_recomendacion = $Indic->min;
-            $max_recomendacion = $Indic->max;
-            $regs_max = $Indic->max; // revisar un poco mas
-          }
-        }
-      }
-      
-      $this->fieldsToShow = $PA::getFieldsShow(show: 'calificar');
       $this->arrData = [
         'Periodo'           => $RegPeriodo,
         'Asignatura'        => $RegAsignatura,
         'Salon'             => $RegSalon,
         'Indicadores'       => $RegsIndicad,
-        'annio_actual'      => (int)Config::get(var: 'config.academic.annio_actual'),
-        'periodo_actual'    => (int)Config::get(var: 'config.academic.periodo_actual'),
-        'min_fortaleza'     =>$min_fortaleza,
-        'max_fortaleza'     =>$max_fortaleza,
-        'min_debilidad'     =>$min_debilidad,
-        'max_debilidad'     =>$max_debilidad,
-        'min_recomendacion' =>$min_recomendacion,
-        'max_recomendacion' =>$max_recomendacion,
-        'min_indic' => $regs_min,
-        'max_indic' => $regs_max,
-        'cnt_indicador'   =>count(value: $RegsIndicad),
+        'annio_actual'      => $this->_annio_actual,
+        'periodo_actual'    => $this->_periodo_actual,
+        'min_fortaleza'     => $MinMaxIndicad['min_fortaleza'],
+        'max_fortaleza'     => $MinMaxIndicad['max_fortaleza'],
+        'min_debilidad'     => $MinMaxIndicad['min_debilidad'],
+        'max_debilidad'     => $MinMaxIndicad['max_debilidad'],
+        'min_recomendacion' => $MinMaxIndicad['min_recomendacion'],
+        'max_recomendacion' => $MinMaxIndicad['max_recomendacion'],
+        'min_indic'         => $MinMaxIndicad['regs_min'],
+        'max_indic'         => $MinMaxIndicad['regs_max'],
+        'ancho_lim'         => $MinMaxIndicad['ancho_lim'],
+        'cnt_indicador'     => count($RegsIndicad),
       ];
       
     } catch (\Throwable $th) {
@@ -362,12 +262,12 @@ class DocentesController extends AppController
     View::select(view: 'notas/planes_apoyo/index');
   }//END-
   
+
   public function perfilUsuario(): void {
     $this->page_action = 'Perfil del Usuario';
     //$this->data = (array)(new Usuario())::get($this->user_id);
     View::select(view: 'perfilUsuario/index');
   }//END-perfilUsuario
-
 
 
 

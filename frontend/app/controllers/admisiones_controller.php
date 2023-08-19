fc8974a9bb66162449575c654f6d0dba40f1c241
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-19 02:59:26 -0500

    Update admisiones_controller.php

--- frontend/app/controllers/admisiones_controller.php
+++ frontend/app/controllers/admisiones_controller.php
@@ -7,16 +7,36 @@
   */
 class AdmisionesController extends DmzController
 {
-    
+  
   public function index() {
     try {
+      $this->theme = 'dark';
+      $this->themei = 'd';
+
       $this->page_action = 'Inicio';
-      View::select('auth-commingsoon-v1', 'looper/auth-comingsoon-v1');
+      View::template('admisiones');
+      //View::select('index', 'looper/component-steps');
 
     } catch (\Throwable $th) {
       OdaFlash::error($th);
     }
   } //END-index
 
-  
+  public function success($id) {
+    try {
+      $this->page_action = 'Success';
+      View::template('admisiones');
+      $this->data = (new Aspirante())::get($id);
+      $GradosActivos = (new Grado())->getListActivos();
+      $this->arrData['Grados'] = null;
+      foreach ($GradosActivos as $key => $grado) {
+        $this->arrData['Grados'][$grado->id] = $grado->nombre;
+      }
+      
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  } //END-success
+
+
 } // END CLASS

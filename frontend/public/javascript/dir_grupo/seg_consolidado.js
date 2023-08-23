30b1541154442a7a26b3e34821053be79df61ca3
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-23 08:33:49 -0500

    Create seg_consolidado.js

---
+++ frontend/public/javascript/dir_grupo/seg_consolidado.js
@@ -0,0 +1,121 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('btn-0').click();
+});
+
+function traer_salones(periodo, user_id) {
+  //console.clear();
+  document.getElementById('salones').innerHTML = '';
+
+  let ruta_base = document.getElementById('public_path').innerHTML.trim();
+  let url = ruta_base+`api/salones/by_director/${user_id}`;  // cambiarlo por salones_por_coordinador
+  fetch(url)
+  .then((res) => res.json())
+  .then(datos => {
+    let html = `<div class="w3-bar">`;
+    for (let key in datos) { 
+      html += `<div class=\"w3-bar-item\">
+                  <button id="salon-${key}" onclick="traer_data('${datos[key].nombre}', '${datos[key].uuid}', ${periodo})" class="w3-bar-item">
+                    ${datos[key].nombre}
+                  </button>
+                </div>`;
+    }
+    html += '</div>';
+    document.getElementById('salones').innerHTML = html;
+    document.getElementById('salon-0').click();
+  })
+    .catch(
+        error => document.querySelector('#resultados').innerHTML = error
+    );
+} //END-traer_salones
+
+
+function traer_data(salon_nombre, salon_uuid, periodo) {
+  let ruta_base = document.getElementById('public_path').innerHTML.trim();
+  let theme = document.getElementById('theme').innerHTML.trim();
+  document.querySelector('#resultados').innerHTML = '';
+  
+  //console.clear();
+  fetch(ruta_base+`api/seguimientos/by_salon_periodo/${salon_uuid}/${periodo}`)
+  .then((res) => res.json())
+  .then(datos => {
+    let caption = `<h2>Periodo ${periodo} :: Seguimientos de ${salon_nombre}</h2>`;
+    //console.log(datos);
+    
+    let result = '';
+    let cnt = 0;
+    let AsignaturasValidas = [];
+
+    // SOLO PARA VERIFICAR CUALES TIENE DATOS DE SEGUIMIENTO
+    for (let cod_estudiante in datos) {
+      for (let nom_estudiante in datos[cod_estudiante]) {
+        for (let text_abrev in datos[cod_estudiante][nom_estudiante]) {
+          for (let abrev in datos[cod_estudiante][nom_estudiante][text_abrev]) {
+            const obj = Object.keys(datos[cod_estudiante][nom_estudiante][text_abrev][abrev]);
+            if (obj.length>0){
+              AsignaturasValidas[abrev] = 1;
+            }
+          }
+        }
+      }
+    }//END-for
+    //console.log(AsignaturasValidas);
+    //console.log(AsignaturasValidas.keys());
+
+    // ARMA LA TABLA FINAL
+    for (let cod_estudiante in datos) {
+      cnt +=1;
+      result += `<tr>`;
+      for (let nom_estudiante in datos[cod_estudiante]) {
+        let columnas = '';
+        let anadir = false;
+        for (let text_abrev in datos[cod_estudiante][nom_estudiante]) {
+          for (let abrev in datos[cod_estudiante][nom_estudiante][text_abrev]) {
+            const obj = Object.keys(datos[cod_estudiante][nom_estudiante][text_abrev][abrev]);
+            
+            let data = datos[cod_estudiante][nom_estudiante][text_abrev][abrev];
+
+            if (AsignaturasValidas.hasOwnProperty(abrev)) {
+              if (obj.length==0){
+                columnas += `<td></td>`;
+              } else {
+                columnas += `
+                  <td class="w3-center">
+                    <a href="${ruta_base}admin/seguimientos/exportSeguimientosRegistroPdf/${data.uuid}" 
+                       class="w3-btn w3-pale-red " 
+                       target="_blank"
+                       title="Seguimiento ${nom_estudiante} ${abrev} P${periodo}">
+                       ${abrev}
+                    </a>
+                    <br>${data.asi_desempeno}<br>${data.asi_fecha_entrega}
+                  </td>
+                `;
+                anadir = true;
+              }
+            }//END-if
+
+          }
+        }
+
+        if (anadir) {
+          result += `<td>${cnt}</td><td>${nom_estudiante} [${cod_estudiante}]</td> ${columnas}`;
+        }
+
+      }
+      result += `</tr>`;
+    }
+
+
+    document.querySelector('#resultados').innerHTML = `
+      <table id="myTable"class="w3-table w3-responsive w3-bordered w3-small">
+        <caption id="tcaption" class="w3-left-align w3-bottombar w3-border-blue">${caption}</caption>
+        <tbody id="tbody">${result}</tbody>
+      </table>
+      `;
+
+  })
+  .catch(
+    error => document.getElementById('resultados').innerHTML = error
+  );
+
+
+}

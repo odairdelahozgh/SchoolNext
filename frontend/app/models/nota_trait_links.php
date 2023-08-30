ee3516b33f9ad2d17a489cabaf3561c28d82340d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-08-30 05:16:49 -0500

    Update nota_trait_links.php

--- frontend/app/models/nota_trait_links.php
+++ frontend/app/models/nota_trait_links.php
@@ -77,4 +77,52 @@ trait NotaTraitLinks {
   } //END-lnkCuadroHonorGeneralBachilleratoPDF
 
 
+  
+  public static function lnkPageCalificar(int $periodo_id, int $salon_id, int $asignatura_id): string {
+    try {
+      return OdaTags::linkButton(
+        action: "docentes/notasCalificar/{$periodo_id}/{$salon_id}/{$asignatura_id}", 
+        text: "Calificar <br>P{$periodo_id}", 
+        icon: '', 
+        attrs: 'title="Ingresar NOTAS" class="w3-button w3-small w3-pale-green w3-round-large"'
+      );
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+      return '';
+    }
+  } //END-lnkPageCalificar
+
+
+  public static function lnkPageSeguimientos(int $periodo_id, int $salon_id, int $asignatura_id): string {
+    try {
+      return OdaTags::linkButton(
+        action: "docentes/notasCalificarSeguimientos/{$periodo_id}/{$salon_id}/{$asignatura_id}", 
+        text: "Seguimientos <br>Intermedios P{$periodo_id}", 
+        icon: '', 
+        attrs: 'title="Ingresar SEGUIMIENTOS INTERMEDIOS" class="w3-button w3-small w3-pale-blue w3-round-large"'
+      );
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+      return '';
+    }
+  } //END-lnkPageSeguimientos
+
+
+  public static function lnkPagePlanesApoyo(int $periodo_id, int $salon_id, int $asignatura_id): string {
+    try {
+      return OdaTags::linkButton(
+        action: "docentes/notasCalificarPlanesApoyo/{$periodo_id}/{$salon_id}/{$asignatura_id}", 
+        text: "Planes de <br>Apoyo P{$periodo_id}", 
+        icon: '', 
+        attrs: 'title="Ingresar PLANES DE APOYO" class="w3-button w3-small w3-pale-red w3-round-large"'
+      );
+
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+      return '';
+    }
+  } //END-lnkPagePlanesApoyo
+
 }//END-Trait

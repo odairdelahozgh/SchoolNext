8b385c674e20e543015d96959187140cc8a691e1
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-01 14:13:14 -0500

    Update indicador_trait_props.php

--- frontend/app/models/indicador_trait_props.php
+++ frontend/app/models/indicador_trait_props.php
@@ -13,17 +13,34 @@
   public function __toString() { return $this->codigo; }
 
   
-  public static function indicadorF(int $indicador=101, $ListaIndicadores) {
+  public static function indicadorF(int $indicador=101, array $ListaIndicadores, array $attr = [], $react = false) {
     if (0==$indicador) {
       return '';
     }
     if (!is_null($ListaIndicadores)) {
       if (array_key_exists($indicador, $ListaIndicadores)) {
+        
         $valorativo = Valorativo::tryFrom( $ListaIndicadores[$indicador]['valorativo'][0] );
+        $concepto = trim($ListaIndicadores[$indicador]['concepto']);
+        
+        $fnc_react = '';
+        if ($react) {
+          $fnc_react = "
+            onclick=\"indicador_onclick($indicador)\"
+            onmouseenter=\"indicador_onmouseenter('$concepto')\"
+            onmouseleave=\"indicador_onmouseleave()\"
+          ";
+        }
+
+
         return "
           <span 
-            title=\"[$indicador] {$ListaIndicadores[$indicador]['concepto']}\"
-            class=\"w3-tag w3-round w3-{$valorativo->color()} w3-border w3-border-white\">
+            title=\"[$indicador] $concepto\"
+            codigo_indic=\"$indicador\"
+            color_indic=\"{$valorativo->color()}\"
+            $fnc_react
+            class=\"show-indic w3-tag w3-round w3-{$valorativo->color()} w3-border w3-border-white\"
+          >
               $indicador
           </span>";
       } else {

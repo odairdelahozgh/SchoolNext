e675f96ac051b12fdd8cf0a628fdbc6039c109cc
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-05 06:42:40 -0500

    Update pagado.php

--- frontend/app/libs/enums/pagado.php
+++ frontend/app/libs/enums/pagado.php
@@ -8,25 +8,26 @@ enum Pagado: int {
 
   //public const Bueno = self::Si;
 
-  public function label(): string {
+  public function label($attr_class=''): string {    
     return match($this) {
-      static::No  => 'No Pagado',
-      static::Si  => 'Pagado',
-      default     => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
+      static::No => "<span class=\"$attr_class\">No Pagado</span>",
+      static::Si => "<span class=\"$attr_class\">Pagado</span>",
+      default    => throw new InvalidArgumentException(message: "Erroneo"),
     };
   } //END-label
   
-  public function ico(): string {
+  public function ico(bool $with_color=true, $attr_class=''): string {
     $ico = match($this) {
-        static::No => 'fa-sack-ban',
+        static::No => 'fa-sack-dollar',
         static::Si => 'fa-sack-dollar',
         default    => 'fa-question',
     };
-    return "<i class=\"fa-solid $ico w3-small\"></i>&nbsp;";
+    $color = ($with_color) ? 'style="color: '.self::color().'"' : '' ;
+    return "<i class=\"fa-solid $ico $attr_class\" $color></i>&nbsp;";
   } //END-ico
   
-  public function label_ico(): string {
-    return $this->ico().$this->label();
+  public function label_ico(bool $with_color=true, $attr_class=''): string {
+    return $this->ico($with_color, $attr_class).$this->label($attr_class);
   } //END-label_ico
 
   public function color(): string {

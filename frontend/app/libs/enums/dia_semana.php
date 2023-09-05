58989fed8d23b57081aa6933ebd5e38d85a0a3b9
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-05 06:42:47 -0500

    Create dia_semana.php

---
+++ frontend/app/libs/enums/dia_semana.php
@@ -0,0 +1,38 @@
+<?php
+enum DiaSemana: int {
+  Use EnumsFunciones;
+
+  case Domingo = 0;
+  case Lunes = 1;
+  case Martes = 2;
+  case Miercoles = 3;
+  case Jueves = 4;
+  case Viernes = 5;
+  case Sabado = 6;
+  
+  public function label(bool $abrev = false): string {
+    return match($this) {
+      static::Domingo   => (($abrev)?'Do':'Domingo'),
+      static::Lunes     => (($abrev)?'Lu':'Lunes'),
+      static::Martes    => (($abrev)?'Ma':'Martes'),
+      static::Miercoles => (($abrev)?'Mi':'Miércoles'),
+      static::Jueves    => (($abrev)?'Ju':'Jueves'),
+      static::Viernes   => (($abrev)?'Vi':'Viernes'),
+      static::Sabado    => (($abrev)?'Sa':'Sábado'),
+      default           => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
+    };
+  }//END-label
+
+  public function ico(): string {
+    return "<i class=\"fa-solid fa-calendar-day w3-small\"></i>&nbsp;";
+  }//END-ico
+  
+  public function label_ico(): string {
+    return $this->ico().$this->label();
+  }
+
+  public static function caption(): string {
+    return 'Dia de la semana';
+  }//END-caption
+
+} // END-ENUM

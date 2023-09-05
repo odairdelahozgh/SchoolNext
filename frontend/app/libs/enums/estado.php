22d79098611900077dff3636ffaa27e453f881fd
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-05 06:42:43 -0500

    Update estado.php

--- frontend/app/libs/enums/estado.php
+++ frontend/app/libs/enums/estado.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * uso
+ *   $estado = Estado::tryFrom((int)$this->is_active) ?? Estado::Inactivo;
+ *   $estado->color()
+ *   $estado->label_ico()
+ *   $estado->label()
+ */
 enum Estado: int {
   Use EnumsFunciones;
 
@@ -8,26 +14,27 @@ enum Estado: int {
 
   public const Bueno = self::Activo;
 
-  public function label(): string {
+  public function label($attr_class=''): string {
     return match($this) {
-      static::Inactivo => 'Inactivo',
-      static::Activo   => 'Activo',
-      default          => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
+      static::Inactivo => "<span class=\"$attr_class\">Inactivo</span>",
+      static::Activo   => "<span class=\"$attr_class\">Activo</span>",
+      default          => throw new InvalidArgumentException(message: "Erroneo"),
     };
   }//END-label
   
-  public function ico(): string {
+  public function ico(bool $with_color=true, $attr_class=''): string {
     $ico = match($this) {
         static::Inactivo => 'fa-face-frown',
         static::Activo   => 'fa-face-smile',
         default          => 'fa-face-frown',
     };
-    return "<i class=\"fa-solid $ico w3-small\"></i>&nbsp;";
+    $color = ($with_color) ? 'style="color: '.self::color().'"' : '' ;
+    return "<i class=\"fa-solid $ico $attr_class\" $color></i>&nbsp;";
   }//END-ico
   
-  public function label_ico(): string {
-    return $this->ico().$this->label();
-  }
+  public function label_ico(bool $with_color=true, $attr_class=''): string {
+    return $this->ico($with_color, $attr_class).$this->label($attr_class);
+  } //END-label_ico
 
   public function color(): string {
     return match($this) {

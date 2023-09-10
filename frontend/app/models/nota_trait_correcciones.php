2d1201e5fa69e3b600b77da7bd6e968f33a3bf09
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-10 08:11:50 -0500

    Update nota_trait_correcciones.php

--- frontend/app/models/nota_trait_correcciones.php
+++ frontend/app/models/nota_trait_correcciones.php
@@ -3,13 +3,43 @@
 trait NotaTraitCorrecciones {
 
   public static function generarCalif_BySalonAsignatura(int $salon_id=1, int $asignatura_id=1) {
-    $Estudiantes = (new Estudiante())::filter( 'SELECT * FROM swb_estudiantes WHERE salon_id=?', [$salon_id] );
-    if ($Estudiantes) {
-      foreach ($Estudiantes as $key => $Estud) {
-        echo "$Estud :: $Estud->id<br>";
+    try {
+      $Salon = (new Salon())::get($salon_id);
+
+      $DQL = new OdaDql(__CLASS__);
+      $DQL->setFrom('sweb_estudiantes');
+      $Estudiantes = $DQL->where('t.is_active=1 AND t.salon_id=?')
+                      ->setParams([$salon_id])
+                      ->execute(true);
+          
+      if ($Estudiantes) {
+        foreach ($Estudiantes as $key => $Estud) {
+          OdaLog::debug("$Estud :: $Estud->id<br>");
+          $DQL = new OdaDql(__CLASS__);
+          $DQL->setFrom('sweb_notas');
+          $Now = new DateTime('now', new DateTimeZone('America/Bogota'));
+          $DQL->insert([
+            //'uuid' => $Estud->xxh3Hash(),
+            'annio' => 2023,
+            'periodo_id' => 3,
+            'grado_id' => $Salon->grado_id,
+            'salon_id' => $salon_id,
+            'asignatura_id' => $asignatura_id,
+            'estudiante_id' => $Estud->id,
+            'created_at' => $Now->format('Y-m-d H:i:s'),
+            'updated_at' => $Now->format('Y-m-d H:i:s'),
+            'created_by' => 1,
+            'updated_by' => 1,
+            ]
+          );
+          $DQL->execute(true);
+        }
+      } else {
+        OdaFlash::info('No hay registros');
       }
-    } else {
-      OdaFlash::info('No hay registros');
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
     }
   }
 

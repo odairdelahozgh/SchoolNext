558d17b595a6d7436d1aa2b9de59c42f16e21fd2
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-12 09:33:02 -0500

    Create dirgrupo_controller.php

---
+++ frontend/app/controllers/dirgrupo_controller.php
@@ -0,0 +1,66 @@
+<?php
+/**
+  * Controlador Docentes  
+  * @category App
+  * @package Controllers https://github.com/KumbiaPHP/Documentation/blob/master/es/controller.md
+  */
+
+require_once APP_PATH . '../vendor/autoload.php';
+//use Mpdf\Mpdf;
+
+class DocentesController extends AppController
+{
+
+  public function registros() {
+    try {
+      $this->page_action = 'Registros del Grupo';
+      $this->data = range((int)Config::get('config.academic.annio_actual'), 2021, -1);
+      //OdaLog::debug("mensaje","rastreo");
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+    View::select('registros/dg_registros_consoli');
+  }//END-seguimientos_grupo
+  
+  
+
+  public function seguimientos() {
+    try {
+      $this->page_action = 'Seguimientos del Grupo';
+      $this->data = (new usuario)->misGrupos();
+      //OdaLog::debug("mensaje","rastreo");
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+    View::select('seguimientos/dg_seguimientos_consoli');
+  }//END-seguimientos_grupo
+  
+
+  public function index(): void {
+    try {
+      $this->page_action = 'Direcci&oacute;n de Grupo';
+      $periodo = Session::get('periodo');
+      $this->data = (new usuario)->misGrupos();
+
+      for ($i=1; $i <=$periodo ; $i++) { 
+        $a_regs = (new Nota)::getNotasPromAnnioPeriodoSalon($i, $this->data[0]->id);
+        foreach ($a_regs as $key => $value) {
+          $this->arrData[$value->asignatura_nombre][$i]['avg'] = $value->avg;
+        }
+      }
+
+      $this->buttons = [];
+      foreach ($this->data as $key => $salon) {
+        $this->buttons[$key] = ['caption'=>$salon, 'action'=>"traer_data($salon->id)"];
+      }
+      
+      } catch (\Throwable $th) {
+        OdaFlash::error($th);
+      }
+      
+  }//END-direccion_grupo
+
+
+} // END CLASS

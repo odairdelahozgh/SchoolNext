74f3911f599efe3dead57f93dabfc5556f6e74f1
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-17 07:28:29 -0500

    Create instalar cumbia.php

---
+++ frontend/app/help/docs/instalar cumbia.php
@@ -0,0 +1,13 @@
+parece que mucha gente no usa composer para instalar Kumbia
+06:20
+es tan facil como
+06:22
+composer create-project kumbia/framework mi-dir-de-app
+06:22
+y si quieren pueden escoger la version
+06:23
+composer create-project kumbia/framework:1.1.5 mi-dir-de-app
+06:23
+o la ultima version de la 1.1
+06:24
+composer create-project kumbia/framework:^1.1 mi-dir-de-app

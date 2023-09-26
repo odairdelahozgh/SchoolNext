815218f769b3f7d23af5d6b820b48136fe2db53d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-26 14:17:20 -0500

    Update aspir_estatus.php

--- frontend/app/libs/enums/aspir_estatus.php
+++ frontend/app/libs/enums/aspir_estatus.php
@@ -3,7 +3,7 @@
 enum AspirEstatus: string {
   Use EnumsFunciones;
 
-  case Estudio   = 'Estudio';
+  case Estudio   = 'En Estudio';
   case Admitido  = 'Admitido';
   case Rechazado = 'Rechazado';
 
@@ -15,6 +15,32 @@ enum AspirEstatus: string {
       default           => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
     };
   }//END-label
+
+  public function ico(bool $with_color=true, $attr_class=''): string {
+    $ico = match($this) {
+        static::Estudio   => 'fa-user-clock',
+        static::Admitido  => 'fa-user-check',
+        static::Rechazado => 'fa-user-minus',
+        default           => 'fa-question',
+    };
+    $color = ($with_color) ? 'style="color: '.self::color().'"' : '' ;
+    return "<i class=\"fa-solid $ico $attr_class\" $color></i>&nbsp;";
+  } //END-ico
+  
+  public function color(): string {
+    return match($this) {
+        static::Estudio => 'blue',
+        static::Admitido => 'green',
+        static::Rechazado => 'red',
+        default    => 'red',
+    };
+  }//END-color
+
+
+  public function label_ico(bool $with_color=true, $attr_class=''): string {
+    return $this->ico($with_color, $attr_class).$this->label($attr_class);
+  } //END-label_ico
+
 
   public static function caption(): string {
     return 'Estado de la Solicitud';

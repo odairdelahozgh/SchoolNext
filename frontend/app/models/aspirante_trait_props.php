d9bef1f341ee985311a60539139ee31f14c7a8a8
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-28 14:45:07 -0500

    Update aspirante_trait_props.php

--- frontend/app/models/aspirante_trait_props.php
+++ frontend/app/models/aspirante_trait_props.php
@@ -24,7 +24,27 @@ trait AspiranteTraitProps {
     }
   } //END-is_pago_f
 
-  
+  public static function estatus_enum(string $estatus, bool $with_color=true, string $attr_class='', bool $show_ico=true) { 
+    try {
+      return (($show_ico) ? 
+        AspirEstatus::tryFrom($estatus)->label_ico($with_color, $attr_class) : 
+        AspirEstatus::tryFrom($estatus)->label($attr_class));
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  } //END-estatus_enum
+
+  public function estatus_f(): string {
+    try {
+      return self::estatus_enum((string)$this->estatus);
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  } //END-is_pago_f
+
+
   public function lnkPageEditAspirante(): string {
     try {
       return OdaTags::link(
@@ -40,14 +60,16 @@ trait AspiranteTraitProps {
 
 
   public function fecha_entrev_f() {
-    $date = new DateTimeImmutable($this->fecha_entrev);
-    return ( ($this->fecha_entrev=='0000-00-00 00:00:00') ? '' : $date->format('Y-M-d H:i:s'));
+    $timezone = new DateTimeZone("America/Bogota");
+    $date = new DateTimeImmutable($this->fecha_entrev, $timezone);
+    return ( ($this->fecha_entrev=='0000-00-00 00:00:00') ? '' : $date->format('M-d H:iA'));
   } //END-fecha_entrev_f
-
-
+  
+  
   public function fecha_eval_f() {
-      $date = new DateTimeImmutable($this->fecha_eval);
-      return ( ($this->fecha_eval=='0000-00-00 00:00:00') ? '' : $date->format('Y-M-d H:i:s'));
+    $timezone = new DateTimeZone("America/Bogota");
+    $date = new DateTimeImmutable($this->fecha_eval, $timezone);
+      return ( ($this->fecha_eval=='0000-00-00 00:00:00') ? '' : $date->format('M-d H:iA'));
   } //END-fecha_eval_f
 
 

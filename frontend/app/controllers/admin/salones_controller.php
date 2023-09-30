3ae254f2bfd02fbf3326363173ff9b772c2566a5
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-09-30 12:47:09 -0500

    Update salones_controller.php

--- frontend/app/controllers/admin/salones_controller.php
+++ frontend/app/controllers/admin/salones_controller.php
@@ -8,4 +8,11 @@
 class SalonesController extends ScaffoldController
 {
 
+  public function setupCalificarSalon(int $salon_id) {
+    $Salon = new Salon();
+    $result = $Salon->setupCalificarSalon($salon_id);
+    OdaFlash::info("Registros Incluidos: $result");
+    return Redirect::to("admin/$this->controller_name/index");
+  }
+
 } // END CLASS

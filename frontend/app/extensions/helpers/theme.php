323c98f5be733b19c6b9892e96cc0ab8f81b192b
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-11-08 20:16:02 -0500

    Create theme.php

---
+++ frontend/app/extensions/helpers/theme.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Theme: Helper.
+ * 
+ * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
+ * @category Helper.
+ * @source   frontend\app\extensions\helpers
+ */
+
+
+class Theme {
+  
+  use ThemeTraitEnums, ThemeTraitButtons;
+
+  public function __construct() {
+    
+  }
+
+} //END-Class

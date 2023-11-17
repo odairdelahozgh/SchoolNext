0947f846cd23e424c97f1599c3072700dee3d1d7
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-11-17 11:39:01 -0500

    Update cargas_controller.php

--- frontend/app/controllers/admin/cargas_controller.php
+++ frontend/app/controllers/admin/cargas_controller.php
@@ -7,5 +7,19 @@
   
 class CargasController extends ScaffoldController
 {
+  
+  public function exportListasDeClaseProfesorPdf(int $periodo_id) {
+    try {
+      $this->data = (new SalAsigProf())->getCarga($this->user_id);
+      $this->arrData['periodo'] = $periodo_id;
+      $this->file_tipo = 'Listas de clase';
+      $this->file_name = OdaUtils::getSlug("lista-de-clase-periodo-$periodo_id");
+      $this->file_title = "Lista de Clase PDF - Periodo $periodo_id";
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+    View::select(view: "lista_clase.pdf", template: null);
+  } //END-exportListasDeClaseProfesorPdf
+
 
 } // END CLASS

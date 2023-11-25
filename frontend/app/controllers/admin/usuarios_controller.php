856c6e1421d770b195397d772b64ec76ffd1cc25
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-11-25 06:11:09 -0500

    Update usuarios_controller.php

--- frontend/app/controllers/admin/usuarios_controller.php
+++ frontend/app/controllers/admin/usuarios_controller.php
@@ -2,4 +2,17 @@
 
 class UsuariosController extends ScaffoldController
 {
+    
+  public function setPassword(int $id) {
+    try {
+      $usuario = (new Usuario())::get($id);
+      $usuario->setPassword();
+  
+      return Redirect::to("admin/$this->controller_name/index");
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  }
+
 }

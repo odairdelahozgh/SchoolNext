57e7039922acdbbd7bcb54e43a2764e8ccd88e09
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-11-25 06:11:05 -0500

    Update empleados_controller.php

--- frontend/app/controllers/admin/empleados_controller.php
+++ frontend/app/controllers/admin/empleados_controller.php
@@ -2,11 +2,5 @@
 
 class EmpleadosController extends ScaffoldController
 {
-  
-  public function setPassDocente(int $id) {
-    $user = (new Empleado)::get($id);
-    $user->setPassDocente();
-    return Redirect::to("admin/$this->controller_name/index");
-  }
 
 }

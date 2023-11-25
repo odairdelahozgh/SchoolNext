7c1c5c6aa6f67cdc2ecd71032e1060c8dfa5bf6d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-11-25 06:09:10 -0500

    Update empleado.php

--- frontend/app/models/empleado.php
+++ frontend/app/models/empleado.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Modelo Usuario
+ * Modelo Empleado
  * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
  * @category App
  * @package  Models https://github.com/KumbiaPHP/ActiveRecord
@@ -14,16 +14,10 @@
  */
 
   
-class Empleado extends LiteRecord {
+//class Empleado extends LiteRecord {
+class Empleado extends Usuario {
 
-  use EmpleadoTraitSetUp;
-
-  public function __construct() {
-    parent::__construct();
-    self::$table = Config::get('tablas.usuario');
-    self::$order_by_default = 't.is_active DESC, t.apellido1, t.apellido2, t.nombres';
-    $this->setUp();
-  } //END-__construct
+  //use EmpleadoTraitSetUp;
 
   public function getList(int|bool $estado=null, $select='*', string|bool $order_by=null) { 
     $DQL = new OdaDql('usuario');
@@ -31,7 +25,7 @@ class Empleado extends LiteRecord {
       ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'usuario_nombre')
       ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'nombre')
       ->where('t.username<>t.documento')
-      ->orderBy(self::$order_by_default);
+      ->orderBy(self::$_order_by_defa);
 
     if (!is_null($estado)) { $DQL->andWhere("t.is_active=$estado"); }
 
@@ -40,37 +34,6 @@ class Empleado extends LiteRecord {
     }
     return $DQL->execute();
   }//END-getList
-
-  public function getLista() { 
-    $DQL = new OdaDql(__CLASS__);
-    $DQL->select("t.*")
-      ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'usuario_nombre')
-      ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'nombre')
-      ->where('t.username <> t.documento')
-      ->orderBy(self::$order_by_default);
-
-    return $DQL->execute();
-  }//END-getList
-
-
-public function setPassDocente() {
-  try {
-    $salt = md5(rand(100000, 999999).$this->username);
-    $this->salt = $salt;
-    
-    $pass = substr($this->documento, -4);
-    $password_salt = hash('sha1', $salt . $pass);
-    $this->password = $password_salt;
-    $this->update();
-
-    //$rec_count = $this::query("UPDATE ".self::$table." SET password=? WHERE id=? ", [$password_salt, $this->id])->rowCount();
-    //OdaFlash::info("\nUsername: $this->username \nId: $this->id \nDocumento: $this->documento \nPassword:  $pass \nSALT: $this->salt \nPassword SALT $password_salt \nAfect: $rec_count", true);
-  } catch (\Throwable $th) {
-    OdaFlash::error($th);
-  }
-
-} //END-setPassword
-
 
 
 } //END-CLASS

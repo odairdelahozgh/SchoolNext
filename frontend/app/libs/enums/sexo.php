c46fb943033485aeed9f5e3c2e309d3356fb2a03
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-12-06 05:42:10 -0500

    Update sexo.php

--- frontend/app/libs/enums/sexo.php
+++ frontend/app/libs/enums/sexo.php
@@ -23,15 +23,15 @@ enum Sexo: string {
     return "<i class=\"fa-solid $ico w3-small\"></i>&nbsp;";
   }//END-ico
   
-  public function label_ico(): string {
-    return $this->ico().$this->label();
+  public function label_ico(bool $abrev = false): string {
+    return $this->ico().$this->label($abrev);
   }//END-label_ico
 
   public function color(): string {
     return match($this) {
-        static::Masculino => 'dodgerblue',
-        static::Femenino  => 'seagreen',
-        default           => 'coral',
+        static::Masculino => 'dodgerblue', // dodgerblue Hex Code:	#1E90FF
+        static::Femenino  => 'seagreen', // seagreen hex code #2E8B57
+        default           => 'coral', // coral hex code is #FF7F50
     };
   }//END-color
 

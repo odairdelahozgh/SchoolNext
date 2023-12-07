cf191ca8f76325088acb63d02f3559605c9e4de6
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-12-06 21:45:28 -0500

    Update docentes.php

--- frontend/app/config/docentes.php
+++ frontend/app/config/docentes.php
@@ -1,19 +1,19 @@
 <?php
 // filtros 
-// [] = todos, [-1] ninguno, [1,2] filtro
+// [] = todos, [0] ninguno, [1,2] filtro
 
 return [
   'calificaciones' => [
-    'periodos'    => [-1],
-    'salones'     => [-1],
-    'asignaturas' => [-1],
-    'usuarios'    => [-1],
+    'periodos'    => [0],
+    'salones'     => [],
+    'asignaturas' => [],
+    'usuarios'    => [],
   ],
   'seguimientos' => [
-    'periodos'    => [-1],
-    'salones'     => [-1],
-    'asignaturas' => [-1],
-    'usuarios'    => [-1],
+    'periodos'    => [0],
+    'salones'     => [0],
+    'asignaturas' => [0],
+    'usuarios'    => [0],
   ],
   'planes_apoyo' => [
     'periodos'    => [5],

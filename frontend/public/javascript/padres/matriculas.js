ef5538fcadbe4f0e2e650fdf99a13e1bc73b16d4
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-12-07 03:46:04 -0500

    Create matriculas.js

---
+++ frontend/public/javascript/padres/matriculas.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('btn-0').click();
+});
+
+function traer_data(estudiante_id) {
+  let ruta_base = document.getElementById('public_path').innerHTML.trim();
+  console.clear();
+
+  // ========================================================================      
+  const div_info_estudiante = document.getElementById("info_estudiante");
+  fetch(ruta_base+'api/estudiantes/singleid/'+estudiante_id)
+  .then((res) => res.json())
+  .then(data_estudiante => {
+    div_info_estudiante.innerHTML = template_datos_estud(data_estudiante, ruta_base, salon_nombre);
+  });
+  
+  // matriculas
+  const div_matriculas = document.getElementById('proceso_matriculas');
+  if (1==document.getElementById('ver_matriculas').value) { 
+    div_matriculas.innerHTML = `
+    <div>
+      <h2 class="w3-panel w3-theme w3-round-xlarge">Proceso de Matrículas 2024</h2>
+    </div>`;
+  }
+
+} //END-traer_data
+
+
+
+function template_proceso_matriculas(data, ruta) {
+  return `
+    <h2 class="w3-panel w3-theme w3-round-xlarge">Proceso de Matrículas</h2>
+  `;
+} //END-template_proceso_matriculas
+
+
+function template_datos_estud(data, ruta, salon_nombre) {
+  return `
+  <div class="w3-card-1">
+
+    <header class="w3-container w3-light-blue  w3-round-xlarge">
+      <h2 class="w3-panel w3-round-xlarge">[${data.id}] ${data.nombres} ${data.apellido1} ${data.apellido2} [${salon_nombre}]</h2>
+    </header>
+
+    <div class="w3-container">
+      Última Referencia de Pago:<br> Mes ${nombreMes(data.mes_pagado)} de ${data.annio_pagado} 
+    </div>
+
+    <footer class="w3-container w3-light-blue">
+      <h5></h5>
+    </footer>
+  </div>
+  `;
+} //END-template_datos_estud
+

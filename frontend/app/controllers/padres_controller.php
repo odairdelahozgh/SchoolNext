06050af07c6211103c752a3a0ab0f43dec61344f
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-12-12 22:37:33 -0500

    Update padres_controller.php

--- frontend/app/controllers/padres_controller.php
+++ frontend/app/controllers/padres_controller.php
@@ -42,9 +42,47 @@ class PadresController extends AppController
   
   
   public function matriculas(): void {
-    $this->page_action = 'Matr&iacute;culas';
+    $this->page_action = 'Matr&iacute;culas Año '.Config::get('matriculas.annio_mat');
+    
+    $arr_padres_id = [22204, 21985, 22065];
+    $rnd =$arr_padres_id[rand(0, count($arr_padres_id)-1)]; 
+    //$user_id = (1!=$this->user_id) ? $this->user_id : $rnd ; // simular un usuario de padres
+    $user_id = 22482;
+    $this->data = (new Estudiante)->getListPadres($user_id);
+    foreach ($this->data as $estudiante) {
+      $Adjuntos = (new EstudianteAdjuntos())::filter("WHERE estudiante_id=?", [$estudiante->id]);
+      if (!$Adjuntos) {
+        $Adjuntos = (new EstudianteAdjuntos());
+        $Adjuntos->save([
+          'estudiante_id'=>$estudiante->id, 
+          'uuid'=>$Adjuntos->xxh3Hash(), 
+          'created_at'=> '',
+          'updated_at'=> '',
+          'created_by'=> $user_id,
+          'updated_by'=> $user_id,
+          'estado_archivo1'=> EstadoAdjuntos::Revision->value,
+          'estado_archivo2'=> EstadoAdjuntos::Revision->value,
+          'estado_archivo3'=> EstadoAdjuntos::Revision->value,
+          'estado_archivo4'=> EstadoAdjuntos::Revision->value,
+          'estado_archivo5'=> EstadoAdjuntos::Revision->value,
+          'estado_archivo6'=> EstadoAdjuntos::Revision->value,
+        ]);
+      }
+    }
+
     View::select('matriculas/index');
   } //END-matriculas
+  
+  public function subirArchivos($estudiante_id): void {
+    try {
+      $this->page_action = 'Subir Archivos';
+      $this->arrData['Adjuntos'] = (new EstudianteAdjuntos())::filter("WHERE estudiante_id=?", [$estudiante_id]);
+      View::select('matriculas/upload');
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+  } //END-subirArchivos
 
 
 } // END CLASS

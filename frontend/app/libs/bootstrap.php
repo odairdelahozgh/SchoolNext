64be6f70937f62d75b92de9983e7f11f64daef04
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2023-12-12 22:31:42 -0500

    Update bootstrap.php

--- frontend/app/libs/bootstrap.php
+++ frontend/app/libs/bootstrap.php
@@ -3,4 +3,26 @@
 // Para cargar cambia en public/index.php el require del bootstrap a app
 
 // Arranca KumbiaPHP
+
+/* HOSTING
+const ABS_PUBLIC_PATH = '/home/windsor1/schoolnext.windsorschool.edu.co/';
+*/
+
+
+const APP_ROOT_PRIVATE = '/home/windsor1/schoolnext_private/';
+define('VENDOR_PATH', APP_PATH.'../vendor/');
+//const VENDOR_PATH      = APP_ROOT_PRIVATE.'frontend/vendor/';
+
+const ABS_PUBLIC_PATH = 'D:\schoolnext\frontend\public'; // '/home/windsor1/schoolnext.windsorschool.edu.co/';
+
+const SCHOOLWEB_PUBLIC_PATH = 'https://windsortemp.schoolnext.space/';
+const APP_NAME= "SchoolNEXT>>";
+
+const FILE_UPLOAD_PATH   = PUBLIC_PATH.'files/upload/';
+const FILE_DOWNLOAD_PATH = PUBLIC_PATH.'files/download/';
+
+const IMG_UPLOAD_PATH    = PUBLIC_PATH.'img/upload/';
+const IMG_DOWNLOAD_PATH  = PUBLIC_PATH.'img/download/';
+const IMG_ESTUDIANTES_PATH  = IMG_UPLOAD_PATH.'estudiantes';
+
 require_once CORE_PATH . 'kumbia/bootstrap.php';

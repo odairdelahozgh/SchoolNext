291b9e97a288b625e607f390cd5c60f4225b3bef
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-03 12:07:37 -0500

    Update salon_trait_props.php

--- frontend/app/models/salon_trait_props.php
+++ frontend/app/models/salon_trait_props.php
@@ -18,4 +18,26 @@ trait SalonTraitProps {
     return (new Usuario)::get($this->director_id??0);
   }
 
+  public static function getSalonesArray() { 
+    $arrResult = [];
+    foreach ((new Salon())->getList(1) as $salon) { $arrResult[$salon->id] = $salon->nombre; }
+    return $arrResult;
+  } //END-getSalonesArray
+
+
+  public static function getSelectSalones(string $id, string $name, int $salon_selected_id=0): string { 
+    $opts = '';
+    $grado_ant = 0;
+    foreach ((new Salon())->getList(1) as $key => $salon) {
+      if ($salon->grado_id <> $grado_ant) {
+        $opts .= ((0==$key) ? "<optgroup label=\"$salon->grado_nombre\">" : "</optgroup><optgroup label=\"$salon->grado_nombre\">");
+      }
+      $salon_sel = ($salon->id == $salon_selected_id) ? 'selected' : '' ;
+      $opts .= "<option value=\"$salon->id\" $salon_sel>$salon->nombre</option>";
+      $grado_ant = $salon->grado_id;
+    }
+    return "<select id=\"$id\" name=\"$name\"  class=\"w3-input w3-border\">$opts</select>";
+  } //END-getSelectSalones
+
+
 } //END-Trait

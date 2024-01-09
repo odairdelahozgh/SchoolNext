14d315ec776b77c291f4c8f545785e76dfd9b2ad
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-09 05:10:50 -0500

    Update estado_matricula.php

--- frontend/app/libs/enums/estado_matricula.php
+++ frontend/app/libs/enums/estado_matricula.php
@@ -12,7 +12,8 @@ enum EstadoMatricula: int {
   case FaltaFirmas = 7;
   case Terminado = 8;
   
-  public function label(): string {
+  public function label(): string 
+  {
     return match($this) {
       static::Bloqueado       => 'Bloqueado x Contabilidad',
       static::NoPromovido     => 'Estudiante No Promovido', // estan pendientes de notas 
@@ -25,18 +26,37 @@ enum EstadoMatricula: int {
       static::Terminado       => 'Proceso Terminado',
       default => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
     };
-  }//END-label
+  }
 
-  public function ico(): string {
+  public function comments(): string 
+  {
+    return match($this) {
+      static::Bloqueado       => 'Debe comunicarse con el depratamento de contabilidad',
+      static::NoPromovido     => 'EL estudiante está pendiente de validar planes de apoyo del año anterior',
+      static::SinDocumentos   => 'Debe subir la totalidad de los documentos exigidos',
+      static::DocIncompletos  => 'Debe subir los documentos faltantes',
+      static::DocEnRevision   => 'Una vez que el funcionario encargado revise los documentos, pasará al siguiente paso',
+      static::DocRechazados   => 'Debe subir los documentos rechazados nuevamente',
+      static::DocAprobados    => 'Falta ASIGNAR NÚMERO DE MATRÍCULA',
+      static::FaltaFirmas     => 'Debe firmar los documentos de matriciula, dirigirse a la institución para realizarlo',
+      static::Terminado       => 'El estudiante ya se encuentra matriculado.',
+      default => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
+    };
+  }
+
+  public function ico(): string 
+  {
     return ''; // "<i class=\"fa-solid fa-calendar-day w3-small\"></i>&nbsp;";
-  }//END-ico
+  }
   
-  public function label_ico(): string {
+  public function label_ico(): string 
+  {
     return $this->ico().$this->label();
   }
 
-  public static function caption(): string {
+  public static function caption(): string 
+  {
     return 'Estado del Proceso de Matriculas';
-  }//END-caption
+  }
 
 } // END-ENUM

7086c9ccebdb45e8da8e7861470fcb65401cf734
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-10 12:26:04 -0500

    Create mpdf_docs_matricula.php

---
+++ frontend/app/libs/mpdf_docs_matricula.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Creación de Archivos PDF con mPDF
+ * @category   SchoolNext
+ * @package    Libs
+ */
+
+require_once VENDOR_PATH . 'autoload.php';
+use Mpdf\Mpdf;
+
+class MpdfDocsMatricula extends Mpdf {
+  
+  public function __construct(array $config = [], $container = null) 
+  {
+    parent::__construct($config, $container);
+    $this->SetSubject('DOCUMENTOS DE MATRICULA');
+    $this->SetCreator(APP_NAME.' '.Config::get('config.construxzion.name'));
+    $this->SetAuthor(Config::get('config.institution.nombre'));
+    $this->SetTitle('DOCUMENTOS DE MATRICULA');
+    $this->SetDefaultFont('helvetica');
+    $this->SetDefaultFontSize(10);
+    $this->SetMargins(10, 10, 48 );
+    $this->SetDisplayMode('fullpage');
+    $this->watermark_font = 'DejaVuSansCondensed';
+
+    $logo = '<a href="'.Config::get('config.institution.website').'" target="_blank">
+      <img src="'.PUBLIC_PATH.'img/ws_logo.png" alt="Logo" height="40"> </a>';
+    $this->SetHTMLHeader("
+      <div style=\"text-align: center; font-weight: bold;\">
+        $logo <br> <h3>$this->title</h3>
+      </div>"
+    );
+
+    $this->SetHTMLFooter('
+    <table width="100%">
+        <tr>
+            <td width="33%"></td>
+            <td width="33%" align="center">{PAGENO}/{nbpg}</td>
+            <td width="33%" style="text-align: right;"></td>
+        </tr>
+    </table>');
+  }
+
+}

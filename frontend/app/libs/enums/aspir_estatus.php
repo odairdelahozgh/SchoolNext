bf8cd156101e5bc76c7aaa1c96a5b59532d699be
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-16 08:42:10 -0500

    Update aspir_estatus.php

--- frontend/app/libs/enums/aspir_estatus.php
+++ frontend/app/libs/enums/aspir_estatus.php
@@ -3,47 +3,59 @@
 enum AspirEstatus: string {
   Use EnumsFunciones;
 
-  case Estudio   = 'En Estudio';
-  case Admitido  = 'Admitido';
-  case Rechazado = 'Rechazado';
+  case Estudio    = 'En Estudio';
+  case Admitido   = 'Admitido';
+  case Rechazado  = 'Rechazado';
+  case Abandono   = 'Abandonó';
+  case Trasladado = 'Trasladado';
 
-  public function label(bool $abrev = false): string {
+  public function label(bool $abrev = false): string 
+  {
     return match($this) {
-      static::Estudio   => (($abrev)?'ES':'En Estudio'),
-      static::Admitido  => (($abrev)?'AD':'Admitido'),
-      static::Rechazado => (($abrev)?'RE':'Rechazado'),
-      default           => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
+      static::Estudio     => (($abrev) ? 'ES' : 'En Estudio'),
+      static::Admitido    => (($abrev) ? 'AD' : 'Admitido'),
+      static::Rechazado   => (($abrev) ? 'RE' : 'Rechazado'),
+      static::Abandono    => (($abrev) ? 'AB' : 'Abandonó'),
+      static::Trasladado  => (($abrev) ? 'TR' : 'Trasladado'),
+      default => throw new InvalidArgumentException(message: "{$this->caption()} Erroneo"),
     };
-  }//END-label
+  }
 
-  public function ico(bool $with_color=true, $attr_class=''): string {
+  public function ico(bool $with_color=true, $attr_class=''): string 
+  {
     $ico = match($this) {
-        static::Estudio   => 'fa-user-clock',
-        static::Admitido  => 'fa-user-check',
-        static::Rechazado => 'fa-user-minus',
-        default           => 'fa-question',
+        static::Estudio     => 'fa-user-clock',
+        static::Admitido    => 'fa-user-check',
+        static::Rechazado   => 'fa-user-minus',
+        static::Abandono    => 'fa-user-minus',
+        static::Trasladado  => 'fa-user-graduate',
+        default => 'fa-question',
     };
     $color = ($with_color) ? 'style="color: '.self::color().'"' : '' ;
     return "<i class=\"fa-solid $ico $attr_class\" $color></i>&nbsp;";
-  } //END-ico
+  }
   
-  public function color(): string {
+  public function color(): string 
+  {
     return match($this) {
         static::Estudio => 'dodgerblue',
         static::Admitido => 'seagreen',
         static::Rechazado => 'crimson',
-        default    => 'crimson',
+        static::Abandono => 'crimson',
+        static::Trasladado => 'yellow',
+        default => 'dodgerblue',
     };
-  }//END-color
-
-
-  public function label_ico(bool $with_color=true, $attr_class=''): string {
+  }
+  
+  public function label_ico(bool $with_color=true, $attr_class=''): string 
+  {
     return $this->ico($with_color, $attr_class).$this->label($attr_class);
-  } //END-label_ico
+  }
 
 
-  public static function caption(): string {
+  public static function caption(): string 
+  {
     return 'Estado de la Solicitud';
-  }//END-caption
+  }
 
 } //END-ENUM

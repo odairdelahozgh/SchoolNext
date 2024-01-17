259ed1151519dcf3606dea086269de3fa26aaf5c
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-17 05:43:36 -0500

    Create bootstrap5.php

---
+++ frontend/app/extensions/helpers/bootstrap5/bootstrap5.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * $alert = new BootstrapAlert('Este es un mensaje de alerta', 'success');
+ * echo $alert->generateAlert();
+ * echo $alert->generateDismissableAlert();
+ * @link https://getbootstrap.com/docs/5.0/components/alerts/
+ */
+class Bootstrap { 
+  
+}

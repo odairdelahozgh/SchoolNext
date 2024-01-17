169d27ebc6ee420e51b4b7ae39fad7360553299f
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-17 05:11:33 -0500

    Update area_trait_set_up.php

--- frontend/app/models/area_trait_set_up.php
+++ frontend/app/models/area_trait_set_up.php
@@ -1,33 +1,11 @@
 <?php
-require_once VENDOR_PATH.'autoload.php';
-use Respect\Validation\Validator as validar;
-use Respect\Validation\Exceptions\NestedValidationException;
-// https://respect-validation.readthedocs.io/en/latest/
 
 trait AreaTraitSetUp {
   
-  use TraitUuid, TraitForms;
+  use TraitUuid, TraitForms, TraitValidar;
 
   public function __toString() { return $this->nombre; }
-  
 
-  public function validar($input_post) {
-    Session::set('error_validacion', '');
-    try{
-      validar::number()->length(1)->min(0)->max(1)->assert($input_post['is_active']);
-      validar::number()->assert($input_post['orden']);
-      validar::alpha()->assert($input_post['nombre']);
-      return true;
-    } catch(NestedValidationException $exception) {
-      Session::set('error_validacion', $exception->getFullMessage());
-      return false;
-    }
-  } //END-validar
-
-
-  /**
-   * CONFIGURACIÓN DEL MODELO
-   */
   private function setUp() {
 
     self::$_fields_show = [
@@ -69,18 +47,6 @@ trait AreaTraitSetUp {
     self::$_placeholders = [
       'nombre'          => 'nombre del area',
       'orden'           => 'ordenamiento interno',
-    ];
-  
-    // numeric, int, maxlength, length, range, select, email, url, ip, required, alphanum, alpha, date, pattern, decimal, equal
-    self::$_rules_validators = [
-      'nombre' => [
-        'required' => [ 'error' => 'es requerido.' ],
-        'alpha'    => [ 'error' => 'debe contener solo letras.' ],
-      ],
-      'orden'  => [
-        'required' => [ 'error' => 'es requerido.' ],
-        'int'      => [ 'error' => 'debe contener solo numeros enteros.' ],
-      ],
     ];
 
   }

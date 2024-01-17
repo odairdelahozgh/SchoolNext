6170a6eabc11da2821df3395a27b88d30a5aa1a6
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-01-17 05:11:57 -0500

    Update usuario_trait_set_up.php

--- frontend/app/models/usuario_trait_set_up.php
+++ frontend/app/models/usuario_trait_set_up.php
@@ -8,7 +8,8 @@ trait UsuarioTraitSetUp {
   
   use TraitUuid, TraitForms, UsuarioTraitProps;
   
-  public function validar($input_post) {
+  public function validar($input_post) 
+  {
     Session::set('error_validacion', '');
     try{
       validar::alnum()->noWhitespace()->length(3, 20)->assert($input_post['username']);
@@ -19,21 +20,11 @@ trait UsuarioTraitSetUp {
       Session::set('error_validacion', $exception->getFullMessage());
       return false;
     }
-  } //END-validar
-
-/* 
-* id, uuid, username, roll, nombres, apellido1, apellido2, photo, profesion, direccion, documento, email, 
-* telefono1, telefono2, cargo, sexo, fecha_nac, fecha_ing, fecha_ret, observacion, is_carga_acad_ok, 
-* is_partner, usuario_instit, clave_instit, theme, 
-* algorithm, salt, password, is_super_admin, last_login, forgot_password_code, 
-* is_active, created_at, updated_at,  
-*/
+  }
   
-  /**
-   * CONFIGURACIÓN DEL MODELO
-   */
-  private function setUp() {
 
+  private function setUp() 
+  {
     self::$_fields_show['all']      = ['id', 'uuid', 'username', 'roll', 'nombres', 'apellido1', 'apellido2', 'photo', 'profesion', 'direccion', 'documento', 'email', 'telefono1', 'telefono2', 'cargo', 'sexo', 'fecha_nac', 'fecha_ing', 'fecha_ret', 'observacion', 'is_carga_acad_ok', 'is_partner', 'usuario_instit', 'clave_instit', 'theme', 'algorithm', 'salt', 'password', 'is_super_admin', 'last_login', 'forgot_password_code', 'is_active', 'created_at', 'updated_at'];
     self::$_fields_show['index']    = ['is_active', 'id', 'username', 'roll', 'nombres', 'apellido1', 'apellido2', 'documento','usuario_instit', 'clave_instit'];
     self::$_fields_show['create']   = ['username', 'roll', 'nombres', 'apellido1', 'apellido2', 'documento', 'email', 'direccion', 'telefono1', 'telefono2', 'fecha_nac', 'fecha_ing', 'fecha_ret', 'usuario_instit', 'clave_instit', 'theme', 'is_active', 'algorithm' ];
@@ -41,45 +32,44 @@ trait UsuarioTraitSetUp {
     self::$_fields_show['editUuid'] = ['username', 'roll', 'nombres', 'apellido1', 'apellido2', 'documento', 'email', 'direccion', 'telefono1', 'telefono2', 'fecha_nac', 'fecha_ing', 'fecha_ret', 'usuario_instit', 'clave_instit', 'theme', 'is_active', 'algorithm', 'uuid' ];
   
     self::$_attribs = [
-      'id'       => 'required',
-      'uuid'     => 'required',
+      'id'  => 'required',
+      'uuid'  => 'required',
     ];
   
     self::$_defaults = [
-      'is_active'       => 1,
+      'is_active' => 1,
     ];
   
     self::$_helps = [
-      'fecha_nac'          => 'dd/mm/aaaa - Ejemplo: 26/07/1976',
-      'documento'          => 'Advertencia!! sin puntos, sin comas.',
-      
-      'is_active'    => 'Indica si está activo el registro.',
+      'fecha_nac' => 'dd/mm/aaaa - Ejemplo: 26/07/1976',
+      'documento' => 'Advertencia!! sin puntos, sin comas.',
+      'is_active' => 'Indica si está activo el registro.',
     ];
   
     self::$_labels = [
-      'username'   => 'Nombre de Usuario:',
-      'roll'       => 'Roles de Usuario:',
+      'username'  => 'Nombre de Usuario:',
+      'roll'  => 'Roles de Usuario:',
 
-      'nombres'    => 'Nombre(s):',
-      'apellido1'  => 'Primer Apellido:',
-      'apellido2'  => 'Segundo Apellido:',
-      'fecha_nac'  => 'Fecha Nacimiento:',
+      'nombres' => 'Nombre(s):',
+      'apellido1' => 'Primer Apellido:',
+      'apellido2' => 'Segundo Apellido:',
+      'fecha_nac' => 'Fecha Nacimiento:',
       
-      'direccion'  => 'Dirección:',
-      'cargo'      => 'Cargo:',
-      'documento'  => 'Nro. Documento:',
-      'telefono1'  => 'Teléfono Principal:',
-      'telefono2'  => 'Teléfono Secundario:',
-      'sexo'       => 'Sexo:',
+      'direccion' => 'Dirección:',
+      'cargo' => 'Cargo:',
+      'documento' => 'Nro. Documento:',
+      'telefono1' => 'Teléfono Principal:',
+      'telefono2' => 'Teléfono Secundario:',
+      'sexo'  => 'Sexo:',
       
-      'profesion'         => 'Profesión:',
+      'profesion' => 'Profesión:',
       'is_carga_acad_ok'  => 'Carga Académicas:',
       
-      'is_active'       => 'Está Activo? ',
-      'created_at'      => 'Creado el',
-      'created_by'      => 'Creado por',
-      'updated_at'      => 'Actualizado el',
-      'updated_by'      => 'Actualizado por',
+      'is_active' => 'Está Activo? ',
+      'created_at'  => 'Creado el',
+      'created_by'  => 'Creado por',
+      'updated_at'  => 'Actualizado el',
+      'updated_by'  => 'Actualizado por',
     ];
   
     self::$_placeholders = [
@@ -103,6 +93,7 @@ trait UsuarioTraitSetUp {
         ], 
         */
     ];
-
   }
-} //END-SetUp
+
+
+}

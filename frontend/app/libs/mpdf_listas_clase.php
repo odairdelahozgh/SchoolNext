670d2e451ef7391e542813f4acba4e2b13beddcc
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-07 14:45:56 -0500

    Update mpdf_listas_clase.php

--- frontend/app/libs/mpdf_listas_clase.php
+++ frontend/app/libs/mpdf_listas_clase.php
@@ -4,22 +4,25 @@
  * @category   SchoolNext
  * @package    Libs
  */
-
 require_once VENDOR_PATH . 'autoload.php';
 use Mpdf\Mpdf;
 
-class MpdfListasClase extends Mpdf {
+class MpdfListasClase extends Mpdf 
+{
   
-  public function __construct(array $config = [], $container = null) {
+  public function __construct(array $config = [], $container = null) 
+  {
     parent::__construct($config, $container);
-    
-    $this->SetSubject('LISTAS DE CLASE');
-    $this->SetCreator(APP_NAME.' '.Config::get('config.construxzion.name'));
-    $this->SetAuthor(Config::get('config.institution.nombre'));
+
     $this->SetTitle('LISTAS DE CLASE');
+    $this->SetAuthor(Config::get('config.institution.nombre'));
+    $this->SetCreator(APP_NAME.' '.Config::get('config.construxzion.name'));
+    $this->SetSubject('LISTAS DE CLASE');
+    $this->SetKeywords('listas, clase');
+
     $this->SetDefaultFont('helvetica');
     $this->SetDefaultFontSize(9);
-    $this->SetMargins(10, 5, 48 );
+    $this->SetMargins(15, 5, 30 );
     $this->SetDisplayMode('fullpage');
     $this->watermark_font = 'DejaVuSansCondensed';
 
@@ -27,7 +30,6 @@ class MpdfListasClase extends Mpdf {
       <img src="'.PUBLIC_PATH.'img/ws_logo.png" alt="Logo" height="40"> </a>';
     $this->SetHTMLHeader("
     <div style=\"text-align: center; font-weight: bold;\"> $logo </div>");
-
     //<td width="33%">{DATE j-m-Y}</td>
     $this->SetHTMLFooter('
     <table width="100%">
@@ -37,45 +39,59 @@ class MpdfListasClase extends Mpdf {
             <td width="33%" style="text-align: right;">Listas de Clase</td>
         </tr>
     </table>');
-    
-  } //END-__construct  
+  }
 
 
-  public function encabezadoBloque(array $Params = []): string {
-    //[ $profesor_nombre, $periodo, $annio, $salon ] = $Params;
-    
+  public function encabezado(&$tabla): void
+  {
+    $tabla->setHead(
+      [
+        'No.',
+        'Estudiante',
+        "Nota",
+        "Prom",
+        "Partici",
+        "Tareas",
+        "Ev. Oral",
+        "Ev. Escri",
+        "clase",
+        "Actitud",
+        "Final",
+        "P.A.",
+        "Asist",
+      ],
+      '',
+      ['style="width: 0.5cm;"', 'style="width: 6cm;"', 'style="width: 0.8cm;"']
+    );
+  }
+  
+
+  public function cuerpo(&$tabla, $key, $nota): void
+  {  ///estudiante_nombre,asignatura_nombre
     $cols = [
-      'No.',
-      'Estudiante',
-      "Notas",
-      "Promedio",
-      "Participación",
-      "Tareas-Talleres",
-      "Eval. Orales",
-      "Eval. Escritas",
-      "Trab. clase",
-      "Act",
-      "Final",
-      "P.A.",
-      "Asistencia",
-    ]; 
+      ($key+1),
+      $nota->estudiante_nombre,
+      '',
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ];
+    $tabla->addRow($cols);
+  }
+
+
+  public function pie(): string
+  {
+    return '<hr>'.str_repeat('<br>', 2);
+  }
+
   
-    $head = new OdaTable(_attrs: 'bgcolor="red" color="white" cellspacing="0" cellpadding="0" bordercolor="white"  border="1" width="100%"');
-    $head->addRow($cols);
-    return $head;
-  } //END-encabezadoBloque
-  
-  public function pieBloque(array $Params = []): string {
-    //[ $img_tabla_rango, $firma_director, $nombre_director ] = $Params;
-    
-    $col1 = 'clo1';
-    $col2 = 'clo2';
-    $col3 = 'clo3';
-    $foot = new OdaTable('style="width: 100%;"');
-    $foot->addRow( [$col1, $col2, $col3], attrs_td: []);
-    
-    return str_repeat('<br>', 2) .$foot;
-  } //END-pieBloque
-  
+
 }
-// END-CLASS-OdaPdf

67a7b85b87d3a075e0a92ff1fe6f0a9d455d4fa9
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-16 16:17:59 -0500

    Update lite_record.php

--- frontend/app/libs/lite_record.php
+++ frontend/app/libs/lite_record.php
@@ -52,71 +52,126 @@ class LiteRecord extends ORM
   const IS_ACTIVE     = [0 =>'Inactivo', 1=>'Activo']; /// esto se eliminará
   const ICO_IS_ACTIVE = [0=>'face-frown', 1=>'face-smile']; /// esto se eliminará
 
-  public function __construct() {
+  public function __construct() 
+  {
     self::$_user_id = Session::get('id') ?? 0;
     self::$_username = Session::get('username') ?? 'Anonimo';
     self::$_periodo_actual = Session::get('periodo') ?? 0;
     self::$_annio_actual = Session::get('annio') ?? 0;
-  } // END-__construct
+  }
   
-  public function __toString() { return $this->id; }
+  
+  public function __toString(): string 
+  {
+    if (property_exists($this, 'nombre')) 
+    { 
+      return "$this->nombre [$this->id]";
+    }
+    return (string)$this->id;
+  }
+
   
   public function _beforeCreate() { // ANTES de Crear el Registro
     $ahora = date('Y-m-d H:i:s', time());
-    if (property_exists($this, 'is_active'))  { $this->is_active = 1; }
-    if (property_exists($this, 'uuid') and method_exists($this, 'setHash') ) { 
+    if (property_exists($this, 'is_active'))
+    { 
+      $this->is_active = 1; 
+    }
+    if (property_exists($this, 'uuid') and method_exists($this, 'setHash') )
+    {
       $this->setHash();
     }
-    if (property_exists($this, 'created_by')) { $this->created_by = self::$_user_id; }
-    if (property_exists($this, 'created_at')) { $this->created_at = $ahora; }
-    if (property_exists($this, 'updated_by')) { $this->updated_by = self::$_user_id; }
-    if (property_exists($this, 'updated_at')) { $this->updated_at = $ahora; }
-  } // END-_beforeCreate
+    if (property_exists($this, 'created_by')) 
+    {
+      $this->created_by = self::$_user_id; 
+    }
+    if (property_exists($this, 'created_at')) 
+    {
+      $this->created_at = $ahora; 
+    }
+    if (property_exists($this, 'updated_by'))
+    {
+      $this->updated_by = self::$_user_id;
+    }
+    if (property_exists($this, 'updated_at')) 
+    {
+      $this->updated_at = $ahora;
+    }
+  }
   
+
   public function _beforeUpdate() { // ANTES de actualizar el registro
     $ahora = date('Y-m-d H:i:s', time());
-    if (property_exists($this, "is_active")) {
-      if (is_null($this->is_active)) { $this->is_active = 0; }
+    if (property_exists($this, "is_active"))
+    {
+      if (is_null($this->is_active))
+      {
+        $this->is_active = 0; 
+      }
     }
-    if (property_exists($this, 'uuid') and method_exists($this, 'setHash') ) {
-      if (is_null($this->uuid) or (strlen($this->uuid)==0)) { 
+    if (property_exists($this, 'uuid') and method_exists($this, 'setHash') )
+    {
+      if (is_null($this->uuid) or (strlen($this->uuid)==0))
+      { 
         $this->setHash(); 
       }
     }
-    if (property_exists($this, 'updated_by')) { $this->updated_by = self::$_user_id; }
-    if (property_exists($this, 'updated_at')) { $this->updated_at = $ahora; }
-  } // END
-  
-  public function _afterUpdate(): void { // DESPUÉS de ACTUALIZAR el Registro
-  } // END
-  
-  public function _afterCreate() { // DESPUÉS de CREAR el Registro
-    //echo $this->id;
-  } // END
+    if (property_exists($this, 'updated_by'))
+    {
+      $this->updated_by = self::$_user_id;
+    }
+    if (property_exists($this, 'updated_at'))
+    {
+      $this->updated_at = $ahora;
+    }
+  }
+
+
+  //public function _afterUpdate(): void { }
+  //public function _afterCreate(): void { }
   
 
-  public function getList(int|bool $estado=null, string $select='*', string|bool $order_by=null) {
-     $DQL = new OdaDql(_from: self::$_class_name);
-     $DQL->select($select)
+  public function getList(
+    int|bool $estado=null, 
+    string $select='*', 
+    string|bool $order_by=null) 
+  {
+    $DQL = new OdaDql(self::$_class_name);
+    $DQL->select($select)
          ->orderBy(self::$_order_by_defa);
-    if (!is_null(value: $order_by)) { $DQL->orderBy( $order_by); }
-    if (!is_null(value: $estado))   { 
+    if (!is_null(value: $order_by))
+    {
+      $DQL->orderBy( $order_by);
+    }
+    if (!is_null(value: $estado))
+    { 
       $DQL->where( 't.is_active=?')
       ->setParams([$estado]);
     }
     return $DQL->execute();
-  } // END
+  }
 
-  public function getListActivos(string $select='*', string|bool $order_by=null): array|string {
+
+  public function getListActivos(
+    string $select='*', 
+    string|bool $order_by=null): array|string
+  {
     return $this->getList(1,  $select, $order_by);
-  } //END
+  }
 
-  public function getListInactivos(string $select='*', string|bool $order_by=null): array|string {
+
+  public function getListInactivos(
+    string $select='*', 
+    string|bool $order_by=null): array|string 
+  {
     return $this->getList(0,  $select, $order_by);
-  } //END
+  }
 
 
-  public function is_active_f(bool $show_ico=false, string $attr="w3-small"): string {
+  public function is_active_f(
+    bool $show_ico=false, 
+    string $attr="w3-small"): string 
+  {
     $estado = self::IS_ACTIVE[(int)$this->is_active] ?? 'Inactivo';
     $ico    = '';
     if ($show_ico) {
@@ -128,44 +183,57 @@ class LiteRecord extends ORM
     return $ico.$estado;
   }
 
-   public function is_active_enum(bool $show_ico=false, string $attr="w3-small"): string {
-     $estado = Estado::tryFrom((int)$this->is_active) ?? Estado::Inactivo;
+
+  public function is_active_enum(
+    bool $show_ico=false, 
+    string $attr="w3-small"): string 
+  {
+    $estado = Estado::tryFrom((int)$this->is_active) ?? Estado::Inactivo;
     return '<span class="'.$attr.' w3-text-'.$estado->color().'">'. (($show_ico) ? $estado->label_ico() : $estado->label()).'</span>';
-   }
-   
-   public function nombre_mes_enum(int $mes): string {
-    $nombre_mes = Mes::tryFrom(value: $mes) ?? Mes::Enero;
-   return $nombre_mes->label();
+  }
+  
+  
+  public function nombre_mes_enum(int $mes): string
+  {
+    $nombre_mes = Mes::tryFrom(value: $mes) ?? Mes::Enero; // definir otra opción que no sea enero ya que puede generra inconsistencia
+    return $nombre_mes->label();
   }
 
-  public function setActivar(): bool {
-    try {
+
+  public function setActivar(): bool
+  {
+    if (property_exists($this, "is_active"))
+    {
       $this->is_active = 1;
       $this->save();
       return true;
-    } catch (\Throwable $th) {
-      throw $th;
-      return false;
     }
-  } // END
+    return false;
+  }
   
-  public function setDesactivar(): bool {
-    try {
+  public function setDesactivar(): bool 
+  {
+    if (property_exists($this, "is_active"))
+    {
       $this->is_active = 0;
       $this->save();
       return true;
-    } catch (\Throwable $th) {
-      throw $th;
-      return false;
     }
-  } // END
+    return false;
+  }
   
-  public static function valor_moneda(int $val_num): string {
+  public static function valor_moneda(int $val_num): string
+  {
     return '$'.number_format($val_num);
-  } //END-valor_moneda
+  }
   
-  public function valor_letras(int $val_num): string {
+  
+  public function valor_letras(int $val_num): string
+  {
     return strtolower(OdaUtils::getNumeroALetras($val_num));
-  } //END
+  }
   
-} //END-CLASS
+
+
+
+}

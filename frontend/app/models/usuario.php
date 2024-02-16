3e3f9136184f03759abd208cf2fac865b9ab104e
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-16 16:19:55 -0500

    Update usuario.php

--- frontend/app/models/usuario.php
+++ frontend/app/models/usuario.php
@@ -51,62 +51,54 @@ class Usuario extends LiteRecord {
   }
 
 
+  public function getDocentes()
+  { 
+    $DQL = new OdaDql(__CLASS__);
+    $DQL->select("t.*")
+      ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'usuario_nombre')
+      ->concat(['t.apellido1', 't.apellido2', 't.nombres'], 'nombre')
+      ->where('t.username<>t.documento')
+      ->orderBy(self::$_order_by_defa);
+    return $DQL->execute();
+  }
 
-  public function setPassword(string $seed=null) 
+
+  public function setPassword(string $seed=null): bool
   {
-    try {
+    try 
+    {
       $salt = md5(rand(100000, 999999).$this->username);
-      $this->salt = $salt;
-      
-      if (is_null($seed)) {
+      if ( is_null($seed) )
+      {
         $seed = ($this->documento == $this->username) ? $this->documento: substr($this->documento, -4);  // true:padres  false:empleados
       }
-
-      $password_salt = hash('sha1', $salt . $seed);
-      $this->password = $password_salt;
-      $this->update();
-
-      $rec_count = $this::query("UPDATE ".self::$table." SET password=? WHERE id=? ", [$password_salt, $this->id])->rowCount();
-      OdaFlash::info("\nUsername: $this->username \nId: $this->id \nDocumento: $this->documento \nPassword:  $pass \nSALT: $this->salt \nPassword SALT $password_salt \nAfect: $rec_count", true);
-    } catch (\Throwable $th) {
+      return $this->update(
+        [
+          'salt' => $salt,
+          'password' => hash('sha1', $salt.$seed),
+        ]
+      );
+    }
+    catch (\Throwable $th)
+    {
       OdaFlash::error($th);
+      return false;
     }
   }
 
 
-
-  public function setRetirar() 
-  {
-      $id = $this->id;
-      $fecha_ret = date('Y-m-d', time());
-      $usuario_instit = '';
-      $clave_instit = '';
-      $updated_at = date('Y-m-d', time());
-      $updated_by = Session::get('id');
-      $is_carga_acad_ok = 0;
-      $is_active = 0;
-
-      $DQL = new OdaDql(self::$table);
-      
-      $DQL->setFrom(self::$table);
-
-      $DQL->update([
-        'fecha_ret' => $fecha_ret,
-        'usuario_instit' => $usuario_instit,
-        'clave_instit' => $clave_instit,
-        'updated_at' => $updated_at,
-        'updated_by' => $updated_by,
-        'is_carga_acad_ok' => $is_carga_acad_ok,
-        'is_active' => $is_active,
-      ]);
-
-      $DQL->where('t.id=?')
-          ->setParams([$id]);
-
-      $DQL->execute(true);
+  public function setRetirar(): bool 
+  {    
+    return $this->update(
+      [
+        'id' => $this->id,
+        'fecha_ret' => date('Y-m-d', time()),
+        'is_carga_acad_ok' => 0,
+        'is_active' => 0,
+      ]
+    );
   }
 
- 
 
 
 }

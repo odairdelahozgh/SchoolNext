629cb954e696316455759c719d1e6ed51ee97ff8
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-22 10:11:28 -0500

    Create psico_remision.php

---
+++ frontend/app/models/psico_remision.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Modelo
+ * @author   ConstruxZion Soft (odairdelahoz@gmail.com).
+ * @category App
+ * @package  Models https://github.com/KumbiaPHP/ActiveRecord
+ * 
+ */
+
+include "psicologia/psico_remision_trait_props.php";
+include "psicologia/psico_remision_trait_set_up.php";
+
+class PsicoRemision extends LiteRecord 
+{
+  use PsicoRemisionTraitSetUp;
+
+  public function __construct() 
+  {
+    parent::__construct();
+    self::$table = Config::get('tablas.psico_remision');
+    self::$_order_by_defa = 't.fecha, t.estado, t.estudiante_id, t.remite_id';
+    $this->setUp();
+  }
+
+
+
+}

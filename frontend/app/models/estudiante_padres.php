591a25406aff97920bcb78523df86def1e33958d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-22 10:11:38 -0500

    Update estudiante_padres.php

--- frontend/app/models/estudiante_padres.php
+++ frontend/app/models/estudiante_padres.php
@@ -40,5 +40,148 @@ class EstudiantePadres extends LiteRecord
   }
   
 
+  static public function vincularPadresHijos(int $estudiante_id)
+  {
+    try {
+      $RegEstud = (new Estudiante)->get($estudiante_id);
+      if ($RegEstud) 
+      {
+        // Existe Estudiante
+        $source  =  Config::get('tablas.datosestud');
+        $sql = "SELECT * FROM $source WHERE estudiante_id = ?";
+        $RegEstudDetalle = static::query($sql, [$estudiante_id])->fetch();
+        
+        // Existe usuario madre ???
+        $source  =  Config::get('tablas.usuario');
+        $sql = "SELECT * FROM $source WHERE documento = ?";
+        $regUserMadre = static::query($sql, [$RegEstudDetalle->madre_id])->fetch();
+        if ($regUserMadre) 
+        {
+          // Usuario de la madre ya estaba creado
+          // está viculado Madre-Estudiante ???
+          $source  =  Config::get('tablas.usuarios_estudiantes');
+          $sql = "SELECT * FROM $source WHERE dm_user_id = ? AND estudiante_id=?";
+          $regMadreEstud = static::query($sql, [$regUserMadre->id, $RegEstud->id])->fetch();
+          if (!$regMadreEstud) 
+          { // crear vinculo
+            $MadreEstud = new EstudiantePadres();
+            $MadreEstud->create(
+              [
+                'dm_user_id' => $regUserMadre->id, 
+                'estudiante_id' => $RegEstud->id,
+                'relacion' => 'Madre',
+                'created_at'=>'',
+                'updated_at'=>'',
+                ]
+              );
+          }
+        }
+        else 
+        {
+          // se debe crear usuario madre
+          $Usuario = new Usuario();
+          $Usuario->create([
+            'uuid' => ($Usuario->xxh3Hash()),
+            'username'=>$RegEstudDetalle->madre_id, 
+            'roll'=>'padres', 
+            'nombres'=>$RegEstudDetalle->madre, 
+            'apellido1'=>'', 
+            'apellido2'=>'', 
+            'direccion'=>$RegEstudDetalle->madre_dir, 
+            'documento'=>$RegEstudDetalle->madre_id, 
+            'email'=>$RegEstudDetalle->madre_email, 
+            'telefono1'=>$RegEstudDetalle->madre_tel_1, 
+            'telefono2'=>$RegEstudDetalle->madre_tel_2, 
+            'cargo'=>$RegEstudDetalle->madre_ocupa, 
+            'sexo'=>'F',
+            'theme'=>'dark',
+            'algorithm'=>'sha1',
+            'created_at'=>'',
+            'updated_at'=>'',
+            'is_active'=>1,
+          ]);
+          $Usuario->setPassword();
+          $RelMadreEstud = new EstudiantePadres();
+          $RelMadreEstud->create(
+            [
+              'dm_user_id' => $Usuario->id, 
+              'estudiante_id' => $RegEstud->id,
+              'relacion' => 'Madre',
+              'created_at'=>'',
+              'updated_at'=>'',
+              ]
+            );
+        }
+
+
+        // Existe usuario padre ???
+        $source  =  Config::get('tablas.usuario');
+        $sql = "SELECT * FROM $source WHERE documento = ?";
+        $regUserPadre = static::query($sql, [$RegEstudDetalle->padre_id])->fetch();
+        if ($regUserPadre) 
+        {
+          // está viculado Madre-Estudiante ???
+          $source  =  Config::get('tablas.usuarios_estudiantes');
+          $sql = "SELECT * FROM $source WHERE dm_user_id = ? AND estudiante_id=?";
+          $regPadreEstud = static::query($sql, [$regUserPadre->id, $RegEstud->id])->fetch();
+          if (!$regPadreEstud) 
+          { // crear vinculo
+            $PadreEstud = new EstudiantePadres();
+            $PadreEstud->create(
+              [
+                'dm_user_id' => $regUserPadre->id, 
+                'estudiante_id' => $RegEstud->id,
+                'relacion' => 'Padre',
+                'created_at'=>'',
+                'updated_at'=>'',
+                ]
+              );
+            }
+        }
+        else 
+        {
+          // se debe crear usuario PADRE
+          $Usuario = new Usuario();
+          $Usuario->create([
+            'uuid' => ($Usuario->xxh3Hash()),
+            'username'=>$RegEstudDetalle->padre_id, 
+            'roll'=>'padres', 
+            'nombres'=>$RegEstudDetalle->padre, 
+            'apellido1'=>'', 
+            'apellido2'=>'', 
+            'direccion'=>$RegEstudDetalle->padre_dir, 
+            'documento'=>$RegEstudDetalle->padre_id, 
+            'email'=>$RegEstudDetalle->padre_email, 
+            'telefono1'=>$RegEstudDetalle->padre_tel_1, 
+            'telefono2'=>$RegEstudDetalle->padre_tel_2, 
+            'cargo'=>$RegEstudDetalle->padre_ocupa, 
+            'sexo'=>'M',
+            'theme'=>'dark',
+            'algorithm'=>'sha1',
+            'created_at'=>'',
+            'updated_at'=>'',
+            'is_active'=>1,
+          ]);
+          $Usuario->setPassword();
+          $RelPadreEstud = new EstudiantePadres();
+          $RelPadreEstud->create(
+            [
+              'dm_user_id' => $Usuario->id, 
+              'estudiante_id' => $RegEstud->id,
+              'relacion' => 'Padre',
+              'created_at'=>'',
+              'updated_at'=>'',
+              ]
+            );
+        }
+      }
+    
+    } catch (\Throwable $th) {
+      OdaFlash::error($th);
+    }
+
+  }
+
+
   
 }

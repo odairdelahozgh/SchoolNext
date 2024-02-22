38fd7bdc95552f1890a02af55bc550d893482cdd
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-02-22 10:17:55 -0500

    Update consulta_telefonos_padres.sql

--- help/sql/consulta_telefonos_padres.sql
+++ help/sql/consulta_telefonos_padres.sql
@@ -1,5 +1,11 @@
-SELECT e.id, e.grado_mat, g.nombre as grado, CONCAT(e.apellido1, ' ', e.apellido2, ' ', e.nombres) as estudiante, 
-e.email, CONCAT(e.email_instit,'@windsorschool.edu.co') AS email_windsor
+SELECT 
+e.id, 
+e.grado_mat, 
+g.nombre as grado, 
+CONCAT(e.apellido1, ' ', e.apellido2, ' ', e.nombres) as estudiante, 
+e.email, 
+CONCAT(e.email_instit,'@windsorschool.edu.co') AS email_windsor
+
 FROM sweb_estudiantes as e
 LEFT JOIN sweb_grados     AS g ON e.grado_mat  = g.id
 

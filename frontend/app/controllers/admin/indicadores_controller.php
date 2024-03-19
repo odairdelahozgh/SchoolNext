552a4b78ea66c261701625ffcfeca6a2e1c9bdfc
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-03-19 08:42:12 -0500

    Update indicadores_controller.php

--- frontend/app/controllers/admin/indicadores_controller.php
+++ frontend/app/controllers/admin/indicadores_controller.php
@@ -8,10 +8,34 @@
 class IndicadoresController extends ScaffoldController
 {
 
-
   protected function before_filter() 
   {
     $this->nombre_modelo = 'indicador';
   }
  
+
+  public function filtrar(
+    int $periodo_id,
+    int $grado_id,
+    int $asignatura_id,
+  )
+  {
+    try
+    {
+      $this->page_action = "Listado $this->controller_name Filtradas" ;
+      $this->fieldsToShow = (new $this->nombre_modelo())->getFieldsShow('filtrar');
+      $this->fieldsToShowLabels = (new $this->nombre_modelo())->getFieldsShow('filtrar', true);
+      $this->nombre_modelo = OdaUtils::singularize($this->controller_name);
+      $Indicador = new Indicador();
+      $this->data = $Indicador->getByPeriodoGradoAsignatura($periodo_id, $grado_id, $asignatura_id);
+    }
+    catch (\Throwable $th)
+    {
+      OdaFlash::error($th);
+    }
+  }
+
+
+
+
 }

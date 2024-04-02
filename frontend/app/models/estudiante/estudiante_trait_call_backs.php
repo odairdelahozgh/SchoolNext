efd7525f825991e9eb60709b23282ca96a463c23
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-04-02 16:20:56 -0500

    Create estudiante_trait_call_backs.php

---
+++ frontend/app/models/estudiante/estudiante_trait_call_backs.php
@@ -0,0 +1,25 @@
+<?php
+
+trait EstudianteTraitCallBacks {
+
+//  public function _afterCreate() 
+//  {
+//     parent::_afterCreate();
+    
+//     $ahora = date('Y-m-d H:i:s', time());
+//     $DatosEstud = new DatosEstud();
+//     $DatosEstud->create([
+//       'estudiante_id' => $this->id,
+//       'is_active' => 1,
+//       'updated_at' => $ahora,
+//       'created_at' => $ahora,
+//       'updated_by' => $this->_user_id,
+//       'created_by' => $this->_user_id,
+//       'uuid' => $this->setHash(),
+//       'fecha_nac' => date('Y-m-d'),
+//     ]
+//     );
+
+//  }
+ 
+} //END-Trait

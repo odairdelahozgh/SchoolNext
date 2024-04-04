4296437296e3f569e6fa2ecc075c9c8d2189129e
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-04-04 05:29:01 -0500

    Update nota.php

--- frontend/app/models/nota.php
+++ frontend/app/models/nota.php
@@ -326,7 +326,7 @@ class Nota extends LiteRecord {
         $tiene_logros = strlen($reg->i01)+strlen($reg->i02)+strlen($reg->i03)+strlen($reg->i04)+strlen($reg->i05)
         +strlen($reg->i06)+strlen($reg->i07)+strlen($reg->i08)+strlen($reg->i09)+strlen($reg->i10);
         $aResult["$reg->salon;$reg->salon_id;$reg->salon_uuid"]
-                ["$reg->estudiante;$reg->estudiante_id;$reg->estudiante_uuid;$reg->is_active;$reg->madre;$reg->madre_tel_1;$reg->padre;$reg->padre_tel_1"]
+                ["$reg->estudiante;$reg->estudiante_id;$reg->estudiante_uuid;$reg->is_active;$reg->madre;$reg->madre_tel_1;$reg->padre;$reg->padre_tel_1;$reg->annio_pagado;$reg->mes_pagado"]
                 ["$reg->periodo_id"]
                 ["$reg->asignatura;$reg->asignatura_abrev"] 
         = "$reg->id;$reg->uuid;$reg->definitiva;$reg->plan_apoyo;$reg->nota_final;$reg->desempeno;$asi;$paf;$tiene_logros";

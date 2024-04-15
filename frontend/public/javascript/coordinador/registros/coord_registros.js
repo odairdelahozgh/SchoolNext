549f59a630a13347a9d6aa40fdfd3a2a6413002e
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-04-15 17:52:12 -0500

    Update coord_registros.js

--- frontend/public/javascript/coordinador/registros/coord_registros.js
+++ frontend/public/javascript/coordinador/registros/coord_registros.js
@@ -37,6 +37,8 @@ function traer_salones(user_id, annio) {
     );
 } //END-traer_salones
 
+
+
 function cargar_botones(salon_id, salon_nombre, annio, max_periodos) {
   let ruta = document.getElementById('public_path');
   document.getElementById('botones').innerHTML = '';
@@ -91,15 +93,17 @@ function traer_data_reg_gen(salon_id, salon_nombre, annio) {
       let output = '';
       let caption = '';
       for (let key in datos) {
+        color = (('ACAD'==datos[key].tipo_reg) ? 'w3-teal' : 'w3-red');
+        texto_tipo_reg = (('ACAD'==datos[key].tipo_reg) ? 'ACADÉMICO' : 'DISCIPLINARIO');
         output += `
           <tr> 
             <td style="width:65%">
               <h3>${datos[key].estudiante_nombre}</h3>
-              <b>Tipo:</b> <span class="w3-tag w3-round w3-teal w3-border w3-border-white">${datos[key].tipo_reg}</span><br>
-              <b>Asunto:</b> <br> ${datos[key].asunto}
+              <span class="w3-tag w3-round ${color} w3-border w3-border-white">${texto_tipo_reg}</span>
+              <br> ${datos[key].asunto}
             </td>
             <td style="width:35%">
-              <b>Perido ${datos[key].periodo_id}</b>  [${datos[key].fecha}]<br><br>
+              <b>Periodo ${datos[key].periodo_id}</b>  [${datos[key].fecha}]<br><br>
               <b>Docente:</b> <br> ${datos[key].director}<br><br>
               <b>Acudiente:</b> <br> ${datos[key].acudiente}
             </td>
@@ -151,7 +155,7 @@ function traer_data_reg_acad(salon_id, salon_nombre, annio) {
               <b>Compromisos:</b> <br> ${datos[key].compromisos}
             </td>
             <td style="width:35%">
-              <b>Perido ${datos[key].periodo_id}</b>  [${datos[key].fecha}]<br><br>
+              <b>Periodo ${datos[key].periodo_id}</b>  [${datos[key].fecha}]<br><br>
               <b>Docente:</b> <br> ${datos[key].director}<br><br>
               <b>Acudiente:</b> <br> ${datos[key].acudiente}
               </td>

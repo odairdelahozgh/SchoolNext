12772314338ee75abbc32e1e9857cc4a12a8ed20
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-05-22 08:41:06 -0500

    Update reg_sweb_notas_repetidas.sql

--- help/sql/reg_sweb_notas_repetidas.sql
+++ help/sql/reg_sweb_notas_repetidas.sql
@@ -1,7 +1,7 @@
 SELECT COUNT(id), annio, periodo_id, grado_id, salon_id, asignatura_id, estudiante_id
 FROM sweb_notas 
 GROUP BY annio, periodo_id, grado_id, salon_id, asignatura_id, estudiante_id
-HAVING COUNT(id)> 1  
+HAVING COUNT(id)> 1  AND periodo_id = 2
 ORDER BY annio, periodo_id, grado_id, salon_id, asignatura_id, estudiante_id ASC;
 
 

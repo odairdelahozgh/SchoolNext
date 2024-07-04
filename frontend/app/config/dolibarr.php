4b82493750e0a179a9d80fc4e9d05ae9a46f9a66
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-07-03 22:27:54 -0500

    Create dolibarr.php

---
+++ frontend/app/config/dolibarr.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * KumbiaPHP Web Framework
+ * Parámetros de configuracion de la aplicacion
+ *  @example (int)Config::get(var: 'academic.periodo_actual');
+ */
+return [
+    'curl' => [
+      'url_root' => 'https://crm.colegiomixtosantarosa.com/api/index.php/',
+      'api_key' => 'DOLAPIKEY: oG8kFOnEFLhBZ81LElpss38M5x0dt888',
+    ],
+];

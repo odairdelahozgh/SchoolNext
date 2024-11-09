91c6a854790618f66bf327fb91a6c17fb9e03afa
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-11-09 16:36:22 -0500

    Update bootstrap.php

--- frontend/app/libs/bootstrap.php
+++ frontend/app/libs/bootstrap.php
@@ -1,22 +1,16 @@
 <?php
 // Bootstrap de la aplicacion para personalizarlo
 // Para cargar cambia en public/index.php el require del bootstrap a app
-
 // Arranca KumbiaPHP
 
-/* HOSTING
-const ABS_PUBLIC_PATH = '/home/windsor1/schoolnext.windsorschool.edu.co/';
-*/
-
-
-const APP_ROOT_PRIVATE = '/home/windsor1/schoolnext_private/';
 define('VENDOR_PATH', APP_PATH.'../vendor/');
+
 define('HELPERS_PATH', APP_PATH.'extensions/helpers/');
-//const VENDOR_PATH      = APP_ROOT_PRIVATE.'frontend/vendor/';
 
-const ABS_PUBLIC_PATH = 'D:\schoolnext\frontend\public'; // '/home/windsor1/schoolnext.windsorschool.edu.co/';
+//const ABS_PUBLIC_PATH  = '/home/u113041793/domains/colegiomixtosantarosa.com/public_html/_schoolnext_website/';  // SERVIDOR SANTA ROSA
+//const ABS_PUBLIC_PATH = '/home/windsor1/schoolnext.windsorschool.edu.co/';  // SERVIDOR WINDSOR
+const ABS_PUBLIC_PATH = 'D:\schoolnext\frontend\public'; // LOCAL
 
-const SCHOOLWEB_PUBLIC_PATH = 'https://windsortemp.schoolnext.space/';
 const APP_NAME= "SchoolNEXT>>";
 
 const FILE_UPLOAD_PATH   = PUBLIC_PATH.'files/upload/';

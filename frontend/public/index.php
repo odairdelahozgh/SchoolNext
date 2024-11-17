65b81fbba48c7c3b366b6457aad4bff91ff50987
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-11-17 00:50:12 -0500

    Update index.php

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -3,25 +3,23 @@ setlocale(LC_ALL, 'es_ES');
 ini_set('date.timezone', 'America/Bogota');
 const APP_CHARSET = 'UTF-8';
 error_reporting(E_ALL ^ E_STRICT);
-ini_set('display_errors', 'On');
-const PRODUCTION = false; // true / false
+ini_set('display_errors', 'Off'); // Off/On
+const PRODUCTION = false; // servidor=true / local=false
 
-//const APP_ROOT_PRIVATE = '/home/u113041793/domains/colegiomixtosantarosa.com/_schoolnext_santarosa_private/'; // SANTAROSA
-//const APP_ROOT_PRIVATE = '/home/windsor1/schoolnext_private/'; // WINDSOR
+//const APP_ROOT_PRIVATE = '/home/u113041793/domains/colegiomixtosantarosa.com/_schoolnext_santarosa_private/';
+//const APP_ROOT_PRIVATE = '/home/tecnoro1/schoolnext_private/';
 
-//const APP_PATH         = APP_ROOT_PRIVATE.'frontend/app/'; // SERVIDOR
-define('APP_PATH', dirname(__DIR__).'/app/');  // LOCAL
+//const APP_PATH = APP_ROOT_PRIVATE.'frontend/app/'; // SERVIDOR
+define('APP_PATH', dirname(__DIR__).'/app/');
 
-// const CORE_PATH        = APP_ROOT_PRIVATE.'core1.2/';  // SERVIDOR
-define('CORE_PATH', dirname(dirname(APP_PATH)).'/core1.2/');  // LOCAL
+// const CORE_PATH = APP_ROOT_PRIVATE.'core1.2/';  // SERVIDOR
+define('CORE_PATH', dirname(dirname(APP_PATH)).'/core1.2/');
 
-//const PUBLIC_PATH = 'https://schoolnext.colegiomixtosantarosa.com/'; // SANTAROSA
-//const PUBLIC_PATH = 'https://schoolnext.windsorschool.edu.co/'; // WINDSOR
+//const PUBLIC_PATH = 'https://schoolnext.colegiomixtosantarosa.com/';
+//const PUBLIC_PATH = 'https://schoolnext.windsorschool.edu.co/';
 define('PUBLIC_PATH', substr($_SERVER['SCRIPT_NAME'], 0, -9)); // - index.php string[9]  LOCAL
 
-//$url = $_SERVER['PATH_INFO'] ?? '/';  // SANTAROSA
 //$url = $_SERVER['ORIG_PATH_INFO'] ?? '/'; // WINDSOR
-$url = $_SERVER['PATH_INFO'] ?? '/';  // LOCAL
+$url = $_SERVER['PATH_INFO'] ?? '/';  // LOCAL y SANTAROSA
 
 require APP_PATH . 'libs/bootstrap.php'; //bootstrap de app
-//require CORE_PATH.'kumbia/bootstrap.php'; //bootstrap del core (POR DEFAULT)

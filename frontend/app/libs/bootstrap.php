517291d1997e5845636019b4cc1d0d899f27312a
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-11-16 22:48:43 -0500

    Update bootstrap.php

--- frontend/app/libs/bootstrap.php
+++ frontend/app/libs/bootstrap.php
@@ -3,21 +3,21 @@
 // Para cargar cambia en public/index.php el require del bootstrap a app
 // Arranca KumbiaPHP
 
-define('VENDOR_PATH', APP_PATH.'../vendor/');
+const VENDOR_PATH = APP_PATH.'../vendor/' ;
+const HELPERS_PATH = APP_PATH.'extensions/helpers/' ;
 
-define('HELPERS_PATH', APP_PATH.'extensions/helpers/');
-
-//const ABS_PUBLIC_PATH  = '/home/u113041793/domains/colegiomixtosantarosa.com/public_html/_schoolnext_website/';  // SERVIDOR SANTA ROSA
-//const ABS_PUBLIC_PATH = '/home/windsor1/schoolnext.windsorschool.edu.co/';  // SERVIDOR WINDSOR
+//const ABS_PUBLIC_PATH  = '/home/u113041793/domains/colegiomixtosantarosa.com/public_html/_schoolnext_website';  // SANTAROSA
+//const ABS_PUBLIC_PATH = '/home/tecnoro1/schoolnext.tecnorobotica.com';  // WINDSOR
 const ABS_PUBLIC_PATH = 'D:\schoolnext\frontend\public'; // LOCAL
 
 const APP_NAME= "SchoolNEXT>>";
+const INSTITUTION_KEY = "DEVELOPMENT"; // "SANTAROSA" "WINDSOR" "DEVELOPMENT"
 
 const FILE_UPLOAD_PATH   = PUBLIC_PATH.'files/upload/';
 const FILE_DOWNLOAD_PATH = PUBLIC_PATH.'files/download/';
 
 const IMG_UPLOAD_PATH    = PUBLIC_PATH.'img/upload/';
 const IMG_DOWNLOAD_PATH  = PUBLIC_PATH.'img/download/';
-const IMG_ESTUDIANTES_PATH  = IMG_UPLOAD_PATH.'estudiantes';
+const IMG_ESTUDIANTES_PATH  = IMG_UPLOAD_PATH.'estudiantes/';
 
 require_once CORE_PATH . 'kumbia/bootstrap.php';

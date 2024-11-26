07d8d020dd9573320d521b685e04449cfff39902
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-11-25 21:26:10 -0500

    Update usuario_trait_setters.php

--- frontend/app/models/usuario/usuario_trait_setters.php
+++ frontend/app/models/usuario/usuario_trait_setters.php
@@ -26,6 +26,31 @@ trait UsuarioTraitSetters {
   }
 
 
+
+  public function setPasswordDolibarr(string $seed=null): bool
+  {
+    try 
+    {
+      $salt = md5(rand(100000, 999999).$this->username);
+      if ( is_null($seed) )
+      {
+        $seed = (trim(strtolower($this->documento)) == trim(strtolower($this->username))) ? $this->documento: substr($this->documento, -4);
+      }
+      return $this->update(
+        [
+          'salt' => $salt,
+          'password' => hash('sha1', $salt.$seed),
+        ]
+      );
+    }
+    catch (\Throwable $th)
+    {
+      OdaFlash::error($th);
+      return false;
+    }
+  }
+
+
   public function setRetirar(): bool 
   {    
     return $this->update(

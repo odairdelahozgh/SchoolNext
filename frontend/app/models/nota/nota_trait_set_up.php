4e7c9f1252113cfb96285b54af307dedb620dd07
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-12-08 17:17:40 -0500

    Update nota_trait_set_up.php

--- frontend/app/models/nota/nota_trait_set_up.php
+++ frontend/app/models/nota/nota_trait_set_up.php
@@ -4,23 +4,6 @@ trait NotaTraitSetUp {
   
   use TraitUuid, TraitForms, TraitValidar, 
   NotaTraitProps, NotaTraitLinks, NotaTraitCorrecciones;
-    
-  /*
-   * CONFIGURACIÓN DEL MODELO
-   *   'id', 'uuid', 'annio', 'periodo_id', 'grado_id', 'salon_id', 'asignatura_id', 'estudiante_id', 
-  'asignatura', 'estudiante', 'email_envios', 'asi_num_envios', 
-  'definitiva', 'plan_apoyo', 'nota_final', 'profesor_id', 
-  'i01', 'i02', 'i03', 'i04', 'i05', 'i06', 'i07', 'i08', 'i09', 'i10', 
-  'i11', 'i12', 'i13', 'i14', 'i15', 'i16', 'i17', 'i18', 'i19', 'i20', 
-  'i21', 'i22', 'i23', 'i24', 'i25', 'i26', 'i27', 'i28', 'i29', 'i30', 
-  'i31', 'i32', 'i33', 'i34', 'i35', 'i36', 'i37', 'i38', 'i39', 'i40', 
-  'asi_desempeno', 'asi_activ_profe', 'asi_activ_estud', 'asi_fecha_entrega', 
-  'asi_link_externo1', 'asi_link_externo2', 'paf_link_externo1', 'paf_link_externo2', 
-  'paf_temas', 'paf_acciones', 'paf_activ_estud', 'paf_activ_profe', 'paf_fecha_entrega', 
-  'is_paf_ok_coord', 'is_paf_validar_ok', 'is_paf_ok_dirgrupo', 'paf_num_envios', 
-  'ausencias', 'inthoraria', 'created_at', 'updated_at', 'created_by', 'updated_by'
-  'is_asi_validar_ok', 'asi_calificacion', 'is_asi_ok_dirgrupo', 'is_asi_ok_coord', 
-   */
 
   private function setUp() {
 

aca4de48895329b8b55dc1c0c5763b56b8534ebb
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2024-12-11 00:16:38 -0500

    Update salon_trait_links.php

--- frontend/app/models/salon/salon_trait_links.php
+++ frontend/app/models/salon/salon_trait_links.php
@@ -4,7 +4,7 @@ trait SalonTraitLinks {
   
   public function getLnkPreboletin(): string {
     try {
-      $periodo_actual = Config::get('config.academic.periodo_actual');
+      $periodo_actual = self::$_periodo_actual;
       return OdaTags::linkButton(action: "admin/notas/exportBoletinSalonPdf/$periodo_actual/$this->uuid/0", text: 'Preboletin', attrs: " target=\"_blank\" class=\"w3-button w3-green\"");
 
     } catch (\Throwable $th) {
@@ -14,7 +14,7 @@ trait SalonTraitLinks {
   
   public function getLnkBoletin(): string {
     try {
-      $periodo_actual = Config::get('config.academic.periodo_actual');
+      $periodo_actual = self::$_periodo_actual;
       return OdaTags::linkButton(action: "admin/notas/exportBoletinSalonPdf/$periodo_actual/$this->uuid/1", text: 'Boletin', attrs: " target=\"_blank\" class=\"w3-button w3-green\"");
 
     } catch (\Throwable $th) {
@@ -22,18 +22,22 @@ trait SalonTraitLinks {
     }
   } //END-getLnkPreboletin
   
-  public function getLnkSetupCalificarSalon(): string {
-    try {
+  public function getLnkSetupCalificarSalon(): string
+  {
+    try
+    {
       $result = '';
-      if (Config::get('config.academic.periodo_actual') != $this->is_ready_print) {
+      if (self::$_periodo_actual != $this->is_ready_print)
+      {
         $result = OdaTags::linkButton(action: "admin/salones/setupCalificarSalon/$this->id", text: 'Setup Cal', attrs: " class=\"w3-button w3-green\"");
       }
       return $result;
-
-    } catch (\Throwable $th) {
+    } 
+    catch (\Throwable $th)
+    {
       OdaFlash::error($th);
     }
-  } //END-getLnkPreboletin
+  }
   
 
 } //END-Trait

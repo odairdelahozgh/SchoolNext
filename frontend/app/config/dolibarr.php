ad3ba82afb39079adbe4206ef9eaebbe2738e5ac
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2025-02-15 04:31:39 -0500

    Update dolibarr.php

--- frontend/app/config/dolibarr.php
+++ frontend/app/config/dolibarr.php
@@ -2,11 +2,13 @@
 
 return [
   'windsor' => [
+    'prefijo_bd' => 'llx_',
     'api_url' => 'https://crm.tecnorobotica.com/api/index.php',
-    'api_key' => '',
+    'api_key' => 'HTVd6TM2jl2w3v79EKclAjO07wo26CNb',
   ],
-
+  
   'santarosa' => [
+    'prefijo_bd' => 'llx_',
     'api_url' => 'https://crm.colegiomixtosantarosa.com/api/index.php',
     'api_key' => '1g3mbFb7Gga15MIIJ60jC95xkz6bCTXE',
   ],

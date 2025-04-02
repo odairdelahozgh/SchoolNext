90c212ce4488ec8933f558c117c84825c9ec6a48
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2025-04-02 03:52:37 -0500

    Delete _actualizat_nota_final_antiguos.sql

--- help/sql/_actualizat_nota_final_antiguos.sql
+++
@@ -1,3 +0,0 @@
-update sweb_notas_2011 set nota_final = IF(nota_final=0 and plan_apoyo=0, definitiva, nota_final)
-
-update sweb_notas_2011 set nota_final = IF(nota_final=0, IF(definitiva>plan_apoyo, definitiva, plan_apoyo), nota_final)

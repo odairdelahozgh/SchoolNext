e5ad0281419b7cdbb260111c2f2094d7c9cd3e89
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2025-04-02 03:37:07 -0500

    Update nota_trait_props.php

--- frontend/app/models/nota/nota_trait_props.php
+++ frontend/app/models/nota/nota_trait_props.php
@@ -3,35 +3,42 @@
  * Summary of NotaTraitProps
  */
 trait NotaTraitProps {
+  
+  private static $arrExcepProm = array(30,35,36,37,38,39,40);
 
-  public function __toString() {
+  public function __toString() 
+  {
     return $this->id.'-'.$this->annio.'-'.$this->periodo_id.'-'.$this->salon_id.'-'.$this->asignatura_id.'-'.$this->estudiante_id.'-'.$this->definitiva;
   }  
 
-  public function getFoto(bool $show_cod=true) { 
+  public function getFoto(bool $show_cod=true) 
+  { 
     return Estudiante::getFotoEstud(id: $this->estudiante_id, show_cod: $show_cod, sexo:''); 
   }
   
-  public static function imgTablaRango(): string { 
+  public static function imgTablaRango(): string 
+  { 
     return OdaTags::img(src: "upload/asignaturas/rangos.png", attrs: "style=\"width:100%;max-width:214px\""); 
   }
 
   
-  public function verNota(): string {
-    $color = (new Rango)::getColorRango(valor: $this->nota_final);
-    $rango = (new Rango)::getRango(valor: $this->nota_final);
-    $plan_apoyo = ($this->definitiva<60) ? 'Definitiva: '.$this->definitiva.' => Plan de Apoyo: '.$this->plan_apoyo : '' ;
+  public function verNota(): string 
+  {
+    $color = (new Rango)->getColorNota($this->nota_final);
+    $rango = (new Rango)->getRangoNota($this->nota_final);
+    $nota_limite = (new Rango)->getLimiteInferior(Rangos::Basico);
+    $plan_apoyo = ($this->definitiva < $nota_limite) ? 'Definitiva: '.$this->definitiva.' => Plan de Apoyo: '.$this->plan_apoyo : '' ;
+    
     return "<span class=\"w3-tag w3-round $color\">
               $this->nota_final $rango
             </span> $plan_apoyo ";
   }
 
 
-  private static $arrExcepProm = array(30,35,36,37,38,39,40);
   
 
-  public function fieldForm(string $field_name, string $attr='', string|bool $type=null): string {
-
+  public function fieldForm(string $field_name, string $attr='', string|bool $type=null): string 
+  {
     $attribs = ($attr) ? $attr : $this->getAttrib(field: $field_name) ;
     return Form::input(
       type:  ((!is_null($type)) ? $type : $this->getWidget($field_name)),
@@ -39,7 +46,7 @@ trait NotaTraitProps {
       attrs: $attribs . $this->getPlaceholder($field_name),
       value: $this->$field_name,
     );
-  }//END-fieldForm
+  }
 
 
-} //END-TraitProps
+}

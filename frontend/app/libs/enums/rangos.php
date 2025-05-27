e1875d9326cfb748222a0aa2aeb51753ba7af40c
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2025-05-26 23:02:36 -0500

    Update rangos.php

--- frontend/app/libs/enums/rangos.php
+++ frontend/app/libs/enums/rangos.php
@@ -19,59 +19,9 @@ enum Rangos: string {
     };
   }
 
-  public function color(): string 
-  {
-    return match($this) 
-    {
-      static::Bajo        => 'crimson',
-      static::Basico      => 'coral',
-      static::Alto        => 'light-blue',
-      static::Superior    => 'seagreen',
-      default             => 'crimson',
-    };
-  }
-
-  public function limiteInf(): string 
-  {
-    return match($this) 
-    {
-      static::Bajo        => '1',
-      static::Basico      => '60',
-      static::Alto        => '80',
-      static::Superior    => '95',
-      default             => '0',
-    };
-  }
-  
-  public function limiteSup(): string 
-  {
-    return match($this) 
-    {
-      static::Bajo        => '59',
-      static::Basico      => '79',
-      static::Alto        => '94',
-      static::Superior    => '100',
-      default             => '0',
-    };
-  }
-
-  public function rango(int $valor): string 
-  {
-    return match($this) 
-    {
-      $valor > 100  => 'Rango no válido: Superior a 100',
-      $valor >= 95  => 'superior',
-      $valor >= 80  => 'alto',
-      $valor >= 60  => 'basico',
-      $valor >= 1   => 'bajo',
-      $valor == 0   => '',
-      $valor < 0    => 'Rango no válido: Inferior a Cero',
-    };
-  }
-
   public static function caption(): string 
   {
-    return 'Rango de la califiación';
+    return 'Rango de la calificación';
   }
 
 }

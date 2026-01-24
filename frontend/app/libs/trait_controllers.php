47461723040a583ff1b6d03b67ea60bddb9b8206
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-01-23 22:56:18 -0500

    Create trait_controllers.php

---
+++ frontend/app/libs/trait_controllers.php
@@ -0,0 +1,9 @@
+<?php
+trait TraitControllers {
+
+  public int|null $_max_periodos = 0;
+  public int|null $_periodo_actual = 0;
+  public int|null $_annio_actual = 0;
+  public int|null $_annio_inicial = 0;
+  
+}

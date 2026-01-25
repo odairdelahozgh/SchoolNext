0e9ce26b56f09cc771e848efd95059c01caac774
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-01-25 08:23:40 -0500

    Update Adminlte layout

---
+++ frontend/app/views/_shared/templates/layout_invited_config.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = $page_title;
+$layoutOptions = [
+    'fixed_header' => false,
+    'fixed_sidebar' => false,
+    'collapsed_sidebar' => false,
+    'dist_path' => PUBLIC_PATH. 'vendor/adminlte'
+];
+
+$menuItems = [
+];

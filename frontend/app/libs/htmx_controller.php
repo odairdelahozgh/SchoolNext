afe36f8841dc9e5abc9820d8b8c78395956e96c4
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-01-27 13:28:52 -0500

    Update htmx_controller.php

--- frontend/app/libs/htmx_controller.php
+++ frontend/app/libs/htmx_controller.php
@@ -19,6 +19,14 @@ abstract class HtmxController extends Controller
   
   use TraitControllers;
 
+  public string $_instituto_id = '';
+  public string $_instituto_nombre = '';
+  public int|null $_max_periodos = 0;
+  public int|null $_periodo_actual = 0;
+  public int|null $_annio_actual = 0;
+  public int|null $_annio_inicial = 0;
+  public int|null $_annio_matricula = 0;
+
   protected function before_filter() 
   {
     View::template(null);
@@ -30,6 +38,13 @@ abstract class HtmxController extends Controller
 
   protected function initialize()
   {
+    $this->_instituto_id = INSTITUTION_KEY;
+    $this->_instituto_nombre = INSTITUTION_NAME;
+    $this->_max_periodos = MAX_PERIODOS;
+    $this->_periodo_actual = PERIODO_ACTUAL;
+    $this->_annio_actual = ANNIO_ACTUAL;
+    $this->_annio_inicial = ANNIO_INICIAL;
+    $this->_annio_matricula = ANNIO_MATRICULA;
   }
 
   protected function finalize()

03f04fce6f358f56a7580a5afe4443a38e47bb79
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-01-28 22:13:00 -0500

    Update index.php

--- frontend/public/index.php
+++ frontend/public/index.php
@@ -3,27 +3,15 @@ setlocale(LC_ALL, 'es_ES');
 ini_set('date.timezone', 'America/Bogota');
 const APP_CHARSET = 'UTF-8';
 error_reporting(E_ALL ^ E_STRICT);
-ini_set('display_errors', 'Off'); // Off/On
-const PRODUCTION = false; // servidor=true / local=false
+//ini_set('display_errors', 'Off');
 
-const APP_ROOT_PRIVATE = '/home/windsor1/schoolnext_private/';
-//const APP_ROOT_PRIVATE = '/home/u113041793/domains/colegiomixtosantarosa.com/_schoolnext_santarosa_private/';
-
-//const APP_PATH = APP_ROOT_PRIVATE.'frontend/app/';
-define('APP_PATH', dirname(__DIR__).'/app/');
-
-// const CORE_PATH = APP_ROOT_PRIVATE.'core1.2/';  // SERVIDOR
-define('CORE_PATH', dirname(dirname(APP_PATH)).'/core1.2/');
-
-//const PUBLIC_PATH = 'https://schoolnext.colegiomixtosantarosa.com/';
-//const PUBLIC_PATH = 'https://schoolnext.windsorschool.edu.co/';
+const PRODUCTION = false;
+const APP_ROOT_PRIVATE = 'D:/schoolnext/';
+const APP_PATH = APP_ROOT_PRIVATE.'frontend/app/';
+//define('APP_PATH', dirname(__DIR__).'/app/');
+const CORE_PATH = APP_ROOT_PRIVATE.'core1.2/';
+//define('CORE_PATH', dirname(dirname(APP_PATH)).'/core1.2/');
 define('PUBLIC_PATH', substr($_SERVER['SCRIPT_NAME'], 0, -9)); // - index.php string[9]  LOCAL
+$url = $_SERVER['PATH_INFO'] ?? '/';
 
-//$url = $_SERVER['ORIG_PATH_INFO'] ?? '/'; // WINDSOR
-$url = $_SERVER['PATH_INFO'] ?? '/';  // LOCAL y SANTAROSA
-
-const INSTITUTION_KEY = "windsor"; // "santarosa" "windsor" "development" En minúsculas
-const APP_NAME= "SchoolNEXT>>";
-
-
-require APP_PATH . 'libs/bootstrap.php'; //bootstrap de app
+require APP_PATH . 'libs/bootstrap.php';

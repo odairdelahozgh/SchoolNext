c9845536416d4c0bd6cd7b413be6bfca3bc4d41a
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-02-05 13:59:26 -0500

    Update bootstrap.php

--- frontend/app/libs/bootstrap.php
+++ frontend/app/libs/bootstrap.php
@@ -2,7 +2,7 @@
 // Bootstrap de la aplicacion para personalizarlo
 // Para cargar cambia en public/index.php el require del bootstrap a app
 // Arranca KumbiaPHP
-const INSTITUTION_KEY = "windsor";
+const INSTITUTION_KEY = "development";
 const APP_NAME= "SchoolNEXT>>";
 
 const VENDOR_PATH = APP_PATH.'../vendor/' ;

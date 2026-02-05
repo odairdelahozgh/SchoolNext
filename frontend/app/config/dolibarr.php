045e50a060e578ef099c19dbad5c57a950a1c64d
ConstruxZion Soft (co) - Odair <40337443+odairdelahozgh@users.noreply.github.com>
2026-02-05 13:59:43 -0500

    Update dolibarr.php

--- frontend/app/config/dolibarr.php
+++ frontend/app/config/dolibarr.php
@@ -13,4 +13,10 @@ return [
     'api_key' => '1g3mbFb7Gga15MIIJ60jC95xkz6bCTXE',
   ],
 
+  'development' => [
+    'prefijo_bd' => 'llx_',
+    'api_url' => 'https://crm.construxzionsoft.com.co/api/index.php',
+    'api_key' => '30Ldd958Nk4Yow2gwjUM9wXOf2X0UEMv',
+  ],
+
 ];
